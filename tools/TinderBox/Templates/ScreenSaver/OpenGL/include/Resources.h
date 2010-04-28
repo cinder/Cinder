0885cd0d0a90687cdb817e43fe37932b1719c3e0
andrewfb <cinder.afb@gmail.com>
2010-04-28 12:38:12 -0400

    removed TB

--- tools/TinderBox/Templates/ScreenSaver/OpenGL/include/Resources.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "cinder/CinderResources.h"
-
-//#define RES_NAME		CINDER_RESOURCE( parent path, resource file name, unique integer ID, data type name )
-//   Ex: RES_PARTICLE	CINDER_RESOURCE( ../resources, particle.png, 128, PNG )
