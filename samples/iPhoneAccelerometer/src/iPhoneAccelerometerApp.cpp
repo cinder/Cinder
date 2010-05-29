3b59138c41ec8a0db1a16c932a6edb8c027351ba
andrewfb <cinder.afb@gmail.com>
2010-05-29 17:37:27 -0400

    Cleaning up some code on accelerometer sample

--- samples/iPhoneAccelerometer/src/iPhoneAccelerometerApp.cpp
+++ samples/iPhoneAccelerometer/src/iPhoneAccelerometerApp.cpp
@@ -1,7 +1,4 @@
 #include "cinder/app/AppCocoaTouch.h"
-#include "cinder/app/Renderer.h"
-#include "cinder/Surface.h"
-#include "cinder/gl/Texture.h"
 #include "cinder/Camera.h"
 
 using namespace ci;
@@ -11,15 +8,17 @@ class iPhoneAccelerometerApp : public AppCocoaTouch {
   public:
 	virtual void	setup();
 	virtual void	accelerated( AccelEvent event );
-	virtual void	update();
 	virtual void	draw();
 	
 	Matrix44f		mModelView;
+	CameraPersp		mCam;
 };
 
 void iPhoneAccelerometerApp::setup()
 {
-	enableAccelerometer( 30.0f, 0.5f );
+	enableAccelerometer();
+	mCam.setPerspective( 60, getWindowAspectRatio(), 1, 1000 );
+	mCam.lookAt( Vec3f( 0, 0, 3 ), Vec3f::zero() );	
 }
 
 void iPhoneAccelerometerApp::accelerated( AccelEvent event )
@@ -27,19 +26,12 @@ void iPhoneAccelerometerApp::accelerated( AccelEvent event )
 	mModelView = event.getMatrix();
 }
 
-void iPhoneAccelerometerApp::update()
-{
-}
-
 void iPhoneAccelerometerApp::draw()
 {
 	gl::clear( Color( 0.2f, 0.2f, 0.3f ) );
 	gl::enableDepthRead();
 
-	CameraPersp cam = CameraPersp( getWindowWidth(), getWindowHeight(), 45.0f );
-	cam.setPerspective( 60, getWindowAspectRatio(), 1, 1000 );
-	cam.lookAt( Vec3f( 0, 0, 3 ), Vec3f::zero() );
-	gl::setMatrices( cam );
+	gl::setMatrices( mCam );
 	gl::multModelView( mModelView );
 	gl::drawColorCube( Vec3f::zero(), Vec3f( 1, 1, 1 ) );
 }
