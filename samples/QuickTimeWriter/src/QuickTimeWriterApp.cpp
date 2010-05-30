74b8af8cb138010eb668b2084058c51228a83fff
andrewfb <cinder.afb@gmail.com>
2010-05-30 11:28:08 -0400

    Initial checkin of QuickTimeWriter sample

---
+++ samples/QuickTimeWriter/src/QuickTimeWriterApp.cpp
@@ -0,0 +1,55 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/qtime/MovieWriter.h"
+
+#include <list>
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+// We'll create a new Cinder Application by deriving from the AppBasic class
+class QuickTimeWriterApp : public AppBasic {
+ public:
+	void setup();
+	void mouseDrag( MouseEvent event );
+	void draw();
+	
+	// This will maintain a list of points which we will draw line segments between
+	list<Vec2f>			mPoints;
+	qtime::MovieWriter	mMovieWriter;
+};
+
+void QuickTimeWriterApp::setup()
+{
+	string path = getSaveFilePath();
+	if( ! path.empty() )
+		mMovieWriter = qtime::MovieWriter( path, qtime::H264 );
+}
+
+void QuickTimeWriterApp::mouseDrag( MouseEvent event )
+{
+	// add wherever the user drags to the end of our list of points
+	mPoints.push_back( event.getPos() );
+}
+
+void QuickTimeWriterApp::draw()
+{
+	gl::clear( Color( 0.1f, 0.1f, 0.1f ) );
+
+	// We'll set the color to an orange color
+	glColor3f( 1.0f, 0.5f, 0.25f );
+	
+	// now tell OpenGL we've got a series of points it should draw lines between
+	glBegin( GL_LINE_STRIP );
+	// iterate across our list of points, and pass each one to OpenGL
+	for( list<Vec2f>::iterator pointIter = mPoints.begin(); pointIter != mPoints.end(); ++pointIter ) {
+		glVertex2f( pointIter->x, pointIter->y );
+	}
+	// tell OpenGL to actually draw the lines now
+	glEnd();
+	
+	if( mMovieWriter )
+		mMovieWriter.addFrame( copyWindowSurface() );
+}
+
+// This line tells Cinder to actually create the application
+CINDER_APP_BASIC( QuickTimeWriterApp, RendererGl )
