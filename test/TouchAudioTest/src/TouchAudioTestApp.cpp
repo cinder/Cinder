a75f58f4eb01b83c4fe5fe9e3e6a62c283c1de64
Mike O'Toole <mike.otoole@gmail.com>
2010-06-22 13:01:58 -0400

    adding test for audio on CocoaTouch

---
+++ test/TouchAudioTest/src/TouchAudioTestApp.cpp
@@ -0,0 +1,48 @@
+#include "cinder/app/AppCocoaTouch.h"
+#include "cinder/audio/Output.h"
+#include "cinder/audio/Io.h"
+
+#include "Resources.h"
+
+using namespace ci;
+using namespace ci::app;
+
+class TouchAudioTestApp : public AppCocoaTouch {
+  public:
+	virtual void	setup();
+	virtual void	resize( int width, int height );
+	virtual void	update();
+	virtual void	draw();
+	virtual void	touchesBegan( TouchEvent event );
+	
+	audio::SourceRef mAudioSource;
+};
+
+void TouchAudioTestApp::setup()
+{
+	mAudioSource = audio::load( loadResource( RES_BOOYAH ) );
+	std::cout << "Ok!" << std::endl;
+}
+
+void TouchAudioTestApp::resize( int width, int height )
+{
+	
+}
+
+void TouchAudioTestApp::touchesBegan( TouchEvent event )
+{
+	std::cout << "Touch" << std::endl;
+	audio::Output::play( mAudioSource );
+}
+
+void TouchAudioTestApp::update()
+{
+	
+}
+
+void TouchAudioTestApp::draw()
+{
+	gl::clear( Color( 0.0f, 0.0f, 0.0f ) );
+}
+
+CINDER_APP_COCOA_TOUCH( TouchAudioTestApp, RendererGl )
