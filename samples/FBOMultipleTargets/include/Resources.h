83aa7eac64c82053efdaf01195195e8bd6566d22
Andrew Bell <cinder.afb@gmail.com>
2010-08-07 23:51:21 -0400

    Initial checkin of FBOMultipleTargets sample

---
+++ samples/FBOMultipleTargets/include/Resources.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "cinder/CinderResources.h"
+#include "Resources.h"
+
+//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
---
+++ samples/FBOMultipleTargets/src/FBOMultipleTargetsApp.cpp
@@ -0,0 +1,102 @@
+#include "cinder/app/AppBasic.h"
+#include "cinder/Camera.h"
+#include "cinder/gl/Fbo.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+// This sample shows a very basic use case for FBOs - it renders a spinning torus
+// into an FBO, and uses that as a Texture onto the sides of a cube.
+class FBOMultipleTargetsApp : public AppBasic {
+  public:
+	virtual void	setup();
+	virtual void	update();
+	virtual void	draw();
+
+	void			renderSceneToFbo();
+	
+	gl::Fbo				mFbo;
+	Matrix44f			mTorusRotation;
+	static const int	FBO_WIDTH = 256, FBO_HEIGHT = 256;
+};
+
+void FBOMultipleTargetsApp::setup()
+{
+	gl::Fbo::Format format;
+	//format.setSamples( 4 ); // uncomment this to enable 4x antialiasing
+	mFbo = gl::Fbo( FBO_WIDTH, FBO_HEIGHT, format );
+
+	gl::enableDepthRead();
+	gl::enableDepthWrite();	
+
+	mTorusRotation.setToIdentity();
+}
+
+// Render the torus into the FBO
+void FBOMultipleTargetsApp::renderSceneToFbo()
+{
+	// bind the framebuffer - now everything we draw will go there
+	mFbo.bindFramebuffer();
+
+	// setup the viewport to match the dimensions of the FBO
+	gl::setViewport( mFbo.getBounds() );
+
+	// setup our camera to render the torus scene
+	CameraPersp cam( mFbo.getWidth(), mFbo.getHeight(), 60.0f );
+	cam.setPerspective( 60, mFbo.getAspectRatio(), 1, 1000 );
+	cam.lookAt( Vec3f( 2.8f, 1.8f, -2.8f ), Vec3f::zero() );
+	gl::setMatrices( cam );
+
+	// set the modelview matrix to reflect our current rotation
+	gl::multModelView( mTorusRotation );
+	
+	// clear out the FBO with blue
+	gl::clear( Color( 0.25, 0.5f, 1.0f ) );
+
+	// render an orange torus, with no textures
+	glDisable( GL_TEXTURE_2D );
+	gl::color( Color( 1.0f, 0.5f, 0.25f ) );
+	gl::drawTorus( 1.4f, 0.3f, 32, 64 );
+
+	// unbind the framebuffer, so that drawing goes to the screen again
+	mFbo.unbindFramebuffer();
+}
+
+void FBOMultipleTargetsApp::update()
+{
+	// Rotate the torus by .06 radians around an arbitrary axis
+	mTorusRotation.rotate( Vec3f( 0.16666f, 0.333333f, 0.666666f ).normalized(), 0.06f );
+	
+	// render into our FBO
+	renderSceneToFbo();
+}
+
+void FBOMultipleTargetsApp::draw()
+{
+	// clear the window to gray
+	gl::clear( Color( 0.35f, 0.35f, 0.35f ) );
+
+	// setup our camera to render the cube
+	CameraPersp cam( getWindowWidth(), getWindowHeight(), 60.0f );
+	cam.setPerspective( 60, getWindowAspectRatio(), 1, 1000 );
+	cam.lookAt( Vec3f( 2.6f, 1.6f, -2.6f ), Vec3f::zero() );
+	gl::setMatrices( cam );
+
+	// set the viewport to match our window
+	gl::setViewport( getWindowBounds() );
+
+	// use the scene we rendered into the FBO as a texture
+	glEnable( GL_TEXTURE_2D );
+	mFbo.bindTexture();
+
+	// draw a cube textured with the FBO
+	gl::color( Color::white() );
+	gl::drawCube( Vec3f::zero(), Vec3f( 2.2f, 2.2f, 2.2f ) );
+
+	// show the FBO texture in the upper left corner
+	gl::setMatricesWindow( getWindowSize() );
+	gl::draw( mFbo.getTexture(0), Rectf( 0, 0, 96, 96 ) );
+}
+
+CINDER_APP_BASIC( FBOMultipleTargetsApp, RendererGl )
