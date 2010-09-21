bd6768b8812479b4719571b33229d9e4a0cfeb1d
Mike O'Toole <mike.otoole@gmail.com>
2010-09-21 12:54:59 -0400

    cleaning up audio analysis sample

--- samples/AudioAnalysisSample/src/AudioAnalysisSampleApp.cpp
+++ samples/AudioAnalysisSample/src/AudioAnalysisSampleApp.cpp
@@ -2,6 +2,7 @@
 #include "cinder/audio/Io.h"
 #include "cinder/audio/Output.h"
 #include "cinder/audio/FftProcessor.h"
+#include "cinder/audio/PcmBuffer.h"
 
 #include "Resources.h"
 using namespace ci;
@@ -12,84 +13,103 @@ using namespace std;
 class AudioAnalysisSampleApp : public AppBasic {
  public:
 	void setup();
+	void update();
 	void draw();
-	void drawWaveForm( audio::TrackRef track );
-	void drawFft( audio::TrackRef track );
 	void keyDown( KeyEvent e );
 	
-	audio::TrackRef mTrack1;
-	audio::TrackRef mTrack2;
+	void drawWaveForm();
+	void drawFft();
+	
+	audio::TrackRef mTrack;
+	audio::PcmBuffer32fRef mPcmBuffer;
 };
 
 void AudioAnalysisSampleApp::setup()
 {
-	//mTrack1 = audio::Output::addTrack( audio::load( "C:\\code\\cinder\\samples\\AudioPlayback\\resources\\booyah.mp3" ) );
-	//mTrack1->setPcmBuffering( true );
-	mTrack1 = audio::Output::addTrack( audio::load( loadResource( RES_GUITAR ) ) );
-	//mTrack1 = audio::Output::addTrack( audio::load( "../../../../AudioPlayback/resources/booyah.mp3" ) );
-	mTrack1->setPcmBuffering( true );
-	//mTrack2 = audio::Output::addTrack( audio::load( loadResource( RES_DRUMS ) ) );
-	//mTrack2->setPcmBuffering( true );
+	//add the audio track the default audio output
+	mTrack = audio::Output::addTrack( audio::load( loadResource( RES_GUITAR ) ) );
+	
+	//you must enable enable PCM buffering on the track to be able to call getPcmBuffer on it later
+	mTrack->setPcmBuffering( true );
 }
 
 void AudioAnalysisSampleApp::keyDown( KeyEvent e ) {
 	if( e.getChar() == 'p' ) {
-		( mTrack1->isPlaying() ) ? mTrack1->stop() : mTrack1->play();
-	} else if( e.getChar() == 'c' ) {
-		std::cout << audio::Output::getVolume() << std::endl;
-		std::cout << mTrack1->getVolume() << std::endl;
+		( mTrack->isPlaying() ) ? mTrack->stop() : mTrack->play();
 	}
 }
 
-void AudioAnalysisSampleApp::drawWaveForm( audio::TrackRef track )
+void AudioAnalysisSampleApp::update()
 {
-	audio::PcmBuffer32fRef aPcmBuffer = track->getPcmBuffer();
-	if( ! aPcmBuffer ) {
+	//get the latest pcm buffer from the track
+	mPcmBuffer = mTrack->getPcmBuffer();
+}
+
+void AudioAnalysisSampleApp::draw()
+{
+	gl::clear( Color( 0.0f, 0.0f, 0.0f ) );
+	
+	glPushMatrix();
+		glTranslatef( 0.0, 0.0, 0.0 );
+		drawWaveForm();
+#if defined( CINDER_MAC )
+		glTranslatef( 0.0, 200.0, 0.0 );
+		drawFft();
+#endif
+	glPopMatrix();
+}
+
+void AudioAnalysisSampleApp::drawWaveForm()
+{	
+	//if the buffer is null, for example if this gets called before any PCM data has been buffered
+	//don't do anything
+	if( ! mPcmBuffer ) {
 		return;
 	}
 	
-	uint32_t bufferSamples = aPcmBuffer->getSampleCount();
-	audio::Buffer32fRef leftBuffer = aPcmBuffer->getChannelData( audio::CHANNEL_FRONT_LEFT );
-	audio::Buffer32fRef rightBuffer = aPcmBuffer->getChannelData( audio::CHANNEL_FRONT_RIGHT );
+	uint32_t bufferLength = mPcmBuffer->getSampleCount();
+	audio::Buffer32fRef leftBuffer = mPcmBuffer->getChannelData( audio::CHANNEL_FRONT_LEFT );
+	audio::Buffer32fRef rightBuffer = mPcmBuffer->getChannelData( audio::CHANNEL_FRONT_RIGHT );
 
 	int displaySize = getWindowWidth();
-	int endIdx = bufferSamples;
+	float scale = displaySize / (float)bufferLength;
 	
-	float scale = displaySize / (float)endIdx;
+	PolyLine<Vec2f>	leftBufferLine;
+	PolyLine<Vec2f>	rightBufferLine;
 	
-	glColor3f( 1.0f, 0.5f, 0.25f );
-	glBegin( GL_LINE_STRIP );
-	for( int i = 0; i < endIdx; i++ ) {
+	for( int i = 0; i < bufferLength; i++ ) {
+		float x = ( i * scale );
+	
+		//get the PCM value from the left channel buffer
 		float y = ( ( leftBuffer->mData[i] - 1 ) * - 100 );
-		glVertex2f( ( i * scale ) , y );
+		leftBufferLine.push_back( Vec2f( x , y) );
+		
+		y = ( ( rightBuffer->mData[i] - 1 ) * - 100 );
+		rightBufferLine.push_back( Vec2f( x , y) );
 	}
-	glEnd();
+	gl::color( Color( 1.0f, 0.5f, 0.25f ) );
+	gl::draw( leftBufferLine );
+	gl::draw( rightBufferLine );
 	
-	glColor3f( 1.0f, 0.96f, 0.0f );
-	glBegin( GL_LINE_STRIP );
-	for( int i = 0; i < endIdx; i++ ) {
-		float y = ( ( rightBuffer->mData[i] - 1 ) * - 100 );
-		glVertex2f( ( i * scale ) , y );
-	}
-	glEnd();
 }
 
-void AudioAnalysisSampleApp::drawFft( audio::TrackRef track )
+#if defined(CINDER_MAC)
+void AudioAnalysisSampleApp::drawFft()
 {
 	float ht = 100.0f;
 	uint16_t bandCount = 32;
 	
-	audio::PcmBuffer32fRef aPcmBuffer = track->getPcmBuffer();
-	if( ! aPcmBuffer ) {
-		return;
-	}
-	boost::shared_ptr<float> fftRef = audio::calculateFft( aPcmBuffer->getChannelData( audio::CHANNEL_FRONT_LEFT ), bandCount );
+	if( ! mPcmBuffer ) return;
+	
+	//use the most recent Pcm data to calculate the Fft
+	boost::shared_ptr<float> fftRef = audio::calculateFft( mPcmBuffer->getChannelData( audio::CHANNEL_FRONT_LEFT ), bandCount );
 	if( ! fftRef ) {
 		return;
 	}
 	
 	float * fftBuffer = fftRef.get();
 	
+	//draw the bands
 	for( int i = 0; i < ( bandCount ); i++ ) {
 		float barY = fftBuffer[i] / bandCount * ht;
 		glBegin( GL_QUADS );
@@ -102,20 +122,7 @@ void AudioAnalysisSampleApp::drawFft( audio::TrackRef track )
 		glEnd();
 	}
 }
-
-void AudioAnalysisSampleApp::draw()
-{
-	gl::clear( Color( 0.0f, 0.0f, 0.0f ) );
-	
-	glPushMatrix();
-		glTranslatef( 0.0, 0.0, 0.0 );
-		drawFft( mTrack1 );
-		//drawWaveForm( mTrack1 );
-		glTranslatef( 0.0, 120.0, 0.0 );
-		//drawFft( mTrack2 );
-		//drawWaveForm( mTrack2 );
-	glPopMatrix();
-}
+#endif
 
 // This line tells Cinder to actually create the application
 CINDER_APP_BASIC( AudioAnalysisSampleApp, RendererGl )
