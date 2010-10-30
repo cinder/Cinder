52ae371dc01e1758f7e80577101e5c59beb4c1ee
Andrew Bell <cinder.afb@gmail.com>
2010-10-29 22:14:34 -0400

    Updating samples for new XML

--- samples/Earthquake/src/EarthquakeApp.cpp
+++ samples/Earthquake/src/EarthquakeApp.cpp
@@ -242,7 +242,7 @@ void EarthquakeApp::draw()
 
 void EarthquakeApp::parseEarthquakes( const string &url )
 {
-	XmlDocument doc( loadUrlStream( url ) );
+/*	XmlDocument doc( loadUrlStream( url ) );
 	vector<XmlElement> items( doc.rootNode().children() );
 
 	for( vector<XmlElement>::iterator itemIter = items.begin(); itemIter != items.end(); ++itemIter ) {
@@ -259,14 +259,37 @@ void EarthquakeApp::parseEarthquakes( const string &url )
 			titleString.getline( lineBuffer, 511 );
 			title = string( lineBuffer );
 			
+			//const XmlElement &dateEl = itemIter->findChild( "updated" );
+			
 			const XmlElement &locationEl = itemIter->findChild( "georss:point" );
 			istringstream locationString( locationEl.value() );
 			Vec2f locationVector;
 			locationString >> locationVector.x >> locationVector.y;
 			
+			float elevationValue = fromString<float>( itemIter->findChild( "georss:elev" ).value() );
+	console() << "El: " << elevationValue;
 			mEarth.addQuake( locationVector.x, locationVector.y, magnitude, title );
 		}
+	}*/
+	
+	XmlTree xml( loadUrl( Url( url ) ) );
+	for( XmlTree::Iter itemIter = xml.begin( "feed/entry" ); itemIter != xml.end(); ++itemIter ) {
+		string titleLine( itemIter->getChild( "title" ).getValue() );
+		size_t firstComma = titleLine.find( ',' );
+		float magnitude = fromString<float>( titleLine.substr( titleLine.find( ' ' ) + 1, firstComma - 2 ) );
+		string title = titleLine.substr( firstComma + 2 );
+
+		istringstream locationString( itemIter->getChild( "georss:point" ).getValue() );
+		Vec2f locationVector;
+		locationString >> locationVector.x >> locationVector.y;
+		
+		float elevationValue = itemIter->getChild( "georss:elev" ).getValue<float>( 0 );
+std::cout << "Elevation: " << elevationValue << std::endl;
+		mEarth.addQuake( locationVector.x, locationVector.y, magnitude, title );		
 	}
+	console() << xml << std::endl;
+	
+	//mEarth.addQuake( 37.7f, -122.0f, 8.6f, "San Francisco" );
 }
 
 
