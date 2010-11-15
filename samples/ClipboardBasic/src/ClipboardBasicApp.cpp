a13ecab81d40f0f6e3187272b555b717003a24a9
andrewfb <cinder.afb@gmail.com>
2010-11-14 20:59:39 -0500

    Initial checking of ClipboardBasic sample

---
+++ samples/ClipboardBasic/src/ClipboardBasicApp.cpp
@@ -0,0 +1,41 @@
+﻿#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Clipboard.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Utilities.h"
+
+#include "Resources.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class ClipboardBasicApp : public AppBasic {
+  public:
+	void keyDown( KeyEvent event );	
+	void draw();
+};
+
+void ClipboardBasicApp::keyDown( KeyEvent event )
+{
+	if( event.getChar() == 'c' && event.isAccelDown() ) {
+		Clipboard::setImage( loadImage( loadResource( RES_CINDER_LOGO ) ) );
+		// to copy the contents of the window, you might do something like
+		// Clipboard::setImage( copyWindowSurface() );
+	}
+}
+
+void ClipboardBasicApp::draw()
+{
+	gl::clear( Color( 0.5f, 0.5f, 0.5f ) ); 
+	gl::enableAlphaBlending();
+	
+	if( Clipboard::hasImage() )
+		gl::draw( gl::Texture( Clipboard::getImage() ) );
+	else if( Clipboard::hasString() )
+		gl::drawString( Clipboard::getString(), Vec2f::zero() );
+	else
+		gl::drawString( "Clipboard contents unknown", Vec2f::zero() );
+}
+
+CINDER_APP_BASIC( ClipboardBasicApp, RendererGl )
