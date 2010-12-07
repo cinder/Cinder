83271912fe3526056964dbe5efd6403a88dcb634
Andrew Bell <cinder.afb@gmail.com>
2010-12-07 00:47:59 -0500

    Updated build script for iOS 4.2

--- xcode/fullbuild.sh
+++ xcode/fullbuild.sh
@@ -1,3 +1,6 @@
-xcodebuild -project cinder.xcodeproj -alltargets -configuration Release
-xcodebuild -project cinder.xcodeproj -alltargets -configuration Debug
-
+xcodebuild -project cinder.xcodeproj -target cinder -configuration Release
+xcodebuild -project cinder.xcodeproj -target cinder_iphone -configuration Release -sdk iphoneos
+xcodebuild -project cinder.xcodeproj -target cinder_iphone_sim -configuration Release -sdk iphonesimulator
+xcodebuild -project cinder.xcodeproj -target cinder -configuration Debug
+xcodebuild -project cinder.xcodeproj -target cinder_iphone -configuration Debug -sdk iphoneos
+xcodebuild -project cinder.xcodeproj -target cinder_iphone_sim -configuration Debug -sdk iphonesimulator
