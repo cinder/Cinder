23347d65c5a3ec73f0824d9a45013535458a0ea6
Victor Martins <vic@pixelnerve.com>
2011-05-07 17:52:50 +0200

    Changed depth buffer bits for opengl (16 to 24)

--- src/cinder/app/AppImplMswRendererGl.cpp
+++ src/cinder/app/AppImplMswRendererGl.cpp
@@ -188,7 +188,7 @@ bool AppImplMswRendererGl::initializeInternal( HWND wnd, HDC dc )
 		0,											// Shift Bit Ignored
 		0,											// No Accumulation Buffer
 		0, 0, 0, 0,									// Accumulation Bits Ignored
-		16,											// 32Bit Z-Buffer (Depth Buffer)  
+		32,											// 32Bit Z-Buffer (Depth Buffer)  
 		0,											// No Stencil Buffer
 		0,											// No Auxiliary Buffer
 		PFD_MAIN_PLANE,								// Main Drawing Layer
@@ -273,7 +273,7 @@ int AppImplMswRendererGl::initMultisample( PIXELFORMATDESCRIPTOR pfd, int reques
 		WGL_ACCELERATION_ARB,WGL_FULL_ACCELERATION_ARB,
 		WGL_COLOR_BITS_ARB,24,
 		WGL_ALPHA_BITS_ARB,8,
-		WGL_DEPTH_BITS_ARB,16,
+		WGL_DEPTH_BITS_ARB,32,
 		WGL_STENCIL_BITS_ARB,0,
 		WGL_DOUBLE_BUFFER_ARB,GL_TRUE,
 		WGL_SAMPLE_BUFFERS_ARB,GL_TRUE,
