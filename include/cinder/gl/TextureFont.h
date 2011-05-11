ea5af5b5884ca125be365c834fcae03fb1765641
andrewfb <cinder.afb@gmail.com>
2011-05-11 10:33:22 -0400

    Fixed missing 7 glyph in TextureFont default chars

--- include/cinder/gl/TextureFont.h
+++ include/cinder/gl/TextureFont.h
@@ -88,7 +88,7 @@ class TextureFont {
 	};
 
 	//! Creates a new TextureFontRef with font \a font, ensuring that glyphs necessary to render \a supportedChars are renderable, and format \a format
-	static TextureFontRef		create( const Font &font, const std::string &supportedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456890().?!,:;'\"&*=+-/\\@#_[]<>%^llflphridséèáà", const Format &format = Format() )
+	static TextureFontRef		create( const Font &font, const std::string &supportedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890().?!,:;'\"&*=+-/\\@#_[]<>%^llflphridséèáà", const Format &format = Format() )
 	{ return TextureFontRef( new TextureFont( font, supportedChars, format ) ); }
 	
 	//! Draws string \a str at baseline \a baseline with DrawOptions \a options
