d1c5fc01058d60b9ef74d636136015cd35974ea1
Andrew Bell <cinder.afb@gmail.com>
2011-08-05 00:29:18 -0400

    Fixed iOS AudioHardware include

--- include/cinder/audio/OutputImplAudioUnit.h
+++ include/cinder/audio/OutputImplAudioUnit.h
@@ -27,7 +27,7 @@
 #include <AudioUnit/AudioUnit.h>
 #include <AudioToolbox/AUGraph.h>
 #include <boost/thread/mutex.hpp>
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_7
+#if defined( CINDER_MAC) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_7)
 	#include <CoreAudio/AudioHardware.h>
 #endif
 #include <stack>
