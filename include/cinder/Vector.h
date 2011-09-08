c012309e43cf9e4867f6aae7f881de9d60be7cc3
unknown <absolut@.(none)>
2011-09-08 09:51:04 +0100

    Added typedefs for Vec3i and Vec4i

--- include/cinder/Vector.h
+++ include/cinder/Vector.h
@@ -1300,12 +1300,16 @@ template <typename T> bool isNaN( const Vec2<T>& a ) { return isNaN( a.x ) || is
 template <typename T> bool isNaN( const Vec3<T>& a ) { return isNaN( a.x ) || isNaN( a.y ) || isNaN( a.z ); }
 template <typename T> bool isNaN( const Vec4<T>& a ) { return isNaN( a.x ) || isNaN( a.y ) || isNaN( a.z ) || isNaN( a.w ); }
 
-typedef Vec2<float>		Vec2f;
-typedef Vec2<double>	Vec2d;
-typedef Vec3<float>		Vec3f;
-typedef Vec3<double>	Vec3d;
-typedef Vec4<float>		Vec4f;
-typedef Vec4<double>	Vec4d;
 typedef Vec2<int>		Vec2i;
+typedef Vec3<int>		Vec3i;
+typedef Vec4<int>		Vec4i;
+
+typedef Vec2<float>		Vec2f;
+typedef Vec3<float>		Vec3f;
+typedef Vec4<float>		Vec4f;
+
+typedef Vec2<double>	Vec2d;
+typedef Vec3<double>	Vec3d;
+typedef Vec4<double>	Vec4d;
 
 } // namespace cinder
