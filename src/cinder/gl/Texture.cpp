1e3ead650cb66cdc5a65b7684942bbafab8eea07
Roger Sodre <rbentos@yahoo.com>
2012-01-15 17:59:45 -0200

    Remember target on class ImageSourceTexture : public ImageSource

--- src/cinder/gl/Texture.cpp
+++ src/cinder/gl/Texture.cpp
@@ -1090,7 +1090,9 @@ class ImageSourceTexture : public ImageSource {
 		mRowBytes = mWidth * ImageIo::channelOrderNumChannels( mChannelOrder ) * dataSize;
 		mData = shared_ptr<uint8_t>( new uint8_t[mRowBytes * mHeight], boost::checked_array_delete<uint8_t> );
 		gl::SaveTextureBindState( texture.getTarget() );
-		texture.bind();
+		BoolState saveEnabledState( texture.getTarget() );	// ROGER
+		texture.enableAndBind();							// ROGER
+		//texture.bind();									// ROGER
 		glPixelStorei( GL_PACK_ALIGNMENT, 1 );
 		glGetTexImage( texture.getTarget(), 0, format, dataType, mData.get() );
 	}
