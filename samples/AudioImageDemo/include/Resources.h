769f92219c17f77b7830e93fd695519e5651cfef
Jared Duke <glorindel@gmail.com>
2012-01-22 21:06:30 -0800

    Adding forgotten resource files.

---
+++ samples/AudioImageDemo/include/Resources.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+#define RES_SONG		CINDER_RESOURCE( ../resources/, Free_Beats.mp3, 128, MP3 )
+#define RES_IMAGE	CINDER_RESOURCE( ../resources/, Free_Image.jpg, 129, JPG )
