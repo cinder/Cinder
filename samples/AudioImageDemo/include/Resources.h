58fa117036642296f85ee74004e7a08c32f42d85
Jared D <glorindel@gmail.com>
2012-01-31 17:54:13 -0800

    Prep for upstream pull request.

--- samples/AudioImageDemo/include/Resources.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "cinder/CinderResources.h"
-
-#define RES_SONG		CINDER_RESOURCE( ../resources/, Free_Beats.mp3, 128, MP3 )
-#define RES_IMAGE	CINDER_RESOURCE( ../resources/, Free_Image.jpg, 129, JPG )
