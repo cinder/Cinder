2e4afabc0d6748863c205c7621705331b1409466
Andrew Bell <cinder.afb@gmail.com>
2012-02-20 20:27:45 -0500

    Moved PolygonBoolean sample to using static methods

--- samples/PolygonBoolean/src/PolygonBooleanApp.cpp
+++ samples/PolygonBoolean/src/PolygonBooleanApp.cpp
@@ -2,17 +2,7 @@
 #include "cinder/Triangulate.h"
 #include "cinder/TriMesh.h"
 #include "cinder/gl/gl.h"
-
-#include <boost/geometry.hpp>
-#include <boost/geometry/geometries/point_xy.hpp>
-#include <boost/geometry/geometries/polygon.hpp>
-#include <boost/geometry/multi/geometries/multi_polygon.hpp>
-#include <boost/geometry/domains/gis/io/wkt/wkt.hpp>
-#include <boost/geometry/geometry.hpp>
-#include <boost/geometry/geometries/adapted/c_array.hpp>
-#include <boost/geometry/multi/multi.hpp>
-
-#include <boost/foreach.hpp>
+#include "cinder/params/Params.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -21,82 +11,22 @@ using namespace std;
 class PolygonBooleanApp : public AppBasic {
   public:
 	void setup();
-	void mouseDown( MouseEvent event );	
-	void update();
 	void draw();
+
+	void				doUnion();
+	void				doIntersection();
+	void				doXor();
+	void				doDifference();
 
 	vector<PolyLine2f>	makePolyWithHole() const;
 	vector<PolyLine2f>	makeStar() const;
 	vector<PolyLine2f>	makeLetterA() const;
 	
-	vector<PolyLine2f>	calcUnion( const vector<PolyLine2f> &a, vector<PolyLine2f> &b );
-	
 	vector<PolyLine2f> 	mPolyA, mPolyB, mPolyResult;
-	TriMesh2d			mPolyAMesh, mPolyBMesh, mPolyResultMesh;
+	TriMesh2d			mPolyAMesh, mPolyBMesh;
+	
+	params::InterfaceGl	mParams;
 };
-
-namespace {
-typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double> > polygon;
-
-vector<PolyLine2f> convertBoostGeometryPolygons( std::vector<polygon> &polygons )
-{
-	vector<PolyLine2f> result;
-	for( vector<polygon>::const_iterator outIt = polygons.begin(); outIt != polygons.end(); ++outIt ) {
-		typedef polygon::inner_container_type::const_iterator RingIterator;
-		typedef polygon::ring_type::const_iterator PointIterator;
-
-		result.push_back( PolyLine2f() );	
-		for( PointIterator pt = outIt->outer().begin(); pt != outIt->outer().end(); ++pt )
-			result.back().push_back( Vec2f( boost::geometry::get<0>(*pt), boost::geometry::get<1>(*pt) ) );
-
-		for( RingIterator crunk = outIt->inners().begin(); crunk != outIt->inners().end(); ++crunk ) {
-			PolyLine2f contour;
-			for( PointIterator pt = crunk->begin(); pt != crunk->end(); ++pt )
-				contour.push_back( Vec2f( boost::geometry::get<0>(*pt), boost::geometry::get<1>(*pt) ) );
-			result.push_back( contour );
-		}
-	}
-	
-	return result;
-}
-
-polygon convertPolyLinesToBoostGeometry( const vector<PolyLine2f> &a )
-{
-	polygon result;
-	
-	for( vector<Vec2f>::const_iterator ptIt = a[0].getPoints().begin(); ptIt != a[0].getPoints().end(); ++ptIt )
-		result.outer().push_back( boost::geometry::make<boost::geometry::model::d2::point_xy<double> >( ptIt->x, ptIt->y ) );
-	for( vector<PolyLine2f>::const_iterator plIt = a.begin() + 1; plIt != a.end(); ++plIt ) {
-		polygon::ring_type ring;
-		for( vector<Vec2f>::const_iterator ptIt = plIt->getPoints().begin(); ptIt != plIt->getPoints().end(); ++ptIt )
-			ring.push_back( boost::geometry::make<boost::geometry::model::d2::point_xy<double> >( ptIt->x, ptIt->y ) );
-		result.inners().push_back( ring );
-	}
-	
-	boost::geometry::correct( result );
-	
-	return result;
-}
-} // anonymous namespace
-
-vector<PolyLine2f> PolygonBooleanApp::calcUnion( const vector<PolyLine2f> &a, vector<PolyLine2f> &b )
-{
-	typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double> > polygon;
-
-	if( a.empty() )
-		return b;
-	else if( b.empty() )
-		return a;
-
-	polygon polyA = convertPolyLinesToBoostGeometry( a );
-	polygon polyB = convertPolyLinesToBoostGeometry( b );
-	
-	std::vector<polygon> output;
-//	boost::geometry::union_( polyA, polyB, output );
-	boost::geometry::intersection( polyA, polyB, output );
-
-	return convertBoostGeometryPolygons( output );
-}
 
 vector<PolyLine2f> PolygonBooleanApp::makePolyWithHole() const
 {
@@ -151,42 +81,58 @@ TriMesh2d makeMesh( const vector<PolyLine2f> &polys )
 	return triangulator.calcMesh();
 }
 
+void PolygonBooleanApp::doIntersection()
+{
+	mPolyResult = PolyLine2f::calcIntersection( mPolyA, mPolyB );
+}
+
+void PolygonBooleanApp::doUnion()
+{
+	mPolyResult = PolyLine2f::calcUnion( mPolyA, mPolyB );
+}
+
+void PolygonBooleanApp::doXor()
+{
+	mPolyResult = PolyLine2f::calcXor( mPolyA, mPolyB );
+}
+
+void PolygonBooleanApp::doDifference()
+{
+	mPolyResult = PolyLine2f::calcDifference( mPolyA, mPolyB );
+}
+
 void PolygonBooleanApp::setup()
 {
+	mParams = params::InterfaceGl( "App parameters", Vec2i( 180, 100 ) );
+	mParams.addButton( "Union", std::bind( &PolygonBooleanApp::doUnion, this ) );
+	mParams.addButton( "Intersection", std::bind( &PolygonBooleanApp::doIntersection, this ) );
+	mParams.addButton( "XOR", std::bind( &PolygonBooleanApp::doXor, this ) );
+	mParams.addButton( "Difference", std::bind( &PolygonBooleanApp::doDifference, this ) );
+
 	mPolyA = makePolyWithHole();
 	mPolyAMesh = makeMesh( mPolyA );
 
-	mPolyB = makeLetterA();//makeStar();
+	mPolyB = makeLetterA();	//makeStar();
 	mPolyBMesh = makeMesh( mPolyB );
-
-	mPolyResult = calcUnion( mPolyA, mPolyB );
-	mPolyResultMesh = makeMesh( mPolyB );
-}
-
-void PolygonBooleanApp::mouseDown( MouseEvent event )
-{
-}
-
-void PolygonBooleanApp::update()
-{
 }
 
 void PolygonBooleanApp::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0.24f, 0.24f, 0.24f ) );
 	gl::enableAlphaBlending();
+	gl::clear( Color( 0.24f, 0.24f, 0.24f ) );
 	
-	gl::color( ColorA( 0.25f, 0.5f, 1.0f, 0.25f ) );
+	gl::color( ColorA( 0.25f, 0.5f, 1.0f, 0.15f ) );
 	gl::draw( mPolyAMesh );
 
-	gl::color( ColorA( 0.25f, 1.0f, 0.5f, 0.25f ) );
+	gl::color( ColorA( 0.25f, 1.0f, 0.5f, 0.15f ) );
 	gl::draw( mPolyBMesh );
 	
 	glLineWidth( 2.0f );
-	gl::color( ColorA( 1.0f, 0.5f, 0.25f, 0.9f ) );
+	gl::color( ColorA( 1.0f, 0.5f, 0.25f, 1.0f ) );
 	for( size_t c = 0; c < mPolyResult.size(); ++c )
 		gl::draw( mPolyResult[c] );	
+		
+	mParams.draw();
 }
 
 CINDER_APP_BASIC( PolygonBooleanApp, RendererGl )
