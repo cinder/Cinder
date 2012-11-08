b735b1e939489f07634932c26f819494e926a83c
Andrew Bell <cinder.afb@gmail.com>
2012-11-07 20:25:40 -0500

    Fixing StereoscopicRendering sample use of 'half' reserved word in shader

--- samples/StereoscopicRendering/assets/shaders/interlaced_frag.glsl
+++ samples/StereoscopicRendering/assets/shaders/interlaced_frag.glsl
@@ -6,7 +6,7 @@ uniform vec2		window_origin;
 uniform vec2		window_size;
 
 // find the correct half of the texture to sample (over-under FBO)
-const vec2			half = vec2(1.0, 0.5);
+const vec2			halfOffset = vec2(1.0, 0.5);
 const vec2			offset = vec2(0.0, 0.5);
 
 void main()
@@ -18,5 +18,5 @@ void main()
 	float isOdd = mod( floor(y), 2.0 );
 
 	// sample the correct half of the texture
-	gl_FragColor = texture2D( tex0, gl_TexCoord[0].st * half + ( isOdd * offset ) );
+	gl_FragColor = texture2D( tex0, gl_TexCoord[0].st * halfOffset + ( isOdd * offset ) );
 }
