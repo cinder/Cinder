0531531bea384d3a66b9a4fc0863aa11ba4025be
Andrew Bell <cinder.afb@gmail.com>
2013-01-26 02:55:37 -0500

    More improvements to Retina sample - cleanup and text rendering

--- samples/RetinaSample/src/RetinaSampleApp.cpp
+++ samples/RetinaSample/src/RetinaSampleApp.cpp
@@ -1,6 +1,8 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
+#include "cinder/gl/TextureFont.h"
+#include "cinder/PolyLine.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -9,16 +11,16 @@ using namespace std;
 class RetinaSampleApp : public AppNative {
   public:
 	void prepareSettings( Settings *settings ) override;
-	void setup();
-	void mouseDrag( MouseEvent event );
-	void keyDown( KeyEvent event );
+	void setup() override;
+	void mouseDrag( MouseEvent event ) override;
 	void displayChange();
-	void draw();
+	void draw() override;
 	
 	// This will maintain a list of points which we will draw line segments between
-	list<Vec2f>		mPoints;
+	PolyLine2f		mPoints;
 	
-	gl::Texture		mLogo;
+	gl::Texture			mLogo;
+	gl::TextureFontRef	mFont;
 };
 
 void RetinaSampleApp::prepareSettings( Settings *settings )
@@ -32,18 +34,15 @@ void RetinaSampleApp::setup()
 	// helps us see the change in going from Retina to non-Retina
 	mLogo = loadImage( loadResource( "CinderApp.icns" ) );
 
+	// A font suitable for 24points at both Retina and non-Retina
+	mFont = gl::TextureFont::create( Font( "Helvetica", 24 * 2 ), gl::TextureFont::Format().enableMipmapping() );
+
 	getWindow()->getSignalDisplayChange().connect( std::bind( &RetinaSampleApp::displayChange, this ) );
 }
 
 void RetinaSampleApp::mouseDrag( MouseEvent event )
 {
 	mPoints.push_back( event.getPos() );
-}
-
-void RetinaSampleApp::keyDown( KeyEvent event )
-{
-	if( event.getChar() == 'f' )
-		setFullScreen( ! isFullScreen() );
 }
 
 void RetinaSampleApp::displayChange()
@@ -61,14 +60,11 @@ void RetinaSampleApp::draw()
 	
 	gl::pushMatrices();
 		gl::color( 1.0f, 0.5f, 0.25f );
-		glLineWidth( getWindow()->toPixels( 1.0f ) );
-		gl::begin( GL_LINE_STRIP );
-		for( auto pointIter = mPoints.begin(); pointIter != mPoints.end(); ++pointIter ) {
-			gl::vertex( *pointIter );
-		}
-		gl::end();
+		glLineWidth( toPixels( 1.0f ) );
+		gl::draw( mPoints );
 	gl::popMatrices();
 	
+	// rotate a 200x200pt square at the center of the window
 	gl::pushMatrices();
 		gl::color( 1.0f, 0.2f, 0.15f );
 		gl::translate( getWindowCenter() );
@@ -76,9 +72,16 @@ void RetinaSampleApp::draw()
 		gl::drawSolidRect( Rectf( Area( -100, -100, 100, 100 ) ) );
 	gl::popMatrices();
 
-	// draw the logo in the lower-left corner
+	// draw the logo in the lower-left corner at 64x64 points
 	gl::color( Color::white() );
 	gl::draw( mLogo, Rectf( 0, getWindowHeight() - 64, 64, getWindowHeight() ) );
+
+	// The font has been made double the size we need for non-Retina, so we always scale by 0.5f
+	// another strategy would be to use two different TextureFonts
+	std::string s = ( getWindowContentScale() > 1 ) ? "Retina" : "Non-Retina";
+	float stringWidth = mFont->measureString( s ).x * 0.5f;
+	mFont->drawString( s, Vec2f( getWindowWidth() - stringWidth - 12, getWindowHeight() - 12 ),
+		gl::TextureFont::DrawOptions().scale( 0.5f ).pixelSnap( false ) );
 }
 
 CINDER_APP_NATIVE( RetinaSampleApp, RendererGl )
