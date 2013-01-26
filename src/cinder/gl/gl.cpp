8b50138aa28cfdb4c8888be053618908bea6334b
Andrew Bell <cinder.afb@gmail.com>
2013-01-26 01:55:08 -0500

    Removing glViewport call in setMatricesWindowPersp() since Viewport is no longer 1:1

--- src/cinder/gl/gl.cpp
+++ src/cinder/gl/gl.cpp
@@ -352,7 +352,6 @@ void setMatricesWindowPersp( int screenWidth, int screenHeight, float fovDegrees
 	if( originUpperLeft ) {
 		glScalef( 1.0f, -1.0f, 1.0f );           // invert Y axis so increasing Y goes down.
 		glTranslatef( 0.0f, (float)-screenHeight, 0.0f );       // shift origin up to upper-left corner.
-		glViewport( 0, 0, screenWidth, screenHeight );
 	}
 }
 
