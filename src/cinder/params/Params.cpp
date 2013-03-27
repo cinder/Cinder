175ed2c078fb7053589ec7e255fe5399d24a362f
Dale Stammen <stammen@gmail.com>
2013-03-27 13:39:38 -0700

    Added USE_DIRECTX define to prevent DirectX dependency when building for OpenGL

--- src/cinder/params/Params.cpp
+++ src/cinder/params/Params.cpp
@@ -23,7 +23,7 @@
 #include "cinder/app/App.h"
 #include "cinder/params/Params.h"
 
-#if defined( CINDER_MSW )
+#if (defined( CINDER_MSW ) && defined( USE_DIRECTX ))
 #include "cinder/dx/dx.h"
 #include "cinder/app/AppImplMswRendererDx.h"
 #endif
@@ -137,10 +137,10 @@ void TW_CALL implStdStringToClient( std::string& destinationClientString, const 
 class AntMgr {
   public:
 	AntMgr() {
-#if defined( CINDER_MSW )
-		app::Renderer::RendererType rendererType = app::App::get()->getRenderer()->getRendererType();
-		if( ! TwInit( (rendererType == app::Renderer::RENDERER_GL) ? TW_OPENGL : TW_DIRECT3D11, (rendererType == app::Renderer::RENDERER_GL) ? NULL : dx::getDxRenderer()->md3dDevice ) )
+#if defined( USE_DIRECTX )
+		if( ! TwInit( TW_DIRECT3D11, dx::getDxRenderer()->md3dDevice ) ) {
 			throw Exception();
+		}
 #else
 		if( ! TwInit( TW_OPENGL, NULL ) ) {
 			throw Exception();
