fa0e3a2783adb69352fa24093f77e65cfddfaa9c
Rick Barraza <rickbarraza@outlook.com>
2013-04-17 11:47:34 -0700

    updates to the VBO sample. Prep for public

--- samples/DirectX/_winrtVBO/src/basicApp.cpp
+++ samples/DirectX/_winrtVBO/src/basicApp.cpp
@@ -1,14 +1,10 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/ImageIo.h"
-#include "cinder/Rand.h"
 #include "cinder/Font.h"
 #include "cinder/dx/dx.h"
 #include "cinder/dx/DxVbo.h"
 #include "cinder/dx/DxTexture.h"
-
 #include "cinder/Camera.h"
-
-#define NEW_PARTICLES 5
 
 using namespace ci;
 using namespace ci::app;
@@ -34,22 +30,20 @@ void BasicApp::setup()
 	mFont = Font( "Arial", 24.0f );
 
 	int totalVertices = VERTICES_X * VERTICES_Z;
-	int totalQuadIndices = ( VERTICES_X - 1 ) * ( VERTICES_Z - 1 ) * 4;
+	int totalQuads = ( VERTICES_X - 1 ) * ( VERTICES_Z - 1);
 
 	cinder::dx::VboMesh::Layout layout;
 	layout.setStaticIndices();
 	layout.setDynamicPositions();
 	layout.setStaticTexCoords2d();
-	//layout.setStaticColorsRGBA();
-	//layout.setStaticNormals();
-
-	mVboMesh = dx::VboMesh( totalVertices, totalQuadIndices, layout, true );
+	mVboMesh = dx::VboMesh( totalVertices, totalQuads * 4, layout, true );
 
 	// buffer our static data - the texcoords and the indices
 	vector<uint32_t> indices;
 	vector<Vec2f> texCoords;
 	vector<Vec3f> normals;
 	vector<ColorA> colors;
+
 	for( int x = 0; x < VERTICES_X; ++x ) {
 		for( int z = 0; z < VERTICES_Z; ++z ) {
 			// create a quad for each vertex, except for along the bottom and right edges
@@ -59,8 +53,8 @@ void BasicApp::setup()
 				indices.push_back( (x+1) * VERTICES_Z + (z+1) );
 				indices.push_back( (x+0) * VERTICES_Z + (z+1) );
 			}
-			// the texture coordinates are mapped to [0,1.0)
-			texCoords.push_back( Vec2f( x / (float)VERTICES_X, z / (float)VERTICES_Z ) );
+			// the texture coordinates are mapped to [0.0, 1.0]
+			texCoords.push_back( Vec2f( 1.5*x / (float)(VERTICES_X + 0), (z+1) / (float)VERTICES_Z ) );
 			normals.push_back(Vec3f(0, 0, 0));
 			colors.push_back(ColorA(1, 1, 1, 0.2f));
 		}
@@ -68,35 +62,30 @@ void BasicApp::setup()
 
 	mVboMesh.bufferIndices( indices );
 	mVboMesh.bufferTexCoords2d( 0, texCoords );
-	//mVboMesh.bufferNormals(normals);
-	//mVboMesh.bufferColorsRGBA(colors);
 
 	// make a second VBO that uses the statics from the first
-	mVboMesh2 = dx::VboMesh( totalVertices, totalQuadIndices, mVboMesh.getLayout(), true );
+	mVboMesh2 = dx::VboMesh( totalVertices, totalQuads * 4, mVboMesh.getLayout(), true );
+	
 	mVboMesh2.setTexCoordOffset( 0, mVboMesh.getTexCoordOffset( 0 ) );
 	mVboMesh2.bufferIndices( indices );
 	mVboMesh2.bufferTexCoords2d( 0, texCoords );
-	//mVboMesh2.bufferNormals(normals);
-	//mVboMesh2.bufferColorsRGBA(colors);
 
 	mTexture = dx::Texture( loadImage( loadAsset( "testPattern.png" ) ) );
-
 }
 
 void BasicApp::update()
 {
-
 	dx::setMatrices( mCamera );
 
-	const float timeFreq = 5.0f;
-	const float zFreq = 3.0f;
-	const float xFreq = 7.0f;
+	const float timeFreq = 2.50f;
+	const float zFreq = 2.0f;
+	const float xFreq = 5.0f;
 	float offset = getElapsedSeconds() * timeFreq;
 
 	dx::VboMesh::VertexIter iter = mVboMesh.mapVertexBuffer();
 	for ( int x = 0; x < VERTICES_X; ++x ) {
 		for ( int z = 0; z < VERTICES_Z; ++z) {
-			float height = sin( z / (float)VERTICES_Z * zFreq + x / (float)VERTICES_X * xFreq + offset ) / 5.0f;
+			float height = sin( z / (float)VERTICES_Z * zFreq + x / (float)VERTICES_X * xFreq + offset ) / 4.0f;
 			iter.setPosition( Vec3f( x / (float)VERTICES_X, height, z / (float)VERTICES_Z ) );
 			++iter;
 		}
@@ -106,8 +95,8 @@ void BasicApp::update()
 	dx::VboMesh::VertexIter iter2 = mVboMesh2.mapVertexBuffer();
 	for( int x = 0; x < VERTICES_X; ++x ) {
 		for( int z = 0; z < VERTICES_Z; ++z ) {
-			float height = sin( z / (float)VERTICES_Z * zFreq * 2 + x / (float)VERTICES_X * xFreq * 3 + offset ) / 10.0f;
-			iter2.setPosition( Vec3f( x / (float)VERTICES_X, height, z / (float)VERTICES_Z ) + Vec3f( 0, 0.5, 0 ) );
+			float height = sin( z / (float)VERTICES_Z * zFreq * 2 + x / (float)VERTICES_X * xFreq * 2 + offset ) / 8.0f;
+			iter2.setPosition( Vec3f( x / (float)VERTICES_X, height, z / (float)VERTICES_Z ) + Vec3f( 0, 0.7, 0 ) );
 			++iter2;
 		}
 	}
@@ -115,17 +104,18 @@ void BasicApp::update()
 
 void BasicApp::draw()
 {
-	dx::clear( Color( 0, 0, 0 ), true );
-	dx::enableAdditiveBlending();
-
-	dx::scale( Vec3f(10, 10, 10 ) );
-	mTexture.bind();
-	//dx::draw( mVboMesh );
-	dx::draw( mVboMesh2 );
+	dx::clear( Color( 0.15f, 0.15f, 0.15f ), true );
+	dx::enableAlphaBlending();
 
 	std::stringstream s;
 	s << "Framerate:" << getAverageFps();
-	dx::drawString(s.str(),Vec2f(10.0f,10.0f),Color::white(),mFont);
+	dx::drawString(s.str(),Vec2f(100.0f,100.0f),Color::white(),mFont);
+
+	dx::scale( Vec3f(10, 10, 10 ) );
+	mTexture.bind();
+	dx::draw( mVboMesh );
+	dx::draw( mVboMesh2 );
+
 }
 
 // This line tells Cinder to actually create the application
