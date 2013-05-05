6403816195c12d80fed121df201d5c475ac5ee2d
Andrew Bell <cinder.afb@gmail.com>
2013-05-05 00:29:07 -0400

    Moving _winrtBasic to TextureRef

--- samples/DirectX/_winrtBasic/src/basicApp.cpp
+++ samples/DirectX/_winrtBasic/src/basicApp.cpp
@@ -15,7 +15,7 @@ class BasicApp : public AppBasic {
 	void			update();
 	void			draw();
 
-	dx::Texture		mTexture;
+	dx::TextureRef	mTexture;
 	Font			mFont;
 
 	Vec2f			mMousePosition;
@@ -26,7 +26,7 @@ class BasicApp : public AppBasic {
 void BasicApp::setup()
 {
 	mFont = Font( "Arial", 18  );
-	mTexture = dx::Texture( loadImage( loadAsset( "bkg.jpg" ) ) );
+	mTexture = dx::Texture::create( loadImage( loadAsset( "bkg.jpg" ) ) );
 	mMousePosition = getWindowCenter();
 	mFollowerPosition = getWindowCenter();
 }
@@ -56,7 +56,7 @@ void BasicApp::draw()
 {
 	// This clears the screen and set's it to a background color
 	dx::clear( Color( 0.0f, 0.0f, 0.0f ), true );
-	dx::color( Color::white());
+	dx::color( Color::white() );
 
 	// This is optional, but without it, the transparent background of the
 	// drawString() command will appear black.
@@ -66,37 +66,37 @@ void BasicApp::draw()
 
 	// although we cleared out the  background to a solid color, let's use the
 	// texture we loaded in setup ( bkg.jpg ) as a full screen background image
-	if ( mTexture )
+	if( mTexture )
 		dx::draw( mTexture, Rectf(getWindowBounds()) );
 
-	Vec2f radius(8.0f, 8.0f);
-	Vec2f smallerRadius(6.0f, 6.0f);
+	Vec2f radius( 8.0f, 8.0f );
+	Vec2f smallerRadius( 6.0f, 6.0f );
 
 	// lets draw some shapes to show the immediate position of any screen pointer
 	// we will draw some semi-transparent cross hairs...
 	dx::color( ColorA(1.0f, 1.0f, 1.0f, 0.3f) );
-	dx::drawLine( Vec2f( 0, mMousePosition.y), Vec2f(getWindowWidth(), mMousePosition.y ) );
-	dx::drawLine( Vec2f( mMousePosition.x, 0), Vec2f(mMousePosition.x, getWindowHeight() ) );
+	dx::drawLine( Vec2f( 0, mMousePosition.y ), Vec2f( getWindowWidth(), mMousePosition.y ) );
+	dx::drawLine( Vec2f( mMousePosition.x, 0 ), Vec2f( mMousePosition.x, getWindowHeight() ) );
 	// and a solid white targeting square...
-	dx::color( Color::white());
+	dx::color( Color::white() );
 	dx::drawSolidRect( Rectf( mMousePosition - radius, mMousePosition + radius) );
 	
 	// lets also illustrate the animation logic we added in update() and visualize the 
 	// follower as a square while it eases to the immediate pointer target
-	dx::color( Color(1.0f, 0.4f, 0.8f ) );
+	dx::color( Color( 1.0f, 0.4f, 0.8f ) );
 	dx::drawSolidRect( Rectf( mFollowerPosition - smallerRadius, mFollowerPosition + smallerRadius ) );
 
 	// lets explore drawing some additional, static geometries as well...
 	dx::color( ColorA( 0.0f, 1.0f, 1.0f, 0.5) );
 	dx::drawStrokedCircle( Vec2f( 40.0f, 90.0f ), 30.0f, 7 );
 	dx::drawSolidCircle( Vec2f(   40.0f, 90.0f ), 20.0f, 7 );
-	dx::drawSolidTriangle( Vec2f( 40.0f, 144.0f ), Vec2f( 14.0f, 196.0f), Vec2f(66.0f, 196.0f)); 
+	dx::drawSolidTriangle( Vec2f( 40.0f, 144.0f ), Vec2f( 14.0f, 196.0f), Vec2f(66.0f, 196.0f) ); 
 	dx::drawStrokedRect( Rectf(10.0f, 140.0f, 70.0f, 200.0f ) );
 
 	// here is a simple way of drawing basic messages on the screen
 	std::stringstream s;
 	s << "Framerate:" << getAverageFps();
-	dx::drawString(s.str(),Vec2f(10.0f,10.0f),Color(1.0f, 1.0f, 1.0f) ,mFont);
+	dx::drawString( s.str(), Vec2f(10.0f,10.0f), Color(1.0f, 1.0f, 1.0f), mFont );
 }
 
 // This line tells Cinder to actually create the application and use the DirectX required
