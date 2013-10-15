1c324dd522f202385c52f063b540a3ed0a40c5b3
sqrtle <sqrtle@github.fake>
2013-10-15 18:33:01 +0100

    Corrected element indices in comment

--- include/cinder/Matrix44.h
+++ include/cinder/Matrix44.h
@@ -51,10 +51,10 @@ public:
 	//
 	// This class is OpenGL friendly and stores the m as how OpenGL would expect it.
 	// m[i,j]:
-	// | m[0,0] m[0,1] m[0,2] m[0,2] |
-	// | m[1,0] m[1,1] m[1,2] m[1,2] |
-	// | m[2,0] m[2,1] m[2,2] m[2,2] |
-	// | m[3,0] m[3,1] m[3,2] m[3,2] |
+	// | m[0,0] m[0,1] m[0,2] m[0,3] |
+	// | m[1,0] m[1,1] m[1,2] m[1,3] |
+	// | m[2,0] m[2,1] m[2,2] m[2,3] |
+	// | m[3,0] m[3,1] m[3,2] m[3,3] |
 	//
 	// m[idx]
 	// | m[ 0] m[ 4] m[ 8] m[12] |
