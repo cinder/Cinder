84930fd68b6ae1695d3bc67d15e0c75ad36d2fce
Andrew Bell <cinder.afb@gmail.com>
2014-08-09 21:39:52 -0400

    Updating FlickrTest(Multithreaded), eventTest, FBOBasic samples

--- samples/FBOBasic/src/FBOBasicApp.cpp
+++ samples/FBOBasic/src/FBOBasicApp.cpp
@@ -1,6 +1,11 @@
 #include "cinder/Cinder.h"
 #include "cinder/app/AppNative.h"
+#include "cinder/app/RendererGl.h"
 #include "cinder/Camera.h"
+
+#include "cinder/gl/Context.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/gl/GlslProg.h"
 #include "cinder/gl/Fbo.h"
 
 using namespace ci;
@@ -17,7 +22,7 @@ class FBOBasicApp : public AppNative {
 
 	void			renderSceneToFbo();
 	
-	gl::Fbo				mFbo;
+	gl::FboRef			mFbo;
 	Matrix44f			mTorusRotation;
 	static const int	FBO_WIDTH = 256, FBO_HEIGHT = 256;
 };
@@ -25,8 +30,8 @@ class FBOBasicApp : public AppNative {
 void FBOBasicApp::setup()
 {
 	gl::Fbo::Format format;
-//	format.setSamples( 4 ); // uncomment this to enable 4x antialiasing
-	mFbo = gl::Fbo( FBO_WIDTH, FBO_HEIGHT, format );
+	format.setSamples( 4 ); // uncomment this to enable 4x antialiasing, though it will break the depth texture on iOS
+	mFbo = gl::Fbo::create( FBO_WIDTH, FBO_HEIGHT, format.depthTexture() );
 
 	gl::enableDepthRead();
 	gl::enableDepthWrite();	
@@ -38,33 +43,30 @@ void FBOBasicApp::setup()
 void FBOBasicApp::renderSceneToFbo()
 {
 	// this will restore the old framebuffer binding when we leave this function
-	// on non-OpenGL ES platforms, you can just call mFbo.unbindFramebuffer() at the end of the function
+	// on non-OpenGL ES platforms, you can just call mFbo->unbindFramebuffer() at the end of the function
 	// but this will restore the "screen" FBO on OpenGL ES, and does the right thing on both platforms
-	gl::SaveFramebufferBinding bindingSaver;
-	
-	// bind the framebuffer - now everything we draw will go there
-	mFbo.bindFramebuffer();
+	gl::ScopedFramebuffer fbScp( mFbo );
+	// clear out the FBO with blue
+	gl::clear( Color( 0.25, 0.5f, 1.0f ) );
 
 	// setup the viewport to match the dimensions of the FBO
-	gl::setViewport( mFbo.getBounds() );
+	gl::viewport( 0, 0, mFbo->getWidth(), mFbo->getHeight() );
 
 	// setup our camera to render the torus scene
-	CameraPersp cam( mFbo.getWidth(), mFbo.getHeight(), 60.0f );
-	cam.setPerspective( 60, mFbo.getAspectRatio(), 1, 1000 );
+	CameraPersp cam( mFbo->getWidth(), mFbo->getHeight(), 60.0f );
+	cam.setPerspective( 60, mFbo->getAspectRatio(), 1, 1000 );
 	cam.lookAt( Vec3f( 2.8f, 1.8f, -2.8f ), Vec3f::zero() );
 	gl::setMatrices( cam );
 
 	// set the modelview matrix to reflect our current rotation
-	gl::multModelView( mTorusRotation );
+	gl::setModelMatrix( mTorusRotation );
 	
-	// clear out the FBO with blue
-	gl::clear( Color( 0.25, 0.5f, 1.0f ) );
-
 	// render an orange torus, with no textures
-	glDisable( GL_TEXTURE_2D );
+	gl::ScopedGlslProg shaderScp( gl::getStockShader( gl::ShaderDef().color() ) );
 	gl::color( Color( 1.0f, 0.5f, 0.25f ) );
-	gl::drawTorus( 1.4f, 0.3f, 32, 64 );
-//	gl::drawColorCube( Vec3f::zero(), Vec3f( 2.2f, 2.2f, 2.2f ) );
+//	gl::drawTorus( 1.4f, 0.3f, 32, 64 );
+	gl::drawCube( Vec3f::zero(), Vec3f( 2.2f, 2.2f, 2.2f ) );
+	gl::color( Color::white() );
 }
 
 void FBOBasicApp::update()
@@ -81,30 +83,31 @@ void FBOBasicApp::draw()
 	// clear the window to gray
 	gl::clear( Color( 0.35f, 0.35f, 0.35f ) );
 
+	// set the viewport to match our window
+	gl::viewport( Vec2f::zero(), toPixels( getWindowSize() ) );
+
 	// setup our camera to render the cube
 	CameraPersp cam( getWindowWidth(), getWindowHeight(), 60.0f );
 	cam.setPerspective( 60, getWindowAspectRatio(), 1, 1000 );
 	cam.lookAt( Vec3f( 2.6f, 1.6f, -2.6f ), Vec3f::zero() );
 	gl::setMatrices( cam );
 
-	// set the viewport to match our window
-	gl::setViewport( getWindowBounds() );
-
 	// use the scene we rendered into the FBO as a texture
-	glEnable( GL_TEXTURE_2D );
-	mFbo.bindTexture();
+	mFbo->bindTexture();
 
 	// draw a cube textured with the FBO
-	gl::color( Color::white() );
-	gl::drawCube( Vec3f::zero(), Vec3f( 2.2f, 2.2f, 2.2f ) );
+	{
+		gl::ScopedGlslProg shaderScp( gl::getStockShader( gl::ShaderDef().texture() ) );
+		gl::drawCube( Vec3f::zero(), Vec3f( 2.2f, 2.2f, 2.2f ) );
+	}
 
 	// show the FBO texture in the upper left corner
-	gl::setMatricesWindow( getWindowSize() );
-	gl::draw( mFbo.getTexture(0), Rectf( 0, 0, 96, 96 ) );
+	gl::setMatricesWindow( toPixels( getWindowSize() ) );
+	gl::draw( mFbo->getColorTexture(), Rectf( 0, 0, 128, 128 ) );
 	
-#if ! defined( CINDER_GLES ) // OpenGL ES can't do depth textures, otherwise draw the FBO's
-	gl::draw( mFbo.getDepthTexture(), Rectf( 96, 0, 96 + 96, 96 ) );
-#endif
+	if( mFbo->getDepthTexture() ) // NULL if we have multisampling
+		gl::draw( mFbo->getDepthTexture(), Rectf( 128, 0, 128 + 128, 128 ) );
 }
 
-CINDER_APP_NATIVE( FBOBasicApp, RendererGl )
+auto renderOptions = RendererGl::Options().coreProfile( false ).version( 4, 1 );
+CINDER_APP_NATIVE( FBOBasicApp, RendererGl( renderOptions ) )
--- samples/FlickrTestMultithreaded/src/FlickrTestMultithreadedApp.cpp
+++ samples/FlickrTestMultithreaded/src/FlickrTestMultithreadedApp.cpp
@@ -1,66 +1,74 @@
-#include "cinder/app/AppBasic.h"
-#include "cinder/gl/Texture.h"
+#include "cinder/app/AppNative.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/Context.h"
 #include "cinder/Xml.h"
 #include "cinder/Timeline.h"
 #include "cinder/ImageIo.h"
 #include "cinder/Thread.h"
 #include "cinder/ConcurrentCircularBuffer.h"
 
+/*	The heart of this sample is to show how to create a background thread that interacts with OpenGL.
+	It launches a secondary thread which pulls down images from a Flickr group and puts them in a thread-safe buffer.
+	We can allocate a gl::Context which shares resources (specifically Textures) with the primary gl::Context.
+	Note that this Context should be created in the primary thread and passed as a parameter to the secondary thread.
+*/
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-class FlickrTestMTApp : public AppBasic {
- public:		
+class FlickrTestMTApp : public AppNative {
+  public:		
 	void setup();
 	void update();
 	void draw();
 	void shutdown();
+	void loadImagesThreadFn( gl::ContextRef sharedGlContext );
 
-	void loadImagesThreadFn();
-
-	ConcurrentCircularBuffer<Surface>	*mSurfaces;
+	ConcurrentCircularBuffer<gl::TextureRef>	*mImages;
 
 	bool					mShouldQuit;
 	shared_ptr<thread>		mThread;
-	gl::Texture				mTexture, mLastTexture;
+	gl::TextureRef			mTexture, mLastTexture;
 	Anim<float>				mFade;
 	double					mLastTime;
 };
 
-
 void FlickrTestMTApp::setup()
 {
 	mShouldQuit = false;
-	mSurfaces = new ConcurrentCircularBuffer<Surface>( 5 ); // room for 5 images
-	// create and launch the thread
-	mThread = shared_ptr<thread>( new thread( bind( &FlickrTestMTApp::loadImagesThreadFn, this ) ) );
-	mLastTime = getElapsedSeconds();
+	mImages = new ConcurrentCircularBuffer<gl::TextureRef>( 5 ); // room for 5 images
+	// create and launch the thread with a new gl::Context just for that thread
+	gl::ContextRef backgroundCtx = gl::Context::create( gl::context() );
+	mThread = shared_ptr<thread>( new thread( bind( &FlickrTestMTApp::loadImagesThreadFn, this, backgroundCtx ) ) );
+	mLastTime = getElapsedSeconds() - 10; // force an initial update by make it "ten seconds ago"
+
+	gl::enableAlphaBlending();
 }
 
-void FlickrTestMTApp::loadImagesThreadFn()
+void FlickrTestMTApp::loadImagesThreadFn( gl::ContextRef context )
 {
 	ci::ThreadSetup threadSetup; // instantiate this if you're talking to Cinder from a secondary thread
+	// we received as a parameter a gl::Context we can use safely that shares resources with the primary Context
+	context->makeCurrent();
 	vector<Url>	urls;
 
 	// parse the image URLS from the XML feed and push them into 'urls'
 	const Url sunFlickrGroup = Url( "http://api.flickr.com/services/feeds/groups_pool.gne?id=52242317293@N01&format=rss_200" );
 	const XmlTree xml( loadUrl( sunFlickrGroup ) );
-	for( XmlTree::ConstIter item = xml.begin( "rss/channel/item" ); item != xml.end(); ++item ) {
+	for( auto item = xml.begin( "rss/channel/item" ); item != xml.end(); ++item ) {
 		const XmlTree &urlXml = ( ( *item / "media:content" ) );
 		urls.push_back( Url( urlXml["url"] ) );
 	}
 
-	// load images as Surfaces into our ConcurrentCircularBuffer
+	// load images as Textures into our ConcurrentCircularBuffer
 	// don't create gl::Textures on a background thread
 	while( ( ! mShouldQuit ) && ( ! urls.empty() ) ) {
 		try {
-			console() << "Loading: " << urls.back() << std::endl;
-			mSurfaces->pushFront( loadImage( loadUrl( urls.back() ) ) );
+			mImages->pushFront( gl::Texture::create( loadImage( loadUrl( urls.back() ) ) ) );
 			urls.pop_back();
 		}
-		catch( ... ) {
-			// just ignore any exceptions
+		catch( ... ) { // just ignore any exceptions
 		}
 	}
 }
@@ -68,12 +76,10 @@ void FlickrTestMTApp::loadImagesThreadFn()
 void FlickrTestMTApp::update()
 {
 	double timeSinceLastImage = getElapsedSeconds() - mLastTime;
-	if( ( timeSinceLastImage > 2.5 ) && mSurfaces->isNotEmpty() ) {
+	if( ( timeSinceLastImage > 2.5 ) && mImages->isNotEmpty() ) {
 		mLastTexture = mTexture; // the "last" texture is now the current text
 		
-		Surface newSurface;
-		mSurfaces->popBack( &newSurface );
-		mTexture = gl::Texture( newSurface );
+		mImages->popBack( &mTexture );
 		
 		mLastTime = getElapsedSeconds();
 		// blend from 0 to 1 over 1.5sec
@@ -82,20 +88,18 @@ void FlickrTestMTApp::update()
 }
 
 void FlickrTestMTApp::draw()
-{	
-	gl::enableAlphaBlending();
-	gl::clear();
-	gl::color( Color::white() );
-	
+{
+	gl::clear( Color( 0.1f, 0.1f, 0.2f ) );
+
 	if( mLastTexture ) {
 		gl::color( 1, 1, 1, 1.0f - mFade );
-		Rectf textureBounds = mLastTexture.getBounds();
+		Rectf textureBounds = mLastTexture->getBounds();
 		Rectf drawBounds = textureBounds.getCenteredFit( getWindowBounds(), true );
 		gl::draw( mLastTexture, drawBounds );
 	}
 	if( mTexture ) {
 		gl::color( 1, 1, 1, mFade );
-		Rectf textureBounds = mTexture.getBounds();
+		Rectf textureBounds = mTexture->getBounds();
 		Rectf drawBounds = textureBounds.getCenteredFit( getWindowBounds(), true );
 		gl::draw( mTexture, drawBounds );
 	}
@@ -104,8 +108,8 @@ void FlickrTestMTApp::draw()
 void FlickrTestMTApp::shutdown()
 {
 	mShouldQuit = true;
-	mSurfaces->cancel();
+	mImages->cancel();
 	mThread->join();
 }
 
-CINDER_APP_BASIC( FlickrTestMTApp, RendererGl )
+CINDER_APP_NATIVE( FlickrTestMTApp, RendererGl )
