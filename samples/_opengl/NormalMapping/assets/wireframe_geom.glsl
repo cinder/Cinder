2dbeada2eb27fb9d76034cc7505c4a89d809524a
Rich E <rich.eakin@gmail.com>
2014-08-19 21:45:52 +0200

    updated NormalMapping geometry shader for core profile

--- samples/_opengl/NormalMapping/assets/wireframe_geom.glsl
+++ samples/_opengl/NormalMapping/assets/wireframe_geom.glsl
@@ -1,5 +1,4 @@
 #version 150
-#extension GL_EXT_geometry_shader4 : enable
 
 // we expect the model to be made of triangles (GL_TRIANGLES)
 layout( triangles ) in;
@@ -11,29 +10,29 @@ noperspective out vec3 vDistance;
 // we need to know the size of the viewport in pixels (x = width, y = height)
 uniform vec2 uViewportSize;
 
-void main(void)
+void main()
 {
-// taken from 'Single-Pass Wireframe Rendering'
-vec2 p0 = uViewportSize * gl_PositionIn[0].xy/gl_PositionIn[0].w;
-vec2 p1 = uViewportSize * gl_PositionIn[1].xy/gl_PositionIn[1].w;
-vec2 p2 = uViewportSize * gl_PositionIn[2].xy/gl_PositionIn[2].w;
+	// taken from 'Single-Pass Wireframe Rendering'
+	vec2 p0 = uViewportSize * gl_in[0].gl_Position.xy / gl_in[0].gl_Position.w;
+	vec2 p1 = uViewportSize * gl_in[1].gl_Position.xy / gl_in[1].gl_Position.w;
+	vec2 p2 = uViewportSize * gl_in[2].gl_Position.xy / gl_in[2].gl_Position.w;
 
-vec2 v0 = p2-p1;
-vec2 v1 = p2-p0;
-vec2 v2 = p1-p0;
-float fArea = abs(v1.x*v2.y - v1.y * v2.x);
+	vec2 v0 = p2 - p1;
+	vec2 v1 = p2 - p0;
+	vec2 v2 = p1 - p0;
+	float area = abs( v1.x * v2.y - v1.y * v2.x );
 
-vDistance = vec3(fArea/length(v0),0,0);
-gl_Position = gl_PositionIn[0];
-EmitVertex();
+	vDistance = vec3( area / length( v0 ), 0,0 );
+	gl_Position = gl_in[1].gl_Position;
+	EmitVertex();
 
-vDistance = vec3(0,fArea/length(v1),0);
-gl_Position = gl_PositionIn[1];
-EmitVertex();
+	vDistance = vec3( 0, area / length( v1 ), 0 );
+	gl_Position = gl_in[1].gl_Position;
+	EmitVertex();
 
-vDistance = vec3(0,0,fArea/length(v2));
-gl_Position = gl_PositionIn[2];
-EmitVertex();
-
-EndPrimitive();
+	vDistance = vec3( 0, 0, area / length( v2 ) );
+	gl_Position = gl_in[2].gl_Position;
+	EmitVertex();
+	
+	EndPrimitive();
 }
