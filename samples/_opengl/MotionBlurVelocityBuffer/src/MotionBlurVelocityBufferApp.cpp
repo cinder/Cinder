2a6e700a1a449320923467670c5cb4515b28f941
David Wicks <wicks.db@gmail.com>
2014-08-22 13:36:33 -0400

    Displaying background image for velocity buffer blur sample.
    Actual velocity buffer blurring coming soon.

--- samples/_opengl/MotionBlurVelocityBuffer/src/MotionBlurVelocityBufferApp.cpp
+++ samples/_opengl/MotionBlurVelocityBuffer/src/MotionBlurVelocityBufferApp.cpp
@@ -1,6 +1,7 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -8,14 +9,26 @@ using namespace std;
 
 class MotionBlurVelocityBufferApp : public AppNative {
   public:
+	void prepareSettings( Settings *settings );
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+  private:
+	gl::TextureRef	mBackground;
 };
+
+void MotionBlurVelocityBufferApp::prepareSettings( Settings *settings )
+{
+	settings->setWindowSize( 1280, 720 );
+#if defined( CINDER_COCOA_TOUCH ) || defined( CINDER_COCOA_TOUCH_SIMULATOR )
+  getSignalSupportedOrientations().connect( [] { return InterfaceOrientation::LandscapeAll; } );
+#endif // COCOA_TOUCH
+}
 
 void MotionBlurVelocityBufferApp::setup()
 {
+	mBackground = gl::Texture::create( loadImage( loadAsset( "background.jpg" ) ) );
 }
 
 void MotionBlurVelocityBufferApp::mouseDown( MouseEvent event )
@@ -28,7 +41,8 @@ void MotionBlurVelocityBufferApp::update()
 
 void MotionBlurVelocityBufferApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 0, 0 ) );
+	gl::draw( mBackground, getWindowBounds() );
 }
 
 CINDER_APP_NATIVE( MotionBlurVelocityBufferApp, RendererGl )
