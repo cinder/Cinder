80cad24aca31947ebf728e74a1ea3d8b3afe9551
num3ric <eric.renaud.houde@gmail.com>
2014-09-03 10:18:32 -0400

    Updated Picking3D sample to glNext-glm.

--- samples/Picking3D/src/Picking3DApp.cpp
+++ samples/Picking3D/src/Picking3DApp.cpp
@@ -14,7 +14,8 @@
  POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "cinder/app/AppBasic.h"
+#include "cinder/app/AppNative.h"
+#include "cinder/app/RendererGl.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/gl/GlslProg.h"
 #include "cinder/ImageIo.h"
@@ -36,17 +37,26 @@ typedef boost::shared_ptr<class Bubble> BubbleRef;
 class Bubble
 {
   public:
-	Bubble(const vec3 &origin) 
-		:	age(0.0), 
-			lifespan(3.0),
-			position(origin),
-			speed( vec3::zero() )
-		{};
+	Bubble( const vec3 &origin )
+		:	age( 0.0 ),
+			lifespan( 3.0 ),
+			position( origin ),
+			speed( vec3( 0 ) )
+		{ };
 
-	void update(double elapsed);
-	void draw();
+	void update( double elapsed ) {
+		age += elapsed;
+		speed += 0.1f * vec3( Rand::randFloat() -0.5f, 1.0f, Rand::randFloat() -0.5f );
+		position += speed * (float) elapsed;
+	}
+	
+	void draw() const {
+		gl::drawSphere( position, 0.05f, 8 );
+	}
 
-	bool isDead();
+	bool isDead() const {
+		return age >= lifespan;
+	}
   protected:
 	double age;
 	double lifespan;
@@ -55,128 +65,88 @@ class Bubble
 	vec3  position;
 };
 
-void Bubble::update(double elapsed)
-{
-	age += elapsed;
-	speed += 0.1f * vec3(Rand::randFloat() -0.5f, 1.0f, Rand::randFloat() -0.5f);
-	position += speed * (float) elapsed;
-}
-
-void Bubble::draw()
-{
-	gl::drawSphere(position, 0.05f, 8);
-}
-
-bool Bubble::isDead(void)
-{
-	return (age >= lifespan);
-}
-
-
 // our main application
 class Picking3DApp : public AppBasic 
 {
   public:
-	void setup();
-	void update();
-	void draw();
-
-	void drawGrid(float size=100.0f, float step=10.0f);
-		
-	bool performPicking( vec3 *pickedPoint, vec3 *pickedNormal );
-		
-	void mouseMove( MouseEvent event );
-	void mouseDown( MouseEvent event );
-	void mouseDrag( MouseEvent event );
-	void resize();
-
+	void setup() override;
+	void update() override;
+	void draw() override;
+	
+	void mouseMove( MouseEvent event ) override;
+	void mouseDown( MouseEvent event ) override;
+	void mouseDrag( MouseEvent event ) override;
+	void resize() override;
   protected:
-	// shader and texture for our model
-	gl::GlslProg	mShader;
-	gl::Texture		mTexture;
+	void drawGrid(float size=100.0f, float step=10.0f);
+	bool performPicking( vec3 *pickedPoint, vec3 *pickedNormal );
+	
+	
+	gl::GlslProgRef		mShader;		// shader for our model
+	gl::Texture2dRef	mTexture;		// texture for our model
+	TriMeshRef			mMesh;			// the model of a rubber duckys
+	AxisAlignedBox3f	mObjectBounds; 	// the object space bounding box of the mesh
+	mat4				mTransform;		// transformations (translate, rotate, scale) of the model
+	MayaCamUI			mMayaCam;		// our camera
+	ivec2				mMousePos;		// keep track of the mouse
+	double				mTime;			// keep track of time
 
-	// the model of a rubber ducky
-	TriMesh		mMesh;
-
-	// the object space bounding box of the mesh
-	AxisAlignedBox3f	mObjectBounds;
-
-	// transformations (translate, rotate, scale) of the model
-	mat4	mTransform;
-
-	// our camera
-	MayaCamUI	mMayaCam;
-
-	// keep track of the mouse
-	ivec2		mMousePos;
-
-	// keep track of time
-	double		mTime;
-
-	// particles
-	vector<BubbleRef>	mBubbles;
+	vector<BubbleRef>	mBubbles;		// particles
 };
 
 void Picking3DApp::setup()
 {
 	// initialize stuff
 	mTime = getElapsedSeconds();
-	mTransform.setToIdentity();
 
 	// load and compile the shader
-	//  (note: a shader is not required, but gives a much better visual appearance.
-	//	See for yourself by disabling the 'mShader.bind()' call in the draw method.)
-	mShader = gl::GlslProg( loadResource( RES_SHADER_VERT ), loadResource( RES_SHADER_FRAG ) );
+	mShader = gl::GlslProg::create( loadResource( RES_SHADER_VERT ), loadResource( RES_SHADER_FRAG ) );
 
 	// load the texture
-	//  (note: enable mip-mapping by default)
-	gl::Texture::Format format;
-	format.enableMipmapping(true);			
-	ImageSourceRef img = loadImage( loadResource( RES_DUCKY_TEX ) );
-	if(img) mTexture = gl::Texture( img, format );
-
+	mTexture = gl::Texture::create( loadImage( loadResource( RES_DUCKY_TEX ) ), gl::Texture::Format().loadTopDown( true ).mipmap() );
 
 	// load the mesh 
 	//  (note: the mesh was created from an OBJ file
 	//  using the ObjLoader class. The data was then saved using the
 	//  TriMesh::write() method. Reading binary files is much quicker.)
-	mMesh.read( loadResource( RES_DUCKY_MESH ) );	
+	mMesh = TriMesh::create();
+	mMesh->read( loadResource( RES_DUCKY_MESH ) );
 
 	// get the object space bounding box of the model, for fast intersection testing
-	mObjectBounds = mMesh.calcBoundingBox();
+	mObjectBounds = mMesh->calcBoundingBox();
 
 	// set up the camera
 	CameraPersp cam;
-	cam.setEyePoint( vec3(5.0f, 10.0f, 10.0f) );
-	cam.setCenterOfInterestPoint( vec3(0.0f, 2.5f, 0.0f) );
+	cam.setEyePoint( vec3( 5.0f, 10.0f, 10.0f ) );
+	cam.setCenterOfInterestPoint( vec3( 0.0f, 2.5f, 0.0f ) );
 	cam.setPerspective( 60.0f, getWindowAspectRatio(), 1.0f, 1000.0f );
 	mMayaCam.setCurrentCam( cam );
+	
+	// enable the depth buffer (after all, we are doing 3D)
+	gl::enableDepthRead();
+	gl::enableDepthWrite();
 }
 
-void Picking3DApp::update(void)
+void Picking3DApp::update()
 {
 	// calculate elapsed time
 	double elapsed = getElapsedSeconds() - mTime;
 	mTime = getElapsedSeconds();
 
 	// animate our little ducky
-	mTransform.setToIdentity();
-	mTransform.rotate( vec3::xAxis(), sinf( (float) getElapsedSeconds() * 3.0f ) * 0.08f );
-	mTransform.rotate( vec3::yAxis(), (float) getElapsedSeconds() * 0.1f );
-	mTransform.rotate( vec3::zAxis(), sinf( (float) getElapsedSeconds() * 4.3f ) * 0.09f );
+	mTransform = mat4( 1.0 );
+	mTransform *= rotate( sin( (float) getElapsedSeconds() * 3.0f ) * 0.08f, vec3( 1, 0, 0 ) );
+	mTransform *= rotate( (float) getElapsedSeconds() * 0.1f, vec3( 0, 1, 0 ) );
+	mTransform *= rotate( sin( (float) getElapsedSeconds() * 4.3f ) * 0.09f, vec3( 0, 0, 1 ) );
 
 	// animate our bubbles
-	vector<BubbleRef>::iterator itr;
-	for(itr = mBubbles.begin();itr!=mBubbles.end();)
-	{
-		(*itr)->update(elapsed);
-
-		// remove dead bubbles
-		if( (*itr)->isDead() )
-			itr = mBubbles.erase(itr);
-		else
-			++itr;
+	for( auto& bubble : mBubbles ) {
+		bubble->update( elapsed );
 	}
+	
+	// remove dead bubbles
+	auto newEnd = std::remove_if( mBubbles.begin(), mBubbles.end(), []( const BubbleRef& b ) -> bool { return b->isDead(); } );
+	mBubbles.erase( newEnd, mBubbles.end() );
 }
 
 void Picking3DApp::draw()
@@ -185,40 +155,29 @@ void Picking3DApp::draw()
 	gl::clear( Colorf(0.5f, 0.5f, 0.5f) );
 
 	// set up the camera 
-	gl::pushMatrices();
+	gl::ScopedMatrices push;
 	gl::setMatrices( mMayaCam.getCamera() );
-
-	// enable the depth buffer (after all, we are doing 3D)
-	gl::enableDepthRead();
-	gl::enableDepthWrite();
 
 	// draw the grid on the floor
 	drawGrid();
-
-	// bind the texture
-	mTexture.enableAndBind();
-
-	// bind the shader and tell it to use our texture
-	mShader.bind();
-	mShader.uniform("tex0", 0);
-
-	// draw the mesh 
-	//  (note: reset current color to white so the actual texture colors show up)
-	gl::color( Color::white() );
-	//  (note: apply transformations to the model)
-	gl::pushModelView();
-		gl::multModelView( mTransform );
-		gl::draw( mMesh );
-	gl::popModelView();
-
-	// unbind the shader and texture
-	mShader.unbind();
-	mTexture.unbind();
+	
+	{
+		gl::ScopedModelMatrix model;
+		gl::ScopedTextureBind tex0( mTexture );
+		gl::ScopedGlslProg prog( mShader );
+		mShader->uniform( "tex0", 0 );
+		//  (note: reset current color to white so the actual texture colors show up)
+		gl::color( Color::white() );
+		//  (note: apply transformations to the model)
+		gl::multModelMatrix( mTransform );
+		// Draw the trimesh
+		gl::draw( *mMesh );
+	}
 
 	// perform 3D picking now, so we can draw the intersection as a sphere
 	vec3 pickedPoint, pickedNormal;
 	if( performPicking( &pickedPoint, &pickedNormal ) ) {
-		gl::color( Color(0, 1, 0) );
+		gl::color( Color( 0, 1, 0 ) );
 		// draw an arrow to the picked point along its normal
 		gl::drawVector( pickedPoint + pickedNormal, pickedPoint );
 
@@ -227,25 +186,25 @@ void Picking3DApp::draw()
 	}
 
 	// draw bubbles 
-	gl::color( Colorf(0.1f, 0.2f, 0.25f) );
+	gl::color( Colorf( 0.1f, 0.2f, 0.25f ) );
 	gl::enableAdditiveBlending();
-
-	vector<BubbleRef>::iterator itr;
-	for(itr = mBubbles.begin();itr!=mBubbles.end();++itr)
-		(*itr)->draw();
-
+	for( const auto& bubble : mBubbles ) {
+		bubble->draw();
+	}
 	gl::disableAlphaBlending();
-
-	gl::popMatrices();
 }
 
 void Picking3DApp::drawGrid(float size, float step)
 {
-	gl::color( Colorf(0.2f, 0.2f, 0.2f) );
-	for(float i=-size;i<=size;i+=step) {
-		gl::drawLine( vec3(i, 0.0f, -size), vec3(i, 0.0f, size) );
-		gl::drawLine( vec3(-size, 0.0f, i), vec3(size, 0.0f, i) );
+	gl::color( Colorf( 0.2f, 0.2f, 0.2f ) );
+	gl::begin( GL_LINES );
+	for( float i=-size; i <= size; i += step ) {
+		gl::vertex( vec3( i, 0.0f, -size ) );
+		gl::vertex( vec3( i, 0.0f, size ) );
+		gl::vertex( vec3( -size, 0.0f, i ) );
+		gl::vertex( vec3( size, 0.0f, i ) );
 	}
+	gl::end();
 }
 
 bool Picking3DApp::performPicking( vec3 *pickedPoint, vec3 *pickedNormal )
@@ -262,56 +221,55 @@ bool Picking3DApp::performPicking( vec3 *pickedPoint, vec3 *pickedNormal )
 
 	// draw the object space bounding box in yellow
 	gl::color( Color(1, 1, 0) );
-	gl::drawStrokedCube(mObjectBounds);
+	gl::drawStrokedCube( mObjectBounds );
 
 	// the coordinates of the bounding box are in object space, not world space,
 	// so if the model was translated, rotated or scaled, the bounding box would not
 	// reflect that. 
 	//
 	// One solution would be to pass the transformation to the calcBoundingBox() function: 
-	AxisAlignedBox3f worldBoundsExact = mMesh.calcBoundingBox(mTransform);		// slow
+	AxisAlignedBox3f worldBoundsExact = mMesh->calcBoundingBox( mTransform ); // slow
 
 	// draw this transformed box in orange
-	gl::color( Color(1, 0.5, 0) );
-	gl::drawStrokedCube(worldBoundsExact);
+	gl::color( Color( 1, 0.5, 0 ) );
+	gl::drawStrokedCube( worldBoundsExact );
 	
 	// But if you already have an object space bounding box, it's much faster to
 	// approximate the world space bounding box like this:
-	AxisAlignedBox3f worldBoundsApprox = mObjectBounds.transformed(mTransform);	// fast
+	AxisAlignedBox3f worldBoundsApprox = mObjectBounds.transformed( mTransform ); // fast
 
 	// draw this transformed box in cyan
-	gl::color( Color(0, 1, 1) );
-	gl::drawStrokedCube(worldBoundsApprox);
+	gl::color( Color( 0, 1, 1 ) );
+	gl::drawStrokedCube( worldBoundsApprox );
 
 	// fast detection first - test against the bounding box itself
-	if( ! worldBoundsExact.intersects(ray) )
+	if( ! worldBoundsExact.intersects( ray ) )
 		return false;
 
 	// set initial distance to something far, far away
 	float result = 1.0e6f;
 
 	// traverse triangle list and find the picked triangle
-	size_t polycount = mMesh.getNumTriangles();
+	size_t polycount = mMesh->getNumTriangles();
 	float distance = 0.0f;
-	for(size_t i=0;i<polycount;++i)
-	{
+	for( size_t i=0; i<polycount; ++i ) {
 		vec3 v0, v1, v2;
 		// get a single triangle from the mesh
-		mMesh.getTriangleVertices(i, &v0, &v1, &v2);
+		mMesh->getTriangleVertices( i, &v0, &v1, &v2 );
 
 		// transform triangle to world space
-		v0 = mTransform.transformPointAffine(v0);
-		v1 = mTransform.transformPointAffine(v1);
-		v2 = mTransform.transformPointAffine(v2);
+		v0 = vec3( mTransform * vec4( v0, 1.0 ) );
+		v1 = vec3( mTransform * vec4( v1, 1.0 ) );
+		v2 = vec3( mTransform * vec4( v2, 1.0 ) );
 
 		// test to see if the ray intersects with this triangle
-		if( ray.calcTriangleIntersection(v0, v1, v2, &distance) ) {
+		if( ray.calcTriangleIntersection( v0, v1, v2, &distance ) ) {
 			// set our result to this if its closer than any intersection we've had so far
 			if( distance < result ) {
 				result = distance;
 				// assuming this is the closest triangle, we'll set our normal
 				// while we've got all the points handy
-				*pickedNormal = ( v1 - v0 ).cross( v2 - v0 ).normalized();
+				*pickedNormal = normalize( cross( v1 - v0, v2 - v0 ) );
 			}
 		}
 	}
