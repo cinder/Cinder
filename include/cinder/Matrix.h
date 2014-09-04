d69ae58043406cff92f5f5ab30660f33f30c15d6
Rich E <rich.eakin@gmail.com>
2014-09-04 22:06:11 +0200

    add glm/gtx/euler_angles.hpp to Matrix.h, needed for rotation /w euler angles

--- include/cinder/Matrix.h
+++ include/cinder/Matrix.h
@@ -34,9 +34,10 @@
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_inverse.hpp"
 #include "glm/gtc/type_ptr.hpp"
-#include "glm/gtx/transform.hpp"
-#include "glm/gtx/io.hpp"
 #include "glm/gtc/matrix_access.hpp"
+#include "glm/gtx/transform.hpp"
+#include "glm/gtx/euler_angles.hpp"
+#include "glm/gtx/io.hpp"
 
 namespace cinder {
 
