3fdfbd247408d09ba80cfd5ff343b8085cac25db
Paul Houx <phoux@barbariangroup.com>
2014-09-26 10:33:20 -0400

    Allows you to override startDraw and finishDraw functions. These were necessary to add support for Oculus.

--- include/cinder/app/RendererGl.h
+++ include/cinder/app/RendererGl.h
@@ -6,10 +6,10 @@
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and
-	the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-	the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+ the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ the following disclaimer in the documentation and/or other materials provided with the distribution.
 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
@@ -19,7 +19,7 @@
  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #pragma once
 
@@ -27,42 +27,46 @@
 #include "cinder/gl/gl.h"
 
 #if defined( CINDER_MAC )
-	#if defined __OBJC__
-		@class AppImplCocoaRendererGl;
-		@class NSOpenGLContext;
-	#else
-		class AppImplCocoaRendererGl;
-		class NSOpenGLContext;
-	#endif
-	typedef struct _CGLContextObject       *CGLContextObj;
-	typedef struct _CGLPixelFormatObject   *CGLPixelFormatObj;
+#if defined __OBJC__
+@class AppImplCocoaRendererGl;
+@class NSOpenGLContext;
+#else
+class AppImplCocoaRendererGl;
+class NSOpenGLContext;
+#endif
+typedef struct _CGLContextObject       *CGLContextObj;
+typedef struct _CGLPixelFormatObject   *CGLPixelFormatObj;
 #elif defined( CINDER_COCOA_TOUCH )
-	#if defined __OBJC__
-		typedef struct CGContext * CGContextRef;
-		@class AppImplCocoaTouchRendererGl;
-		@class EAGLContext;
-	#else
-		typedef struct CGContext * CGContextRef;
-		class AppImplCocoaTouchRendererGl;
-		class EAGLContext;
-	#endif
+#if defined __OBJC__
+typedef struct CGContext * CGContextRef;
+@class AppImplCocoaTouchRendererGl;
+@class EAGLContext;
+#else
+typedef struct CGContext * CGContextRef;
+class AppImplCocoaTouchRendererGl;
+class EAGLContext;
+#endif
 #endif
 
-namespace cinder { namespace gl {
+namespace cinder {
+namespace gl {
 
 class Context;
 typedef std::shared_ptr<Context>		ContextRef;
 
-} } // cinder::gl
+}
+} // cinder::gl
 
-namespace cinder { namespace app {
+namespace cinder {
+namespace app {
 
 typedef std::shared_ptr<class RendererGl>	RendererGlRef;
 class RendererGl : public Renderer {
-  public:
+public:
 	struct Options {
-	  public:
-		Options() {
+	public:
+		Options()
+		{
 #if defined( CINDER_COCOA_TOUCH )
 			mAntiAliasing = AA_NONE;
 			mCoreProfile = false;
@@ -70,7 +74,7 @@ class RendererGl : public Renderer {
 #else
 			mAntiAliasing = AA_MSAA_16;
 			mCoreProfile = true;
-			mVersion = std::pair<int,int>( 3, 2 );	
+			mVersion = std::pair<int, int>( 3, 2 );
 #endif
 #if ! defined( CINDER_GL_ES )
 			mDebugContext = false;
@@ -85,13 +89,13 @@ class RendererGl : public Renderer {
 		Options&	coreProfile( bool enable = true ) { mCoreProfile = enable; return *this; }
 		bool		getCoreProfile() const { return mCoreProfile; }
 		void		setCoreProfile( bool enable ) { mCoreProfile = enable; }
-		
+
 		Options&			version( int major, int minor ) { mVersion = std::make_pair( major, minor ); return *this; }
-		Options&			version( std::pair<int,int> version ) { mVersion = version; return *this; }
-		std::pair<int,int>	getVersion() const { return mVersion; }
+		Options&			version( std::pair<int, int> version ) { mVersion = version; return *this; }
+		std::pair<int, int>	getVersion() const { return mVersion; }
 		void				setVersion( int major, int minor ) { mVersion = std::make_pair( major, minor ); }
-		void				setVersion( std::pair<int,int> version ) { mVersion = version; }
-		
+		void				setVersion( std::pair<int, int> version ) { mVersion = version; }
+
 		Options&	antiAliasing( int amount ) { mAntiAliasing = amount; return *this; }
 		int			getAntiAliasing() const { return mAntiAliasing; }
 		void		setAntiAliasing( int amount ) { mAntiAliasing = amount; }
@@ -122,17 +126,17 @@ class RendererGl : public Renderer {
 		int			getDepthBufferDepth() const { return mDepthBufferBits; }
 		//! Sets the number of bits dedicated to the depth buffer. Default is \c 24.
 		void		setDepthBufferDepth( int depthBufferBits ) { mDepthBufferBits = depthBufferBits; }
-		
+
 		//! Enables or disables a stencil buffer. Default is \c false
 		Options&	stencil( bool createStencil = true ) { mStencil = createStencil; return *this; }
 		//! Returns whether a stenci buffer is enabled. Default is \c false
 		bool		getStencil() const { return mStencil; }
 		//! Enables or disables a stencil buffer. Default is \c false
 		void		setStencil( bool createStencil = true ) { mStencil = createStencil; }
-		
-	  protected:
+
+	protected:
 		bool					mCoreProfile;
-		std::pair<int,int>		mVersion;
+		std::pair<int, int>		mVersion;
 		int						mAntiAliasing;
 		bool					mStencil;
 		int						mDepthBufferBits;
@@ -150,18 +154,18 @@ class RendererGl : public Renderer {
 
 	static RendererGlRef	create( const Options &options = Options() ) { return RendererGlRef( new RendererGl( options ) ); }
 	virtual RendererRef		clone() const { return RendererGlRef( new RendererGl( *this ) ); }
- 
+
 #if defined( CINDER_COCOA )
-	#if defined( CINDER_MAC )
-		virtual void setup( App *aApp, CGRect frame, NSView *cinderView, RendererRef sharedRenderer, bool retinaEnabled );
-		virtual CGLContextObj			getCglContext();
-		virtual CGLPixelFormatObj		getCglPixelFormat();
-		virtual NSOpenGLContext*		getNsOpenGlContext();		
-	#elif defined( CINDER_COCOA_TOUCH )
-		virtual void 	setup( App *aApp, const Area &frame, UIView *cinderView, RendererRef sharedRenderer );
-		virtual bool 	isEaglLayer() const { return true; }
-		EAGLContext*	getEaglContext() const;
-	#endif
+#if defined( CINDER_MAC )
+	virtual void setup( App *aApp, CGRect frame, NSView *cinderView, RendererRef sharedRenderer, bool retinaEnabled );
+	virtual CGLContextObj			getCglContext();
+	virtual CGLPixelFormatObj		getCglPixelFormat();
+	virtual NSOpenGLContext*		getNsOpenGlContext();		
+#elif defined( CINDER_COCOA_TOUCH )
+	virtual void 	setup( App *aApp, const Area &frame, UIView *cinderView, RendererRef sharedRenderer );
+	virtual bool 	isEaglLayer() const { return true; }
+	EAGLContext*	getEaglContext() const;
+#endif
 	virtual void	setFrameSize( int width, int height );
 #elif defined( CINDER_MSW )
 	virtual void	setup( App *aApp, HWND wnd, HDC dc, RendererRef sharedRenderer );
@@ -172,16 +176,22 @@ class RendererGl : public Renderer {
 #endif
 
 	static const int sAntiAliasingSamples[];
-	enum	{ AA_NONE = 0, AA_MSAA_2, AA_MSAA_4, AA_MSAA_6, AA_MSAA_8, AA_MSAA_16, AA_MSAA_32 };
+	enum { AA_NONE = 0, AA_MSAA_2, AA_MSAA_4, AA_MSAA_6, AA_MSAA_8, AA_MSAA_16, AA_MSAA_32 };
 	const Options&	getOptions() const { return mOptions; }
 
 	virtual void	startDraw();
 	virtual void	finishDraw();
 	virtual void	defaultResize();
 	virtual void	makeCurrentContext();
+	virtual void	swapBuffers();
 	virtual Surface	copyWindowSurface( const Area &area );
-	
- protected:
+
+	//!
+	virtual void overrideStartDraw( const std::function<void( Renderer* )>& function ) override { mStartDrawFunction = function; }
+	//!
+	virtual void overrideFinishDraw( const std::function<void( Renderer* )>& function ) override { mFinishDrawFunction = function; }
+
+protected:
 	RendererGl( const RendererGl &renderer );
 
 	Options		mOptions;
@@ -190,15 +200,16 @@ class RendererGl : public Renderer {
 #elif defined( CINDER_COCOA_TOUCH )
 	AppImplCocoaTouchRendererGl	*mImpl;
 #elif defined( CINDER_MSW )
-	#if defined( CINDER_GL_ANGLE )
-		class AppImplMswRendererAngle	*mImpl;
-		friend class					AppImplMswRendererAngle;
-	#else
-		class AppImplMswRendererGl		*mImpl;
-		friend class					AppImplMswRendererGl;
-	#endif
+#if defined( CINDER_GL_ANGLE )
+	class AppImplMswRendererAngle	*mImpl;
+	friend class					AppImplMswRendererAngle;
+#else
+	class AppImplMswRendererGl		*mImpl;
+	friend class					AppImplMswRendererGl;
+#endif
 	HWND						mWnd;
 #endif
 };
 
-} } // namespace cinder::app
+}
+} // namespace cinder::app
--- src/cinder/app/RendererGl.cpp
+++ src/cinder/app/RendererGl.cpp
@@ -6,10 +6,10 @@
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice, this list of conditions and
-	the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-	the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+ the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+ the following disclaimer in the documentation and/or other materials provided with the distribution.
 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
@@ -19,36 +19,38 @@
  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #include "cinder/app/RendererGl.h"
 #include "cinder/ip/Flip.h"
 
 #if defined( CINDER_COCOA ) && ( ! defined( __OBJC__ ) )
-	#error "This file must be compiled as Objective-C++ on the Mac"
+#error "This file must be compiled as Objective-C++ on the Mac"
 #endif
 
 #if defined( CINDER_COCOA )
-	#if defined( CINDER_MAC )
-		#import "cinder/app/AppImplCocoaRendererGl.h"
-	#elif defined( CINDER_COCOA_TOUCH )
-		#import "cinder/app/AppImplCocoaTouchRendererGl.h"
-	#endif
+#if defined( CINDER_MAC )
+#import "cinder/app/AppImplCocoaRendererGl.h"
+#elif defined( CINDER_COCOA_TOUCH )
+#import "cinder/app/AppImplCocoaTouchRendererGl.h"
+#endif
 #elif defined( CINDER_MSW )
-	#if defined( CINDER_GL_ANGLE )
-		#include "cinder/app/AppImplMswRendererAngle.h"
-	#else
-		#include "cinder/app/AppImplMswRendererGl.h"
-	#endif
+#if defined( CINDER_GL_ANGLE )
+#include "cinder/app/AppImplMswRendererAngle.h"
+#else
+#include "cinder/app/AppImplMswRendererGl.h"
+#endif
 #endif
 
-namespace cinder { namespace app {
+namespace cinder {
+namespace app {
 
 const int RendererGl::sAntiAliasingSamples[] = { 0, 2, 4, 6, 8, 16, 32 };
 
 RendererGl::RendererGl( const RendererGl::Options &options )
 	: Renderer(), mImpl( 0 ), mOptions( options )
-{}
+{
+}
 
 RendererGl::RendererGl( const RendererGl &renderer )
 	: Renderer( renderer ), mOptions( renderer.mOptions )
@@ -72,7 +74,7 @@ void RendererGl::setup( App *aApp, CGRect frame, NSView *cinderView, RendererRef
 {
 	mApp = aApp;
 	RendererGlRef sharedGl = std::dynamic_pointer_cast<RendererGl>( sharedRenderer );
-	mImpl = [[AppImplCocoaRendererGl alloc] initWithFrame:NSRectFromCGRect(frame) cinderView:cinderView app:mApp renderer:this sharedRenderer:sharedGl withRetina:retinaEnabled];
+	mImpl = [[AppImplCocoaRendererGl alloc] initWithFrame:NSRectFromCGRect( frame ) cinderView : cinderView app : mApp renderer : this sharedRenderer : sharedGl withRetina : retinaEnabled];
 	// This is necessary for Objective-C garbage collection to do the right thing
 	::CFRetain( mImpl );
 }
@@ -89,7 +91,7 @@ void RendererGl::finishDraw()
 
 void RendererGl::setFrameSize( int width, int height )
 {
-	[mImpl setFrameSize:NSSizeToCGSize(NSMakeSize( width, height ))];
+	[mImpl setFrameSize : NSSizeToCGSize( NSMakeSize( width, height ) )];
 }
 
 void RendererGl::defaultResize()
@@ -102,27 +104,27 @@ Surface RendererGl::copyWindowSurface( const Area &area )
 	Surface s( area.getWidth(), area.getHeight(), false );
 	glFlush(); // there is some disagreement about whether this is necessary, but ideally performance-conscious users will use FBOs anyway
 	GLint oldPackAlignment;
-	glGetIntegerv( GL_PACK_ALIGNMENT, &oldPackAlignment ); 
+	glGetIntegerv( GL_PACK_ALIGNMENT, &oldPackAlignment );
 	glPixelStorei( GL_PACK_ALIGNMENT, 1 );
 	glReadPixels( area.x1, mApp->getWindow()->toPixels( mApp->getWindowHeight() ) - area.y2, area.getWidth(), area.getHeight(), GL_RGB, GL_UNSIGNED_BYTE, s.getData() );
-	glPixelStorei( GL_PACK_ALIGNMENT, oldPackAlignment );		
+	glPixelStorei( GL_PACK_ALIGNMENT, oldPackAlignment );
 	ip::flipVertical( &s );
 	return s;
 }
 
 CGLContextObj RendererGl::getCglContext()
 {
-	return [mImpl getCglContext];
+	return[mImpl getCglContext];
 }
 
 CGLPixelFormatObj RendererGl::getCglPixelFormat()
 {
-	return [mImpl getCglPixelFormat];
+	return[mImpl getCglPixelFormat];
 }
 
 NSOpenGLContext* RendererGl::getNsOpenGlContext()
 {
-	return [mImpl getNsOpenGlContext];
+	return[mImpl getNsOpenGlContext];
 }
 
 void RendererGl::makeCurrentContext()
@@ -140,12 +142,12 @@ void RendererGl::setup( App *aApp, const Area &frame, UIView *cinderView, Render
 	mApp = aApp;
 
 	RendererGlRef sharedRendererGl = std::dynamic_pointer_cast<RendererGl>( sharedRenderer );
-	mImpl = [[AppImplCocoaTouchRendererGl alloc] initWithFrame:cocoa::createCgRect( frame ) cinderView:(UIView*)cinderView app:mApp renderer:this sharedRenderer:sharedRendererGl];
+	mImpl = [[AppImplCocoaTouchRendererGl alloc] initWithFrame:cocoa::createCgRect( frame ) cinderView : (UIView*) cinderView app : mApp renderer : this sharedRenderer : sharedRendererGl];
 }
 
 EAGLContext* RendererGl::getEaglContext() const
 {
-	return [mImpl getEaglContext];
+	return[mImpl getEaglContext];
 }
 
 void RendererGl::startDraw()
@@ -160,7 +162,7 @@ void RendererGl::finishDraw()
 
 void RendererGl::setFrameSize( int width, int height )
 {
-	[mImpl setFrameSize:CGSizeMake( width, height )];
+	[mImpl setFrameSize : CGSizeMake( width, height )];
 }
 
 void RendererGl::defaultResize()
@@ -178,10 +180,10 @@ Surface	RendererGl::copyWindowSurface( const Area &area )
 	Surface s( area.getWidth(), area.getHeight(), true );
 	glFlush(); // there is some disagreement about whether this is necessary, but ideally performance-conscious users will use FBOs anyway
 	GLint oldPackAlignment;
-	glGetIntegerv( GL_PACK_ALIGNMENT, &oldPackAlignment ); 
+	glGetIntegerv( GL_PACK_ALIGNMENT, &oldPackAlignment );
 	glPixelStorei( GL_PACK_ALIGNMENT, 1 );
 	glReadPixels( area.x1, mApp->getWindow()->toPixels( mApp->getWindowHeight() ) - area.y2, area.getWidth(), area.getHeight(), GL_RGBA, GL_UNSIGNED_BYTE, s.getData() );
-	glPixelStorei( GL_PACK_ALIGNMENT, oldPackAlignment );	
+	glPixelStorei( GL_PACK_ALIGNMENT, oldPackAlignment );
 	ip::flipVertical( &s );
 
 	return s;
@@ -197,13 +199,13 @@ void RendererGl::setup( App *aApp, HWND wnd, HDC dc, RendererRef sharedRenderer 
 {
 	mWnd = wnd;
 	mApp = aApp;
-	if( ! mImpl )
+	if( !mImpl )
 #if defined( CINDER_GL_ANGLE )
 		mImpl = new AppImplMswRendererAngle( mApp, this );
 #else
 		mImpl = new AppImplMswRendererGl( mApp, this );
 #endif
-	if( ! mImpl->initialize( wnd, dc, sharedRenderer ) )
+	if( !mImpl->initialize( wnd, dc, sharedRenderer ) )
 		throw ExcRendererAllocation();
 }
 
@@ -224,7 +226,10 @@ void RendererGl::finishToggleFullScreen()
 
 void RendererGl::startDraw()
 {
-	mImpl->makeCurrentContext();
+	if( mStartDrawFunction )
+		mStartDrawFunction(this);
+	else
+		mImpl->makeCurrentContext();
 }
 
 void RendererGl::makeCurrentContext()
@@ -232,9 +237,17 @@ void RendererGl::makeCurrentContext()
 	mImpl->makeCurrentContext();
 }
 
-void RendererGl::finishDraw()
+void RendererGl::swapBuffers()
 {
 	mImpl->swapBuffers();
+}
+
+void RendererGl::finishDraw()
+{
+	if( mFinishDrawFunction )
+		mFinishDrawFunction(this);
+	else
+		mImpl->swapBuffers();
 }
 
 void RendererGl::defaultResize()
@@ -247,13 +260,14 @@ Surface	RendererGl::copyWindowSurface( const Area &area )
 	Surface s( area.getWidth(), area.getHeight(), false );
 	glFlush(); // there is some disagreement about whether this is necessary, but ideally performance-conscious users will use FBOs anyway
 	GLint oldPackAlignment;
-	glGetIntegerv( GL_PACK_ALIGNMENT, &oldPackAlignment ); 
+	glGetIntegerv( GL_PACK_ALIGNMENT, &oldPackAlignment );
 	glPixelStorei( GL_PACK_ALIGNMENT, 1 );
 	glReadPixels( area.x1, mApp->getWindow()->toPixels( mApp->getWindowHeight() ) - area.y2, area.getWidth(), area.getHeight(), GL_RGB, GL_UNSIGNED_BYTE, s.getData() );
-	glPixelStorei( GL_PACK_ALIGNMENT, oldPackAlignment );	
+	glPixelStorei( GL_PACK_ALIGNMENT, oldPackAlignment );
 	ip::flipVertical( &s );
 	return s;
 }
 #endif
 
-} } // namespace cinder::app
+}
+} // namespace cinder::app
