161a232c321dc219c3e9c00d1b348ca2d7b2384f
Andrew Bell <cinder.afb@gmail.com>
2014-10-16 17:14:43 -0400

    Updates to ImmediateMode sample

--- samples/_opengl/ImmediateMode/src/ImmediateModeApp.cpp
+++ samples/_opengl/ImmediateMode/src/ImmediateModeApp.cpp
@@ -18,33 +18,14 @@ class ImmediateModeApp : public AppNative {
 
 void ImmediateModeApp::setup()
 {
-	mGlsl = gl::GlslProg::create( gl::GlslProg::Format()
-		.vertex(	CI_GLSL( 150,
-			uniform mat4	ciModelViewProjection;
-			in vec4			ciPosition;
-			in vec3			ciColor;
-			out vec3		Color;
-			
-			void main( void ) {
-				gl_Position	= ciModelViewProjection * ciPosition;
-				Color = ciColor;
-			}
-		 ) )
-		.fragment(	CI_GLSL( 150,
-			in vec3		Color;
-			out vec3	oColor;
-			
-			void main( void ) {
-				oColor = Color;
-			}
-		) ) );
+	mGlsl = gl::getStockShader( gl::ShaderDef().color() );
+	mGlsl->bind();
 }
 
 void ImmediateModeApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) );	
+	gl::clear();	
 
-	mGlsl->bind();
 	gl::VertBatch vb( GL_TRIANGLES );
 		vb.color( 1, 0, 0 );
 		vb.vertex( getWindowWidth() / 2, 50 );
