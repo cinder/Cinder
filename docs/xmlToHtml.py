fd4e934695984b0d32d0501721994daf0cbfb019
Andrew Bell <cinder.afb@gmail.com>
2015-02-13 23:47:19 -0500

    Reasonable start to xmlToHtml.py

--- docs/xmlToHtml.py
+++ docs/xmlToHtml.py
@@ -3,44 +3,55 @@ import sys
 import os
 import codecs
 import shutil
-from xml.dom.minidom import parse
+import copy
+import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
-def getText(nodelist):
-	rc = []
-	for node in nodelist:
-		if node.nodeType == node.TEXT_NODE:
-			rc.append(node.data)
-	return ''.join(rc)
+def findCompoundName( tree ):
+	for compoundDef in tree.iter( "compounddef" ):
+		for compoundName in tree.iter( "compoundname" ):
+			print compoundName.text
+			return compoundName.text
 
-def findCompoundName( tagDom ):
-#	root = tagDom.getElementsByTagName( "doxygen" )[0]
-	for compoundDef in tagDom.getElementsByTagName( "compounddef" ):
-		for compoundName in compoundDef.getElementsByTagName( "compoundname" ):
-			print compoundName.toxml()
-			return getText( compoundName.childNodes )
-
-def processXmlFile( inPath, outPath ):
-	tagDom = parse( inPath )
-	soup = BeautifulSoup()
-	htmlTag = soup.new_tag( "html", xmlns="http://www.w3.org/1999/xhtml" )
-	soup.append( htmlTag )
-	headTag = soup.new_tag( "head" )
-	htmlTag.insert( 0, headTag )
-	titleTag = soup.new_tag( "title" )
-	titleTag.append( "Cinder: " + str(findCompoundName( tagDom )) )
+def markupFunction( fnXml, parent, bs4 ):
+	li = bs4.new_tag( "li" )
+	definition = bs4.new_tag( "em" )
+	definition.append( fnXml.find( "definition" ).text );
+	li.append( definition )
+	li.append( fnXml.find( "argsstring" ).text )
+	parent.append( li )
+	
+def processClassXmlFile( inPath, outPath, html ):
+	tree = ET.parse( inPath )
+	
+	# add title
+	headTag = html.head
+	titleTag = html.new_tag( "title" )
+	titleTag.append( "Cinder: " + str(findCompoundName( tree )) )
 	headTag.insert( 0, titleTag )
+
+	contentsTag = html.find( "div", "contents" )
+	ulTag = html.new_tag( "ul" )
+	contentsTag.append( ulTag )
+
+	# member functions
+	for memberFn in tree.findall( r'compounddef/sectiondef/memberdef[@kind="function"]' ):
+		markupFunction( memberFn, ulTag, html )
 
 	# write the file	
 	outFile = codecs.open( outPath, "w", "UTF-8" )
-	outFile.write( soup.prettify() )
+	outFile.write( html.prettify() )
 
+def parseTemplateHtml( templatePath ):
+	file = codecs.open( templatePath, "r", "UTF-8" )
+	return BeautifulSoup( file )
 
 if __name__ == "__main__":
-	doxygenHtmlPath = os.path.dirname( os.path.realpath(__file__) ) + os.sep + 'test' + os.sep
 	htmlSourcePath = os.path.dirname( os.path.realpath(__file__) ) + os.sep + 'htmlsrc' + os.sep
+	doxygenHtmlPath = os.path.dirname( os.path.realpath(__file__) ) + os.sep + 'html' + os.sep
 
 	if len( sys.argv ) == 3: # process a specific file
-		processXmlFile( sys.argv[1], os.path.join( doxygenHtmlPath, sys.argv[2] ) )
+		templateHtml = parseTemplateHtml( os.path.join( htmlSourcePath, "cinderClassTemplate.html" ) )
+		processClassXmlFile( sys.argv[1], os.path.join( doxygenHtmlPath, sys.argv[2] ), copy.deepcopy( templateHtml ) )
 	else:
 		print "Unknown usage"
