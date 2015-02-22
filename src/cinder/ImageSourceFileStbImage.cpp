c3507be30e3eb7528d210706f1ce812f65b364c0
Andrew Bell <cinder.afb@gmail.com>
2015-02-21 22:38:22 -0500

    Initial experiment with stb_image(_write)

---
+++ src/cinder/ImageSourceFileStbImage.cpp
@@ -0,0 +1,143 @@
+/*
+ Copyright (c) 2015, The Cinder Project, All rights reserved.
+ 
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice, this list of conditions and
+	the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "cinder/ImageSourceFileStbImage.h"
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+namespace cinder {
+
+///////////////////////////////////////////////////////////////////////////////
+// Registrar
+void ImageSourceFileStbImage::registerSelf()
+{
+	static bool alreadyRegistered = false;
+	static const int32_t SOURCE_PRIORITY = 3; // OS-default are 2; this is lower priority
+	
+	if( alreadyRegistered )
+		return;
+	alreadyRegistered = true;
+	
+	ImageIoRegistrar::SourceCreationFunc sourceFunc = ImageSourceFileStbImage::create;
+	ImageIoRegistrar::registerSourceType( "jpg", sourceFunc, SOURCE_PRIORITY );
+	ImageIoRegistrar::registerSourceType( "jpeg", sourceFunc, SOURCE_PRIORITY );
+	ImageIoRegistrar::registerSourceType( "png", sourceFunc, SOURCE_PRIORITY );
+	ImageIoRegistrar::registerSourceType( "bmp", sourceFunc, SOURCE_PRIORITY );
+	ImageIoRegistrar::registerSourceType( "psd", sourceFunc, SOURCE_PRIORITY );
+	ImageIoRegistrar::registerSourceType( "tga", sourceFunc, SOURCE_PRIORITY );
+	ImageIoRegistrar::registerSourceType( "gif", sourceFunc, SOURCE_PRIORITY );
+	ImageIoRegistrar::registerSourceType( "hdr", sourceFunc, SOURCE_PRIORITY );
+
+	ImageIoRegistrar::registerSourceGeneric( ImageSourceFileStbImage::create, SOURCE_PRIORITY );
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// ImageSourceFileStbImage
+ImageSourceFileStbImage::ImageSourceFileStbImage( DataSourceRef dataSourceRef, ImageSource::Options options )
+	: mData8u( nullptr ), mData32f( nullptr ), mRowBytes( 0 )
+{
+	int width = 0, height = 0, components = 0;
+	
+	if( dataSourceRef->isFilePath() ) {
+		if( stbi_is_hdr( dataSourceRef->getFilePath().string().c_str() ) ) {		
+			mData32f = stbi_loadf( dataSourceRef->getFilePath().string().c_str(), &width, &height, &components, 0 /*any # of components*/ );
+			if( ! mData32f )
+				throw ImageIoException();
+
+			mRowBytes = width * components * sizeof( float );
+		}
+		else {
+			mData8u = stbi_load( dataSourceRef->getFilePath().string().c_str(), &width, &height, &components, 0 /*any # of components*/ );
+			if( ! mData8u )
+				throw ImageIoException();
+
+			mRowBytes = width * components;		
+		}
+	}
+	else { // we'll use a dataref from the buffer
+		Buffer buffer = dataSourceRef->getBuffer();
+		if( stbi_is_hdr_from_memory( (unsigned char*)buffer.getData(), (int)buffer.getDataSize() ) ) {
+			mData32f = stbi_loadf_from_memory( (unsigned char*)buffer.getData(), (int)buffer.getDataSize(), &width, &height, &components, 0 /*any # of components*/ );
+			if( ! mData32f )
+				throw ImageIoException();
+			
+			mRowBytes = width * components * sizeof(float);
+		}
+		else {
+			mData8u = stbi_load_from_memory( (unsigned char*)buffer.getData(), (int)buffer.getDataSize(), &width, &height, &components, 0 /*any # of components*/ );
+			if( ! mData8u )
+				throw ImageIoException();
+				
+			mRowBytes = width * components;
+		}
+	}
+
+	if( mData8u )
+		setDataType( ImageIo::UINT8 );
+	else if( mData32f )
+		setDataType( ImageIo::FLOAT32 );
+	setSize( width, height );
+
+	switch( components ) {
+		case 1:
+			setColorModel( ImageIo::CM_GRAY );
+			setChannelOrder( ImageIo::ChannelOrder::Y );
+		break;
+		case 2:
+			setColorModel( ImageIo::CM_GRAY );
+			setChannelOrder( ImageIo::ChannelOrder::YA );
+		break;
+		case 3:
+			setColorModel( ImageIo::CM_RGB );
+			setChannelOrder( ImageIo::ChannelOrder::RGB );			
+		break;
+		case 4:
+			setColorModel( ImageIo::CM_RGB );
+			setChannelOrder( ImageIo::ChannelOrder::RGBA );
+		break;
+			throw ImageIoException();
+	}
+}
+
+
+ImageSourceFileStbImage::~ImageSourceFileStbImage()
+{
+	if( mData8u )
+		stbi_image_free( (void*)mData8u );
+	if( mData32f )
+		stbi_image_free( (void*)mData32f );
+}
+
+void ImageSourceFileStbImage::load( ImageTargetRef target )
+{
+	ImageSource::RowFunc func = setupRowFunc( target );
+	const uint8_t *data = ( mData8u ) ? mData8u : reinterpret_cast<uint8_t*>( mData32f );
+	for( int32_t row = 0; row < mHeight; ++row ) {
+		((*this).*func)( target, row, data + row * mRowBytes );
+	}
+}
+
+} // namespace cinder
---
+++ src/cinder/ImageTargetFileStbImage.cpp
@@ -0,0 +1,116 @@
+/*
+ Copyright (c) 2015, The Cinder Project, All rights reserved.
+ 
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice, this list of conditions and
+	the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "cinder/ImageTargetFileStbImage.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
+namespace cinder {
+
+void ImageTargetFileStbImage::registerSelf()
+{
+	static bool alreadyRegistered = false;
+	const int32_t PRIORITY = 3;
+	
+	if( alreadyRegistered )
+		return;
+	alreadyRegistered = true;
+
+	ImageIoRegistrar::TargetCreationFunc func = ImageTargetFileStbImage::create;
+	ImageIoRegistrar::registerTargetType( "png", func, PRIORITY, "png" );
+	ImageIoRegistrar::registerTargetType( "bmp", func, PRIORITY, "bmp" );
+	ImageIoRegistrar::registerTargetType( "tga", func, PRIORITY, "tga" );
+	ImageIoRegistrar::registerTargetType( "hdr", func, PRIORITY, "hdr" );
+}
+
+ImageTargetRef ImageTargetFileStbImage::create( DataTargetRef dataTarget, ImageSourceRef imageSource, ImageTarget::Options options, const std::string &extensionData )
+{
+	return ImageTargetRef( new ImageTargetFileStbImage( dataTarget, imageSource, options, extensionData ) );
+}
+
+ImageTargetFileStbImage::ImageTargetFileStbImage( DataTargetRef dataTarget, ImageSourceRef imageSource, ImageTarget::Options options, const std::string &extensionData )
+{
+	if( ! dataTarget->providesFilePath() ) {
+		throw ImageIoExceptionFailedWrite( "ImageTargetFileStbImage only supports writing to files." );
+	}
+
+	mFilePath = dataTarget->getFilePath();
+
+	setSize( imageSource->getWidth(), imageSource->getHeight() );
+	ImageIo::ColorModel cm = options.isColorModelDefault() ? imageSource->getColorModel() : options.getColorModel();
+	
+	switch( cm ) {
+		case ImageIo::ColorModel::CM_RGB:
+			mNumComponents = ( imageSource->hasAlpha() ) ? 4 : 3;
+			setColorModel( ImageIo::ColorModel::CM_RGB );
+			setChannelOrder( ( mNumComponents == 4 ) ? ImageIo::ChannelOrder::RGBA : ImageIo::ChannelOrder::RGB );
+		break;
+		case ImageIo::ColorModel::CM_GRAY:
+			mNumComponents = ( imageSource->hasAlpha() ) ? 2 : 1;
+			setColorModel( ImageIo::ColorModel::CM_GRAY );
+			setChannelOrder( ( mNumComponents == 2 ) ? ImageIo::ChannelOrder::YA : ImageIo::ChannelOrder::Y );
+		break;
+		default:
+			throw ImageIoExceptionIllegalColorModel();
+	}
+
+	mExtension = extensionData;
+	if( mExtension == "hdr" ) { // Radiance files are always float*
+		setDataType( ImageIo::DataType::FLOAT32 );
+		mRowBytes = mNumComponents * imageSource->getWidth() * sizeof(float);
+	}
+	else {
+		setDataType( ImageIo::DataType::UINT8 );
+		mRowBytes = mNumComponents * imageSource->getWidth() * sizeof(float);
+	}
+	
+	mData = std::unique_ptr<uint8_t[]>( new uint8_t[mWidth * mRowBytes] );
+}
+
+void* ImageTargetFileStbImage::getRowPointer( int32_t row )
+{
+	return &mData.get()[row * mRowBytes];
+}
+
+void ImageTargetFileStbImage::finalize()
+{
+	if( mExtension == "png" ) {
+		if( ! stbi_write_png( mFilePath.string().c_str(), (int)mWidth, (int)mHeight, mNumComponents, mData.get(), (int)mRowBytes ) )
+			throw ImageIoExceptionFailedWrite();
+	}
+	else if( mExtension == "bmp" ) {
+		if( ! stbi_write_bmp( mFilePath.string().c_str(), (int)mWidth, (int)mHeight, mNumComponents, mData.get() ) )
+			throw ImageIoExceptionFailedWrite();
+	}
+	else if( mExtension == "tga" ) {
+		if( ! stbi_write_tga( mFilePath.string().c_str(), (int)mWidth, (int)mHeight, mNumComponents, mData.get() ) )
+			throw ImageIoExceptionFailedWrite();
+	}
+	else if( mExtension == "hdr" ) {
+		if( ! stbi_write_hdr( mFilePath.string().c_str(), (int)mWidth, (int)mHeight, mNumComponents, (const float*)mData.get() ) )
+			throw ImageIoExceptionFailedWrite();
+	}
+}
+
+} // namespace cinder
