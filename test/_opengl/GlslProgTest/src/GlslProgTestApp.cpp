cd27b2f5f8dc238ecbe98e0b6d3c6f256f62e072
Rich E <rich.eakin@gmail.com>
2015-02-24 07:43:27 +0100

    added GlslProgTest shim

---
+++ test/_opengl/GlslProgTest/src/GlslProgTestApp.cpp
@@ -0,0 +1,61 @@
+#include "cinder/app/AppNative.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/gl/GlslProg.h"
+#include "cinder/Log.h"
+#include "cinder/System.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class GlslProgTestApp : public AppNative {
+	void setup() override;
+	void keyDown( KeyEvent event ) override;
+	void update() override;
+	void draw() override;
+
+	void testGlslProgInclude();
+
+	gl::GlslProgRef			mGlslProg;
+};
+
+void GlslProgTestApp::setup()
+{
+	testGlslProgInclude();
+}
+
+void GlslProgTestApp::keyDown( KeyEvent event )
+{
+	if( event.getChar() == 'r' ) {
+		CI_LOG_V( "reload" );
+		setup();
+	}
+}
+
+void GlslProgTestApp::testGlslProgInclude()
+{
+	try {
+		mGlslProg = gl::GlslProg::create( loadAsset( "passthrough.vert" ), loadAsset( "shaderWithInclude.frag" ) );
+	}
+	catch( std::exception &exc ) {
+		CI_LOG_E( "exception caught, type: " << System::demangleTypeName( typeid( exc ).name() ) << ", what: " << exc.what() );
+	}
+
+}
+
+void GlslProgTestApp::update()
+{
+}
+
+void GlslProgTestApp::draw()
+{
+	gl::clear( Color( 0.1, 0.2, 0.1 ) );
+
+	if( mGlslProg ) {
+		gl::ScopedGlslProg glslScope( mGlslProg );
+		gl::drawSolidRect( getWindowBounds() );
+	}
+}
+
+CINDER_APP_NATIVE( GlslProgTestApp, RendererGl )
