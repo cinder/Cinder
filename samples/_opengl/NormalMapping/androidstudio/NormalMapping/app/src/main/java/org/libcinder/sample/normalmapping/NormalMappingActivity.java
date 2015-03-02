d5664debae3bec008aad3a010c451e25c9cf113b
Hai Nguyen <chaoticbob@me.com>
2015-03-02 02:33:27 -0500

    Added first androidstudio project

---
+++ samples/_opengl/NormalMapping/androidstudio/NormalMapping/app/src/main/java/org/libcinder/sample/normalmapping/NormalMappingActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.sample.normalmapping;
+
+import android.app.NativeActivity;
+
+public class NormalMappingActivity extends NativeActivity {
+    static final String TAG = "NormalMappingActivity";
+}
