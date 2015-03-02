893b83844a00b51bb762f3f785835c44e7153013
Hai Nguyen <chaoticbob@me.com>
2015-03-02 03:12:39 -0500

    Added androidstudio for CubeMapping

---
+++ samples/_opengl/CubeMapping/androidstudio/CubeMapping/app/src/main/java/org/libcinder/sample/cubemapping/CubeMappingActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.sample.cubemapping;
+
+import android.app.NativeActivity;
+
+public class CubeMappingActivity extends NativeActivity {
+    static final String TAG = "CubeMappingActivity";
+}
