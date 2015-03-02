fa334ca9c360c1ab0d96b14b4d74a67394b1377d
Hai Nguyen <chaoticbob@me.com>
2015-03-02 03:52:53 -0500

    Addded androidstudio for MultiTouchBasic

---
+++ samples/MultiTouchBasic/androidstudio/MultiTouchBasic/app/src/main/java/org/libcinder/sample/multitouchbasic/MultiTouchBasicActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.sample.multitouchbasic;
+
+import android.app.NativeActivity;
+
+public class MultiTouchBasicActivity extends NativeActivity {
+    static final String TAG = "MultiTouchBasicActivity";
+}
