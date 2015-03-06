4102c529bc473cb8a6b0febefc35b71d93be5e69
Hai Nguyen <chaoticbob@me.com>
2015-03-06 04:48:50 -0500

    Updated fullbuild.sh to account for all platforms

--- android/fullbuild.sh
+++ android/fullbuild.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-sh ./fullbuild-arm.sh
-sh ./fullbuild-arm64.sh
-sh ./fullbuild-x86_64.sh
+sh fullbuild-armeabi.sh
+sh fullbuild-armeabi-v7a.sh
+sh fullbuild-x86.sh
+sh fullbuild-mips.sh
+sh fullbuild-arm64-v8a.sh
+sh fullbuild-x86_64.sh
+sh fullbuild-mips64.sh
