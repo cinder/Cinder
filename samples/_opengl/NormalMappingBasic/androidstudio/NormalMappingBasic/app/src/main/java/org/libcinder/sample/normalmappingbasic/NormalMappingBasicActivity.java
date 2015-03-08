d7e362e2580249cb4495fc98ce52520a2889c065
Hai Nguyen <chaoticbob@me.com>
2015-03-07 19:30:05 -0500

    Added NormalMappingBasic

---
+++ samples/_opengl/NormalMappingBasic/androidstudio/NormalMappingBasic/app/src/main/java/org/libcinder/sample/normalmappingbasic/NormalMappingBasicActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.sample.normalmappingbasic;
+
+import android.app.NativeActivity;
+
+public class NormalMappingBasicActivity extends NativeActivity {
+    static final String TAG = "BasicAppActivity";
+}
