b2e6276d7f13c8b0d71e25969d4ae20f8636e916
Hai Nguyen <chaoticbob@me.com>
2015-03-11 02:59:28 -0400

    Added androidstudio to ObjLoader

---
+++ samples/_opengl/ObjLoader/androidstudio/ObjLoader/app/src/main/java/org/libcinder/samples/objloader/ObjLoaderActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.samples.objloader;
+
+import android.app.NativeActivity;
+
+public class ObjLoaderActivity extends NativeActivity {
+    static final String TAG = "ObjLoaderActivity";
+}
