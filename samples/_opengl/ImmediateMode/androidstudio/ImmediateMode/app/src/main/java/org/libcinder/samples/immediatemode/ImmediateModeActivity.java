d13d562adfb168318942991b2626f4a169cec7ca
Hai Nguyen <chaoticbob@me.com>
2015-03-11 02:51:25 -0400

    Added androidstudio to ImmediateMode

---
+++ samples/_opengl/ImmediateMode/androidstudio/ImmediateMode/app/src/main/java/org/libcinder/samples/immediatemode/ImmediateModeActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.samples.immediatemode;
+
+import android.app.NativeActivity;
+
+public class ImmediateModeActivity extends NativeActivity {
+    static final String TAG = "ImmediateModeActivity";
+}
