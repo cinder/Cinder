74ed4cfa811c7d3949a3184b073149d283979bcf
Hai Nguyen <chaoticbob@me.com>
2015-03-11 02:56:02 -0400

    Added androidstudio to InstancedTeapots

---
+++ samples/_opengl/InstancedTeapots/androidstudio/InstancedTeapots/app/src/main/java/org/libcinder/samples/instancedteapots/InstancedTeapotsActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.samples.instancedteapots;
+
+import android.app.NativeActivity;
+
+public class InstancedTeapotsActivity extends NativeActivity {
+    static final String TAG = "InstancedTeapotsActivity";
+}
