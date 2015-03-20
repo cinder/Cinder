2ae2f171bf5017051c7a3b39615ad765744b7fe8
Rich E <rich.eakin@gmail.com>
2015-03-20 05:06:29 +0100

    working VboMesh sample with geom::Plane as geometry provider

--- samples/_opengl/VboMesh/src/VboMeshApp.cpp
+++ samples/_opengl/VboMesh/src/VboMeshApp.cpp
@@ -1,16 +1,16 @@
 //
 // This sample demonstrates the VboMesh class by creating a simple Plane mesh with a texture mapped onto it.
 // The mesh has static indices and texture coordinates, but its vertex positions are dynamic.
-// It also creates a second mesh which shares static and index buffers, but has its own dynamic buffer.
 //
 
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/VboMesh.h"
+#include "cinder/gl/Shader.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/GeomIO.h"
-#include "cinder/Camera.h"
+#include "cinder/MayaCamUI.h"
 #include "cinder/ImageIo.h"
 
 #include "Resources.h"
@@ -25,102 +25,88 @@ const int VERTICES_Z = 50;
 
 class VboSampleApp : public App {
  public:
-	void setup();
-	void update();
-	void draw();
+	void	setup() override;
+	void	update() override;
+	void	draw() override;
 
-	gl::VboMeshRef	mVboMesh, mVboMesh2;
+	void	keyDown( KeyEvent event ) override;
+	void	mouseDown( MouseEvent event ) override;
+	void	mouseDrag( MouseEvent event ) override;
+
+  private:
+	gl::VboMeshRef	mVboMesh;
 	gl::TextureRef	mTexture;
-	CameraPersp		mCamera;
+	MayaCamUI		mMayaCam;
 };
 
 void VboSampleApp::setup()
 {
-	// setup the parameters of the Vbo
-//	int totalVertices = VERTICES_X * VERTICES_Z;
-//	int totalQuads = ( VERTICES_X - 1 ) * ( VERTICES_Z - 1 );
-//	gl::VboMesh::Layout layout;
-//	layout.setStaticIndices();
-//	layout.setDynamicPositions();
-//	layout.setStaticTexCoords2d();
-//	mVboMesh = gl::VboMesh::create( totalVertices, totalQuads * 4, layout, GL_QUADS );
+	auto plane = geom::Plane().size( vec2( 20, 20 ) ).subdivisions( ivec2( VERTICES_X, VERTICES_Z ) );
 
-	// buffer our static data - the texcoords and the indices
-//	vector<uint32_t> indices;
-//	vector<vec2> texCoords;
-//	for( int x = 0; x < VERTICES_X; ++x ) {
-//		for( int z = 0; z < VERTICES_Z; ++z ) {
-//			// create a quad for each vertex, except for along the bottom and right edges
-//			if( ( x + 1 < VERTICES_X ) && ( z + 1 < VERTICES_Z ) ) {
-//				indices.push_back( (x+0) * VERTICES_Z + (z+0) );
-//				indices.push_back( (x+1) * VERTICES_Z + (z+0) );
-//				indices.push_back( (x+1) * VERTICES_Z + (z+1) );
-//				indices.push_back( (x+0) * VERTICES_Z + (z+1) );
-//			}
-//			// the texture coordinates are mapped to [0,1.0)
-//			texCoords.push_back( vec2( x / (float)VERTICES_X, z / (float)VERTICES_Z ) );
-//		}
-//	}
-//	
-//	mVboMesh->bufferIndices( indices );
-//	mVboMesh->bufferTexCoords2d( 0, texCoords );
-//	
-//	// make a second Vbo that uses the statics from the first
-//	mVboMesh2 = gl::VboMesh::create( totalVertices, totalQuads * 4, mVboMesh->getLayout(), GL_QUADS, &mVboMesh->getIndexVbo(), &mVboMesh->getStaticVbo(), NULL );
-//	mVboMesh2->setTexCoordOffset( 0, mVboMesh->getTexCoordOffset( 0 ) );
+	vector<gl::VboMesh::Layout> bufferLayout = {
+		gl::VboMesh::Layout().usage( GL_DYNAMIC_DRAW ).attrib( geom::Attrib::POSITION, 3 ),
+		gl::VboMesh::Layout().usage( GL_STATIC_DRAW ).attrib( geom::Attrib::TEX_COORD_0, 2 )
+	};
 
-	auto layoutDynamic = gl::VboMesh::Layout().usage( GL_DYNAMIC_DRAW ).attrib( geom::POSITION, 3 );
-	auto layoutStatic = gl::VboMesh::Layout().usage( GL_STATIC_DRAW ).attrib( geom::COLOR, 4 ).attrib( geom::TEX_COORD_0, 2 );
-	std::vector<gl::VboMesh::Layout> bufferLayout = { layoutDynamic, layoutStatic };
-
-	auto plane = geom::Plane().subdivisions( ivec2( VERTICES_X, VERTICES_Z ) );
-//	mVboMesh = gl::VboMesh::create( plane, { layoutDynamic, layoutStatic } );
 	mVboMesh = gl::VboMesh::create( plane, bufferLayout );
 
-	mTexture = gl::Texture::create( loadImage( loadResource( RES_IMAGE ) ) );
+	mTexture = gl::Texture::create( loadImage( loadResource( RES_IMAGE ) ), gl::Texture::Format().loadTopDown() );
+}
+
+void VboSampleApp::keyDown( KeyEvent event )
+{
+	if( event.getChar() == 'w' )
+		gl::setWireframeEnabled( ! gl::isWireframeEnabled() );
+}
+
+void VboSampleApp::mouseDown( MouseEvent event )
+{
+	mMayaCam.mouseDown( event.getPos() );
+}
+
+void VboSampleApp::mouseDrag( MouseEvent event )
+{
+#if defined( CINDER_MAC )
+	// remapping key modifiers so mayacam is usable on a mac touchpad
+	bool isLeftDown = event.isLeftDown();
+	bool isMiddleDown = event.isAltDown();
+	bool isRightDown = event.isMetaDown();
+
+	if( isMiddleDown )
+		isLeftDown = false;
+
+	mMayaCam.mouseDrag( event.getPos(), isLeftDown, isMiddleDown, isRightDown );
+#else
+	mMayaCam.mouseDrag( event.getPos(), event.isLeftDown(), event.isMiddleDown(), event.isRightDown() );
+#endif
 }
 
 void VboSampleApp::update()
 {
-	gl::setMatrices( mCamera );
+	const float zFreq = 0.9325f;
+	const float xFreq = 1.3467f;
+	float offset = getElapsedSeconds() * 5.0f;
 
-	const float timeFreq = 5.0f;
-	const float zFreq = 3.0f;
-	const float xFreq = 7.0f;
-	float offset = getElapsedSeconds() * timeFreq;
-
-	// dynmaically generate our new positions based on a simple sine wave
-//	gl::VboMesh::VertexIter iter = mVboMesh->mapVertexBuffer();
-//	for( int x = 0; x < VERTICES_X; ++x ) {
-//		for( int z = 0; z < VERTICES_Z; ++z ) {
-//			float height = sin( z / (float)VERTICES_Z * zFreq + x / (float)VERTICES_X * xFreq + offset ) / 5.0f;
-//			iter.setPosition( vec3( x / (float)VERTICES_X, height, z / (float)VERTICES_Z ) );
-//			++iter;
-//		}
-//	}
-
-	// dynmaically generate our new positions based on a simple sine wave for mesh2
-//	gl::VboMesh::VertexIter iter2 = mVboMesh2->mapVertexBuffer();
-//	for( int x = 0; x < VERTICES_X; ++x ) {
-//		for( int z = 0; z < VERTICES_Z; ++z ) {
-//			float height = sin( z / (float)VERTICES_Z * zFreq * 2 + x / (float)VERTICES_X * xFreq * 3 + offset ) / 10.0f;
-//			iter2.setPosition( vec3( x / (float)VERTICES_X, height, z / (float)VERTICES_Z ) + vec3( 0, 0.5, 0 ) );
-//			++iter2;
-//		}
-//	}
+	// dynmaically generate our new positions based on a sin(x) + cos(z) wave
+	auto mappedPosAttrib = mVboMesh->mapAttrib3f( geom::Attrib::POSITION );
+	for( int i = 0; i < mVboMesh->getNumVertices(); i++ ) {
+			vec3 &pos = *mappedPosAttrib;
+			(*mappedPosAttrib).y = ( sinf( pos.x * xFreq + offset ) + cosf( pos.z * zFreq + offset ) ) / 4.0f;;
+			++mappedPosAttrib;
+	}
+	mappedPosAttrib.unmap();
 }
 
 void VboSampleApp::draw()
 {
-	// this pair of lines is the standard way to clear the screen in OpenGL
 	gl::clear( Color( 0.15f, 0.15f, 0.15f ) );
 
-	gl::scale( vec3( 10, 10, 10 ) );
-//	mTexture->enableAndBind();
+	gl::setMatrices( mMayaCam.getCamera() );
+
+	gl::ScopedGlslProg glslScope( gl::getStockShader( gl::ShaderDef().texture() ) );
 	gl::ScopedTextureBind texScope( mTexture );
 
 	gl::draw( mVboMesh );
-//	gl::draw( mVboMesh2 );
 }
 
 
