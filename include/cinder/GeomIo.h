fc5041b0ba5dcb1490abb4799e40700c1a224a1b
Andrew Bell <cinder.afb@gmail.com>
2015-03-21 11:33:57 -0400

    Making geom primitives conform to colors( bool enable ) convention

--- include/cinder/GeomIo.h
+++ include/cinder/GeomIo.h
@@ -211,11 +211,9 @@ class Rect : public Source {
 
 	Rect&		rect( const Rectf &r );
 	//! Enables default colors. Disabled by default.
-	Rect&		colors();
+	Rect&		colors( bool enable = true ) { mHasColors = enable; return *this; }
 	//! Enables COLOR attrib and specifies corner values in clockwise order starting with the upper-left
 	Rect&		colors( const ColorAf &upperLeft, const ColorAf &upperRight, const ColorAf &lowerRight, const ColorAf &lowerLeft );
-	//! Disables colors. Disabled by default.
-	Rect&		disableColors();
 	//! Enables TEX_COORD_0 attrib and specifies corner values in clockwise order starting with the upper-left
 	Rect&		texCoords( const vec2 &upperLeft, const vec2 &upperRight, const vec2 &lowerRight, const vec2 &lowerLeft );
 
@@ -240,11 +238,9 @@ class Cube : public Source {
 	Cube();
 
 	//! Enables default colors. Disabled by default.
-	Cube&			colors();
+	Cube&			colors( bool enable = true ) { mHasColors = enable; return *this; }
 	//! Enables per-face colors ordered { +X, -X, +Y, -Y, +Z, -Z }. Colors are disabled by default.
 	Cube&			colors( const ColorAf &posX, const ColorAf &negX, const ColorAf &posY, const ColorAf &negY, const ColorAf &posZ, const ColorAf &negZ );
-	//! Disables colors. Disabled by default.
-	Cube&			disableColors();
 
 	Cube&			subdivisions( int sub ) { mSubdivisions = ivec3( std::max<int>( 1, sub ) ); return *this; }
 	Cube&			subdivisionsX( int sub ) { mSubdivisions.x = std::max<int>( 1, sub ); return *this; }
@@ -294,6 +290,9 @@ class Icosahedron : public Source {
 class Icosphere : public Source {
   public:
 	Icosphere();
+
+	// Enables colors. Disabled by default.
+	Icosphere&	colors( bool enable = true ) { mHasColors = enable; return *this; }
 
 	Icosphere&	subdivisions( int sub ) { mSubdivision = (sub > 0) ? (sub + 1) : 1; mCalculationsCached = false; return *this; }
 
@@ -379,7 +378,7 @@ class Sphere : public Source {
   public:
 	Sphere();
 
-	Sphere&		colors() { mHasColors = true; return *this; }
+	Sphere&		colors( bool enable = true ) { mHasColors = enable; return *this; }
 	Sphere&		center( const vec3 &center ) { mCenter = center; return *this; }
 	Sphere&		radius( float radius ) { mRadius = radius; return *this; }
 	//! Specifies the number of segments, which determines the roundness of the sphere.
@@ -393,7 +392,7 @@ class Sphere : public Source {
 	void		loadInto( Target *target, const AttribSet &requestedAttribs ) const override;
 
   protected:
-	void			numRingsAndSegments( int *numRings, int *numSegments ) const;
+	void		numRingsAndSegments( int *numRings, int *numSegments ) const;
 
 	vec3		mCenter;
 	float		mRadius;
@@ -406,7 +405,7 @@ class Capsule : public Source {
 	Capsule();
 
 	//! Enables colors. Disabled by default.
-	Capsule&		colors() { mHasColors = true; return *this; }
+	Capsule&		colors( bool enable = true ) { mHasColors = enable; return *this; }
 	Capsule&		center( const vec3 &center ) { mCenter = center; return *this; }
 	//! Specifies the number of radial subdivisions, which determines the roundness of the capsule. Defaults to \c 6.
 	Capsule&		subdivisionsAxis( int subdiv ) { mSubdivisionsAxis = subdiv; updateCounts(); return *this; }
@@ -504,6 +503,9 @@ class Cylinder : public Source {
   public:
 	Cylinder();
 
+	//! Enables colors. Disabled by default.
+	Cylinder&	colors( bool enable = true ) { mHasColors = enable; return *this; }
+
 	//! Specifices the base of the Cylinder.
 	Cylinder&	origin( const vec3 &origin ) { mOrigin = origin; updateCounts(); return *this; }
 	//! Specifies the number of radial subdivisions, which determines the roundness of the Cylinder. Defaults to \c 18.
@@ -518,8 +520,6 @@ class Cylinder : public Source {
 	Cylinder&	direction( const vec3 &direction ) { mDirection = normalize( direction ); return *this; }
 	//! Conveniently sets origin, height and direction so that the center of the base is \a from and the center of the apex is \a to.
 	Cylinder&	set( const vec3 &from, const vec3 &to );
-	//! Enables colors. Disabled by default.
-	Cylinder&	colors() { mHasColors = true; return *this; }
 
 	size_t		getNumVertices() const override;
 	size_t		getNumIndices() const override;
@@ -549,6 +549,9 @@ class Cone : public Cylinder {
   public:
 	Cone() { radius( 1.0f, 0.0f ); }
 
+	//! Enables colors. Disabled by default.
+	Cone&	colors( bool enable = true ) { mHasColors = enable; return *this; }
+
 	Cone&	origin( const vec3 &origin ) { Cylinder::origin( origin ); return *this; }
 	//! Specifies the number of radial subdivisions, which determines the roundness of the Cone. Defaults to \c 18.
 	Cone&	subdivisionsAxis( int subdiv ) { mSubdivisionsAxis = subdiv; updateCounts(); return *this; }
@@ -568,8 +571,6 @@ class Cone : public Cylinder {
 	Cone&	direction( const vec3 &direction ) { Cylinder::direction( direction ); return *this; }
 	//! Conveniently sets origin, height and direction.
 	Cone&	set( const vec3 &from, const vec3 &to ) { Cylinder::set( from, to ); return *this; }
-	//! Enables colors. Disabled by default.
-	Cone&	colors() { mHasColors = true; return *this; }
 };
 
 //! Defaults to a plane on the z axis, origin = [0, 0, 0], normal = [0, 1, 0]
