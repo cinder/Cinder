a917e4c717d021379a331d482f9456002c6f958a
Andrew Bell <cinder.afb@gmail.com>
2015-03-23 20:24:31 -0400

    Temporary changes to MovieWriter sample for testing; moving AvfWriter into Cinder xcodeproj

--- samples/MovieWriter/src/MovieWriterApp.cpp
+++ samples/MovieWriter/src/MovieWriterApp.cpp
@@ -1,8 +1,13 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
+#include "cinder/Utilities.h"
 #include "cinder/gl/Shader.h"
 
 #include "cinder/qtime/AvfWriter.h"
+
+#if defined( CINDER_COCOA_TOUCH )
+#import <UIKit/UIKit.h>
+#endif
 
 using namespace ci;
 using namespace ci::app;
@@ -16,20 +21,28 @@ class MovieWriterApp : public App {
 	void draw();
 	void cleanup() override { mMovieExporter.reset(); }
 	
-	float mAngle;
 	qtime::MovieWriterRef mMovieExporter;
 };
 
+void prepareSettings( App::Settings *settings )
+{
+	settings->setHighDensityDisplayEnabled( false );
+}
+
 void MovieWriterApp::setup()
 {
-	mAngle = 0;
-	
+#if defined( CINDER_COCOA_TOUCH )
+	auto format = qtime::MovieWriter::Format().codec( qtime::MovieWriter::H264 ).fileType( qtime::MovieWriter::QUICK_TIME_MOVIE )
+					.enableFrameReordering( false ).jpegQuality( 0.09f ).enableFrameReordering( false ).averageBitsPerSecond( 10000000 );
+	mMovieExporter = qtime::MovieWriter::create( getDocumentsDirectory() / "test.mov", getWindowWidth(), getWindowHeight(), format );
+#else
 	fs::path path = getSaveFilePath();
 	if( ! path.empty() ) {
-//		path += "Movie.mov";
-		mMovieExporter = qtime::MovieWriter::create( path, getWindowWidth(), getWindowHeight(), qtime::MovieWriter::Format().codec( qtime::MovieWriter::JPEG ).fileType( qtime::MovieWriter::M4V ) );
+		auto format = qtime::MovieWriter::Format().codec( qtime::MovieWriter::H264 ).fileType( qtime::MovieWriter::QUICK_TIME_MOVIE )
+						.enableFrameReordering( false ).jpegQuality( 0.09f ).enableFrameReordering( false ).averageBitsPerSecond( 10000000 );
+		mMovieExporter = qtime::MovieWriter::create( path, getWindowWidth(), getWindowHeight(), format );
 	}
-	
+#endif
 	gl::bindStockShader( gl::ShaderDef().color() );
 }
 
@@ -39,16 +52,24 @@ void MovieWriterApp::mouseDown( MouseEvent event )
 
 void MovieWriterApp::update()
 {
-	if( mMovieExporter )
+	const int maxFrames = 100;
+	if( mMovieExporter && getElapsedFrames() > 1 && getElapsedFrames() < maxFrames )
 		mMovieExporter->addFrame( copyWindowSurface() );
+	else if( mMovieExporter && getElapsedFrames() >= maxFrames ) {
+		mMovieExporter->finish();
+#if defined( CINDER_COCOA_TOUCH )
+		fs::path path = getDocumentsDirectory() / "test.mov";
+		::UISaveVideoAtPathToSavedPhotosAlbum( [NSString stringWithUTF8String:path.c_str()], nil, nullptr, nullptr );
+#endif
+	}
 }
 
 void MovieWriterApp::draw()
 {
 	gl::clear();
 	gl::enableDepthRead();
-	gl::color( Color( CM_HSV, fmod( getElapsedSeconds() / 10, 1.0f ), 1, 1 ) );
-	gl::draw( geom::Circle().center( getWindowCenter() ).radius( getElapsedSeconds() * 4 ) );
+	gl::color( Color( CM_HSV, fmod( getElapsedFrames() / 30.0f, 1.0f ), 1, 1 ) );
+	gl::draw( geom::Circle().center( getWindowCenter() ).radius( getElapsedFrames() ) );
 }
 
-CINDER_APP( MovieWriterApp, RendererGl )
+CINDER_APP( MovieWriterApp, RendererGl, prepareSettings )
