795458ae6f7111738bd964ddc2e2634ecc80cba5
Andrew Bell <cinder.afb@gmail.com>
2015-03-24 15:20:28 -0400

    Basic cleanup on AVFWriter sample

--- samples/QuickTimeAvfWriter/src/MovieWriterApp.cpp
+++ samples/QuickTimeAvfWriter/src/MovieWriterApp.cpp
@@ -2,13 +2,7 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/Utilities.h"
 #include "cinder/gl/Shader.h"
-#include "cinder/Rand.h"
-
 #include "cinder/qtime/AvfWriter.h"
-
-#if defined( CINDER_COCOA_TOUCH )
-#import <UIKit/UIKit.h>
-#endif
 
 using namespace ci;
 using namespace ci::app;
@@ -45,24 +39,6 @@ void MovieWriterApp::setup()
 	}
 #endif
 	gl::bindStockShader( gl::ShaderDef().color() );
-
-Surface8u s( getWindowWidth(), getWindowHeight(), false ), s2( getWindowWidth(), getWindowHeight(), false );
-for( int h = 0; h < s.getWidth(); ++h )
-	for( int v = 0; v < s.getHeight(); ++v ) {
-		s.setPixel( ivec2( h, v ), Color8u( randInt(), randInt(), randInt() ) );
-		s2.setPixel( ivec2( h, v ), Color8u( randInt(), randInt(), randInt() ) );
-	}
-
-for( int m = 0; m < 100; ++m )
-{
-auto format = qtime::MovieWriter::Format().codec( qtime::MovieWriter::H264 ).fileType( qtime::MovieWriter::QUICK_TIME_MOVIE )
-				.enableFrameReordering( false ).jpegQuality( 0.09f ).enableFrameReordering( false ).averageBitsPerSecond( 10000 );
-mMovieExporter = qtime::MovieWriter::create( getDocumentsDirectory() / ("test" + to_string( m ) + ".mov"), getWindowWidth(), getWindowHeight(), format );
-
-for( int i = 0; i < 3; ++i )
-	mMovieExporter->addFrame( ( i & 1 ) ? s : s2 );
-std::cout << m << std::endl;
-}
 }
 
 void MovieWriterApp::mouseDown( MouseEvent event )
@@ -76,10 +52,6 @@ void MovieWriterApp::update()
 		mMovieExporter->addFrame( copyWindowSurface() );
 	else if( mMovieExporter && getElapsedFrames() >= maxFrames ) {
 		mMovieExporter->finish();
-#if defined( CINDER_COCOA_TOUCH )
-		fs::path path = getDocumentsDirectory() / "test.mov";
-		::UISaveVideoAtPathToSavedPhotosAlbum( [NSString stringWithUTF8String:path.c_str()], nil, nullptr, nullptr );
-#endif
 	}
 }
 
