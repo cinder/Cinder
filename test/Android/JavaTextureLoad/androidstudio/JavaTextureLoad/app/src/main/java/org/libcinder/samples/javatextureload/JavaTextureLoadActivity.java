5b1f22c262e175f6e6384688dd565d2b9875c314
Hai Nguyen <chaoticbob@me.com>
2015-04-06 05:05:07 -0400

    Added JavaTextureLoad test

---
+++ test/Android/JavaTextureLoad/androidstudio/JavaTextureLoad/app/src/main/java/org/libcinder/samples/javatextureload/JavaTextureLoadActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.samples.javatextureload;
+
+import android.app.NativeActivity;
+
+public class JavaTextureLoadActivity extends NativeActivity {
+    static final String TAG = "JavaTextureLoadActivity";
+}
