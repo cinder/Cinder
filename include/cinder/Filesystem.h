884bd5c9d2ae9fef1ea3b49c447eae34a3b451dd
Rich E <rich.eakin@gmail.com>
2015-05-07 15:52:04 -0400

    updated how we expose vc140's filesystem library
    As they are matching the latest spec proposal, the namespace moved from
    std::tr2::sys to std::experimental::filesystem::v1. Also, operations
    like last_write_time() return a file_time_type as per proposed spec,
    whereas boost's version returns std::time_t.

--- include/cinder/Filesystem.h
+++ include/cinder/Filesystem.h
@@ -38,9 +38,14 @@
 namespace cinder {
 
 #if defined( CINDER_WINRT ) || ( defined( _MSC_VER ) && ( _MSC_VER >= 1900 ) )
-	namespace fs = std::tr2::sys;
+	namespace fs = std::experimental::filesystem::v1;
 #else
 	namespace fs = boost::filesystem;
+
+	namespace fs {
+		//! C++17 std filesystem library defines a file_time_type whereas boost::filesystem uses time_t, so we add this typedef to keep the two in harmony.
+		typedef std::time_t	file_time_type;
+	}
 #endif
 
 } // namespace cinder
