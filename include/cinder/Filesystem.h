8d572b9c8f59164c23df7ff386f2ca10f9d5c1fc
Andrew Bell <cinder.afb@gmail.com>
2015-05-12 15:12:36 -0400

    Updating Filesystem.h to reflect VC 2015

--- include/cinder/Filesystem.h
+++ include/cinder/Filesystem.h
@@ -36,14 +36,20 @@
 #endif
 
 namespace cinder {
-
 #if defined( CINDER_WINRT ) || ( defined( _MSC_VER ) && ( _MSC_VER >= 1900 ) )
 	namespace fs = std::tr2::sys;
+} // namespace cinder
 #else
 	namespace fs = boost::filesystem;
-#endif
-
 } // namespace cinder
+
+namespace boost {
+	namespace filesystem {
+		// C++17 filesystem library defines file_time_type, whereas boost::filesystem uses time_t
+		typedef std::time_t			file_time_type;
+	}
+}
+#endif
 
 #if defined( CINDER_WINRT ) || ( defined( _MSC_VER ) && ( _MSC_VER >= 1900 ) && ( _MSC_VER < 2000 ) )
 //! kludge to work around VC120's lack of fs::canonical
