a8def59cdbd250c93ba999915ba387038ae4cad1
Rich E <rich.eakin@gmail.com>
2015-05-21 04:06:26 -0400

    simplified CaptureBasic

--- samples/CaptureBasic/src/CaptureBasicApp.cpp
+++ samples/CaptureBasic/src/CaptureBasicApp.cpp
@@ -3,17 +3,20 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/Capture.h"
+#include "cinder/Log.h"
 
 using namespace ci;
 using namespace ci::app;
+using namespace std;
 
 class CaptureBasicApp : public App {
   public:
-	void setup();
-	void keyDown( KeyEvent event );
-	
-	void update();
-	void draw();
+	void setup() override;
+	void update() override;
+	void draw() override;
+
+  private:
+	void printDevices();
 
 	CaptureRef			mCapture;
 	gl::TextureRef		mTexture;
@@ -21,32 +24,15 @@ class CaptureBasicApp : public App {
 
 void CaptureBasicApp::setup()
 {
-	// print the devices
-	for( auto device = Capture::getDevices().begin(); device != Capture::getDevices().end(); ++device ) {
-		console() << "Device: " << (*device)->getName() << " "
-#if defined( CINDER_COCOA_TOUCH )
-					<< ( (*device)->isFrontFacing() ? "Front" : "Rear" ) << "-facing"
-#endif
-					<< std::endl;
-	}
+	printDevices();
 
 	try {
 		mCapture = Capture::create( 640, 480 );
 		mCapture->start();
 	}
 	catch( ci::Exception &exc ) {
-		console() << "Failed to initialize capture, what: " << exc.what() << std::endl;
+		CI_LOG_EXCEPTION( "Failed to init capture ", exc );
 	}
-}
-
-void CaptureBasicApp::keyDown( KeyEvent event )
-{
-#if defined( CINDER_MAC )
-	if( event.getChar() == 'f' )
-		setFullScreen( ! isFullScreen() );
-	else if( event.getChar() == ' ' )
-		( mCapture && mCapture->isCapturing() ) ? mCapture->stop() : mCapture->start();
-#endif
 }
 
 void CaptureBasicApp::update()
@@ -64,22 +50,32 @@ void CaptureBasicApp::update()
 
 void CaptureBasicApp::draw()
 {
-	gl::clear( Color( 0.0f, 0.0f, 0.0f ) );
-	gl::setMatricesWindow( getWindowWidth(), getWindowHeight() );
-	
-	if( mTexture ) {
-    gl::pushModelMatrix();
-#if defined( CINDER_COCOA_TOUCH )
-		//change iphone to landscape orientation
-		gl::rotate( M_PI / 2 );
-		gl::translate( 0.0f, -getWindowWidth() );
+	gl::clear();
 
-		Rectf flippedBounds( 0.0f, 0.0f, getWindowHeight(), getWindowWidth() );
+	if( mTexture ) {
+		gl::ScopedModelMatrix modelScope;
+
+#if defined( CINDER_COCOA_TOUCH )
+		// change iphone to landscape orientation
+		gl::rotate( M_PI / 2 );
+		gl::translate( 0, - getWindowWidth() );
+
+		Rectf flippedBounds( 0, 0, getWindowHeight(), getWindowWidth() );
 		gl::draw( mTexture, flippedBounds );
 #else
 		gl::draw( mTexture );
 #endif
-    gl::popModelMatrix();
+	}
+}
+
+void CaptureBasicApp::printDevices()
+{
+	for( const auto &device : Capture::getDevices() ) {
+		console() << "Device: " << device->getName() << " "
+#if defined( CINDER_COCOA_TOUCH )
+		<< ( device->isFrontFacing() ? "Front" : "Rear" ) << "-facing"
+#endif
+		<< endl;
 	}
 }
 
