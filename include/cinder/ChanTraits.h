a62a070e3b4322d3a75c519d242a8b1822ae4a47
Andrew Bell <cinder.afb@gmail.com>
2015-06-16 21:00:11 -0400

    Initial work on 'half' support in ImageIo

--- include/cinder/ChanTraits.h
+++ include/cinder/ChanTraits.h
@@ -26,6 +26,19 @@
 
 namespace cinder {
 
+union half_float
+{
+	unsigned short u;
+	struct {
+		unsigned int Mantissa : 10;
+		unsigned int Exponent : 5;
+		unsigned int Sign : 1;
+	};
+};
+
+extern half_float float_to_half( float f );
+extern float half_to_float( half_float h );
+
 template<typename T>
 struct CHANTRAIT 
 {
@@ -39,7 +52,8 @@ struct CHANTRAIT<uint8_t>
 	typedef int32_t SignedSum;
 	static uint8_t max() { return 255; }
 	static uint8_t convert( uint8_t v ) { return v; }
-	static uint8_t convert( uint16_t v ) { return v / 257; }	
+	static uint8_t convert( uint16_t v ) { return v / 257; }
+	static uint8_t convert( half_float v ) { return static_cast<uint8_t>( half_to_float( v ) * 255 ); }
 	static uint8_t convert( float v ) { return static_cast<uint8_t>( v * 255 ); }
 	static uint8_t grayscale( uint8_t r, uint8_t g, uint8_t b ) { return ( r * 54 + g * 183 + b * 19 ) >> 8; } // luma coefficients from Rec. 709
 	static uint8_t premultiply( uint8_t c, uint8_t a ) { return a * c / 255; }
@@ -55,7 +69,8 @@ struct CHANTRAIT<uint16_t>
 	typedef int32_t SignedSum;
 	static uint16_t max() { return 65535; }
 	static uint16_t convert( uint8_t v ) { return ( v << 8 ) | v; }
-	static uint16_t convert( uint16_t v ) { return v; }	
+	static uint16_t convert( uint16_t v ) { return v; }
+	static uint16_t convert( half_float v ) { return static_cast<uint16_t>( half_to_float( v ) * 65535 ); }
 	static uint16_t convert( float v ) { return static_cast<uint16_t>( v * 65535 ); }
 	static uint16_t grayscale( uint16_t r, uint16_t g, uint16_t b ) { return ( r * 6966 + g * 23436 + b * 2366 ) >> 15; } // luma coefficients from Rec. 709
 };
@@ -69,6 +84,7 @@ struct CHANTRAIT<float>
 	static float max() { return 1.0f; }
 	static float convert( uint8_t v ) { return v / 255.0f; }
 	static float convert( uint16_t v ) { return v / 65535.0f; }
+	static float convert( half_float v ) { return half_to_float( v ); }
 	static float convert( float v ) { return v; }
 	static float grayscale( float r, float g, float b ) { return r * 0.2126f + g * 0.7152f + b * 0.0722f; } // luma coefficients from Rec. 709
 	//! Calculates the multiplied version of a color component \a c by alpha \a a
@@ -76,6 +92,7 @@ struct CHANTRAIT<float>
 	static float inverse( float c ) { return 1.0f - c; }	
 };
 
+// Instantiated for ChannelT and SurfaceT
 #define CHANNEL_TYPES (uint8_t)(float)
 
 } // namespace cinder
