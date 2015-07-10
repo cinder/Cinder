008d31ff759301676d49abf5efb4fc3dbeca4e5d
Hai Nguyen <chaoticbob@me.com>
2015-07-10 01:04:01 -0700

    Updated flickr examples

---
+++ samples/FlickrTestMultithreaded/androidstudio/FlickrTestMultithreaded/app/src/main/java/org/libcinder/samples/flickrtestmultithreaded/FlickrTestMultithreadedActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.samples.flickrtestmultithreaded;
+
+import org.libcinder.app.CinderNativeActivity;
+
+public class FlickrTestMultithreadedActivity extends CinderNativeActivity {
+    static final String TAG = "FlickrTestMultithreadedActivity";
+}
