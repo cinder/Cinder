3afa2bbc7a90cfe190fbd6cc9377fae26e320a86
David Wicks <david@sosolimited.com>
2015-08-25 10:06:17 -0400

    getPicturesDirectory compiles in Android Studio.

--- src/cinder/java/org/libcinder/app/CinderNativeActivity.java
+++ src/cinder/java/org/libcinder/app/CinderNativeActivity.java
@@ -14,6 +14,7 @@ import android.graphics.SurfaceTexture;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.ConditionVariable;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
@@ -166,7 +167,7 @@ public class CinderNativeActivity extends NativeActivity {
     }
 
     public String getPicturesDirectory() {
-        String result = this.getExternalStorageDirectory(DIRECTORY_PICTURES).toString();
+        String result = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
         return result;
     }
 
