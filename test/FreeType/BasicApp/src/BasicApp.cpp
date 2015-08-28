e903b29c7fdc2eb67b9d02c54c90b264fc264b60
Hai Nguyen <chaoticbob@me.com>
2015-08-28 14:44:18 -0700

    Firs test rendering

--- test/FreeType/BasicApp/src/BasicApp.cpp
+++ test/FreeType/BasicApp/src/BasicApp.cpp
@@ -1,82 +1,129 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
-
 using namespace ci;
 using namespace ci::app;
+
+#include "ft2build.h"
+#include FT_FREETYPE_H
 
 // We'll create a new Cinder Application by deriving from the App class.
 class BasicApp : public App {
   public:
-	// Cinder will call 'mouseDrag' when the user moves the mouse while holding one of its buttons.
-	// See also: mouseMove, mouseDown, mouseUp and mouseWheel.
+	void setup() override;
 	void mouseDrag( MouseEvent event ) override;
-
-	// Cinder will call 'keyDown' when the user presses a key on the keyboard.
-	// See also: keyUp.
 	void keyDown( KeyEvent event ) override;
-
-	// Cinder will call 'draw' each time the contents of the window need to be redrawn.
 	void draw() override;
-
+  
   private:
-	// This will maintain a list of points which we will draw line segments between
-	std::vector<vec2> mPoints;
+	Channel8uRef	mBitmap;
+	gl::TextureRef	mTex;
 };
 
-void prepareSettings( BasicApp::Settings* settings )
+
+void draw_bitmap( FT_Int x, FT_Int y, FT_Bitmap* bitmap, Channel8uRef& target )
 {
-#if defined( CINDER_ANDROID )
-	settings->setMultiTouchEnabled( false );
-#endif
+	FT_Int i, j, p, q;
+	FT_Int x_max = x + bitmap->width;
+	FT_Int y_max = y + bitmap->rows;
+
+	uint8_t *data = target->getData();
+
+	for( i = x, p = 0; i < x_max; ++i, ++p ) {
+		for( j = y, q = 0; j < y_max; ++j, ++q ) {
+		 	if( i < 0 || j < 0 || i >= target->getWidth() || j >= target->getHeight() ) {
+				continue;
+			}
+			
+			size_t index = j*target->getWidth() + i;
+	  		data[index] |= bitmap->buffer[q * bitmap->width + p];
+		}
+	}
+}
+
+
+void BasicApp::setup()
+{	
+	mBitmap = Channel8u::create( getWindowWidth(), getWindowHeight() );
+	std::memset( mBitmap->getData(), 0, mBitmap->getWidth()*mBitmap->getHeight() );
+
+	fs::path fontPath = getAssetPath ( "fonts/Roboto-Regular.ttf" );
+
+	FT_Library library;
+	FT_Error error;
+
+	error = FT_Init_FreeType( &library );
+	if( error ) {
+		console() << "Failed FT_Init_FreeType" << std::endl;
+		return;
+	}
+
+	FT_Face face;
+	error = FT_New_Face( library, fontPath.string().c_str(), 0, &face );
+	if( error ) {
+		console() << "FT_New_Face" << std::endl;
+		return;
+	}
+	console() << "Loaded " << fontPath << std::endl;
+	
+ 	error = FT_Set_Char_Size( face, 32 * 64, 0, 100, 0 );
+	if( error ) {
+		console() << "FT_Set_Char_Size" << std::endl;
+		return;
+	}	
+
+	FT_GlyphSlot slot;
+	slot = face->glyph;
+
+	FT_Vector pen;
+	pen.x = 10 * 64;
+	pen.y = ( mBitmap->getHeight() - 200 ) * 64;
+
+	std::string s = "Cinder FreeType on Linux";
+	for( size_t n = 0; n < s.size(); ++n ) {
+		FT_Set_Transform( face, nullptr, &pen );
+
+		error = FT_Load_Char( face, s.at(n), FT_LOAD_RENDER );
+		if( error ) {
+		  continue;  
+		} 
+
+		/* now, draw to our target surface (convert position) */
+		int x = slot->bitmap_left;
+		int y = mBitmap->getHeight() - slot->bitmap_top;
+		draw_bitmap( 
+			x,
+			y,
+			&slot->bitmap,
+			mBitmap
+		);
+
+		/* increment pen position */
+		pen.x += slot->advance.x;
+		pen.y += slot->advance.y;
+	}
+
+  	FT_Done_Face( face );
+  	FT_Done_FreeType( library );
+
+	mTex = gl::Texture::create( *mBitmap );	
 }
 
 void BasicApp::mouseDrag( MouseEvent event )
 {
-	// Store the current mouse position in the list.
-	mPoints.push_back( event.getPos() );
 }
 
 void BasicApp::keyDown( KeyEvent event )
 {
-	if( event.getChar() == 'f' ) {
-		// Toggle full screen when the user presses the 'f' key.
-		setFullScreen( ! isFullScreen() );
-	}
-	else if( event.getCode() == KeyEvent::KEY_SPACE ) {
-		// Clear the list of points when the user presses the space bar.
-		mPoints.clear();
-	}
-	else if( event.getCode() == KeyEvent::KEY_ESCAPE ) {
-		// Exit full screen, or quit the application, when the user presses the ESC key.
-		if( isFullScreen() )
-			setFullScreen( false );
-		else
-			quit();
-	}
 }
 
 void BasicApp::draw()
 {
-	// Clear the contents of the window. This call will clear
-	// both the color and depth buffers. 
+	gl::setMatricesWindow( getWindowSize() );
+
 	gl::clear( Color::gray( 0.1f ) );
-
-	// Set the current draw color to orange by setting values for
-	// red, green and blue directly. Values range from 0 to 1.
-	// See also: gl::ScopedColor
-	gl::color( 1.0f, 0.5f, 0.25f );
-
-	// We're going to draw a line through all the points in the list
-	// using a few convenience functions: 'begin' will tell OpenGL to
-	// start constructing a line strip, 'vertex' will add a point to the
-	// line strip and 'end' will execute the draw calls on the GPU.
-	gl::begin( GL_LINE_STRIP );
-	for( const vec2 &point : mPoints ) {
-		gl::vertex( point );
-	}
-	gl::end();
+	gl::draw( mTex );
 }
 
 // This line tells Cinder to actually create and run the application.
-CINDER_APP( BasicApp, RendererGl, prepareSettings )
+CINDER_APP( BasicApp, RendererGl )
