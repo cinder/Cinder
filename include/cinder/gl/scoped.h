4bac4dba5054ca6aed358d55272cf79a9c2a7635
bantherewind <bantherewind@gmail.com>
2015-09-09 10:20:43 -0700

    Fixes issue #1104

--- include/cinder/gl/scoped.h
+++ include/cinder/gl/scoped.h
@@ -152,8 +152,10 @@ struct ScopedTextureBind : private Noncopyable {
 	// These overloads are to alleviate a VS2013 bug where it cannot deduce
 	// the correct constructor when a TextureBaseRef subclass is passed in
 	ScopedTextureBind( const Texture2dRef &texture, uint8_t textureUnit );
-#if ! defined( CINDER_GL_ES_2 )
+#if ! defined( CINDER_GL_ES )
 	ScopedTextureBind( const Texture1dRef &texture, uint8_t textureUnit );
+#endif
+#if ! defined( CINDER_GL_ES_2 )
 	ScopedTextureBind( const Texture3dRef &texture, uint8_t textureUnit );
 #endif // ! defined( CINDER_GL_ES_2 )
 	ScopedTextureBind( const TextureCubeMapRef &texture, uint8_t textureUnit );
