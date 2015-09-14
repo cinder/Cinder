f4728cd34b9b4bcc5a6bacc010b39068a869a910
88mpg <fratino.a@gmail.com>
2015-09-14 11:57:36 -0400

    add search button, convert css to styl

--- docs/htmlsrc/_assets/js/cinder.js
+++ docs/htmlsrc/_assets/js/cinder.js
@@ -210,7 +210,6 @@ $(document).ready(function() {
 		}
 
 		// hide view all results append when search is empty or no results
-		console.log($('#search-results ul'));
 		if(!$('#search-input').val() || $('#search-results ul').children().length <= 0) {
 			$('#search-results-view-all').hide();
 		}
