671ead252a039167cac6fc82d56b2959547ac71d
David Wicks <david@sosolimited.com>
2015-09-28 11:53:17 -0400

    Perform line continuation logic in ObjLoader.
    Handles multiple line continuations.
    Update tests to show success in parsing OBJs with line continuations.

--- test/unit/src/ObjLoaderTest.cpp
+++ test/unit/src/ObjLoaderTest.cpp
@@ -9,7 +9,7 @@ TEST_CASE( "ObjLoader" )
 {
 const auto planeData = std::string( R"obj(
 v 1 1 -1
-v 1 1 1.0
+v 1 1 1
 v -1.0 1.0 1.0
 v -1.0 1.0 -1.0
 vn 0.0 1.0 0.0
@@ -22,7 +22,7 @@ v 1 1 \
 v 1 \
  1 \
  1.0
-v -1.0 \\
+v -1.0 \
  1.0 1.0
 v -1.0 1.0 -1.0
 vn 0.0 1.0 0.0
@@ -31,7 +31,7 @@ f 1 4 3 2
 
 const auto planeDataNewlinesInFaces = std::string( R"obj(
 v 1 1 -1
-v 1 1 1.0
+v 1 1 1
 v -1.0 1.0 1.0
 v -1.0 1.0 -1.0
 vn 0.0 1.0 0.0
@@ -39,7 +39,31 @@ f 1 4 \
  3 2
 )obj" );
 
-const auto correctFirstPosition = vec3( 1, 1, -1 );
+const auto expectedPositions = std::vector<vec3> {
+	vec3( 1, 1, -1 ),
+	vec3( -1, 1, -1 ), // reordered by face indices
+	vec3( -1, 1, 1 ),
+	vec3( 1, 1, 1 )
+};
+
+const auto matchesExpectedPositions = [expectedPositions] (const ci::vec3 *positions) {
+	for( auto i = 0; i < expectedPositions.size(); i += 1 ) {
+		if( expectedPositions[i] != positions[i] ) {
+			return false;
+		}
+	}
+	return true;
+};
+
+SECTION( "Position matcher works properly" )
+{
+	auto wrong = std::vector<vec3> {
+		vec3( 1, 1, -1 ),
+		vec3( 1, 1, 1 )
+	};
+	REQUIRE( matchesExpectedPositions( expectedPositions.data() ) );
+	REQUIRE_FALSE( matchesExpectedPositions( wrong.data() ) );
+}
 
 SECTION( "ObjLoader can parse simple files." )
 {
@@ -47,26 +71,27 @@ SECTION( "ObjLoader can parse simple files." )
 	auto obj = ObjLoader(stream);
 	auto mesh = TriMesh::create(obj);
 	REQUIRE( mesh->getNumTriangles() == 2 );
-
-	auto *positions = mesh->getPositions<3>();
-
-	REQUIRE( positions[0] == correctFirstPosition );
+	REQUIRE( matchesExpectedPositions( mesh->getPositions<3>() ) );
 }
 
-SECTION( "ObjLoader incorrectly parses vertices with line continuations." )
+SECTION( "ObjLoader correctly parses vertices with line continuations." )
 {
 	auto stream = IStreamMem::create(planeDataNewlinesInVertices.c_str(), planeDataNewlinesInVertices.size());
 	auto obj = ObjLoader(stream);
+	auto group = obj.getGroups();
 	auto mesh = TriMesh::create(obj);
 	REQUIRE( mesh->getNumTriangles() == 2 );
-
-	REQUIRE_FALSE( mesh->getPositions<3>()[0] == correctFirstPosition );
+	REQUIRE( matchesExpectedPositions( mesh->getPositions<3>() ) );
 }
 
-SECTION( "ObjLoader throws an exception on faces with line continuations." )
+SECTION( "ObjLoader successfully parses faces with line continuations." )
 {
 	auto stream = IStreamMem::create(planeDataNewlinesInFaces.c_str(), planeDataNewlinesInFaces.size());
-	REQUIRE_THROWS( auto obj = ObjLoader(stream) );
+	auto obj = ObjLoader(stream);
+	auto group = obj.getGroups();
+	auto mesh = TriMesh::create(obj);
+	REQUIRE( mesh->getNumTriangles() == 2 );
+	REQUIRE( matchesExpectedPositions( mesh->getPositions<3>() ) );
 }
 
 } // ObjLoader tests
