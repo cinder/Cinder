2d77b65a72357aa5a21c6675c9bae24387144787
Hai Nguyen <choticbob@me.com>
2015-10-03 09:42:19 -0700

    Updated TessellationBezier for Android. Requires ES3.2 or ES3.1+AEP.

---
+++ samples/_opengl/TessellationBezier/androidstudio/TessellationBezier/app/src/main/java/org/libcinder/samples/tessellationbezier/TessellationBezierActivity.java
@@ -0,0 +1,7 @@
+package org.libcinder.samples.tessellationbezier;
+
+import org.libcinder.app.CinderNativeActivity;
+
+public class TessellationBezierActivity extends CinderNativeActivity {
+    static final String TAG = "TessellationBezierActivity";
+}
