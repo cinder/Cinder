e8fe193864ef01a722b0ab54f35a3fd9987c9035
David Wicks <david@sosolimited.com>
2015-10-26 12:08:29 -0400

    Add CATCH unit testing framework for tests.
    Make it simpler to add new tests.
    Make it easier to run all tests.
    
    Quickly copied some existing tests into the new project and
    made them use CATCH macros instead of custom setup.
    SignalsTest, Base64Test, SystemTest
    
    Stub for potential future ObjLoader test.

---
+++ test/UnitTests/src/ObjLoader_test.cpp
@@ -0,0 +1,29 @@
+//
+//  ObjLoader_test.cpp
+//
+//  Created by Soso Limited on 10/26/15.
+//
+//
+
+#include "catch.hpp"
+#include "cinder/ObjLoader.h"
+
+using namespace cinder;
+
+TEST_CASE("ObjLoader")
+{
+	SECTION("ObjLoader can parse simple files")
+	{
+		// TODO: load a file and check some basic facts about it (e.g. unit size, color)
+	}
+
+	SECTION("ObjLoader successfully parses material files")
+	{
+
+	}
+
+	SECTION("ObjLoader handles files with line continuations")
+	{
+
+	}
+}
