242c8e3a1f24d915a2a022587a1800275fc75162
Hai Nguyen <chaoticbob@me.com>
2015-11-12 09:49:07 +0000

    Updated for building on RPI2

--- linux/cmake/CinderIncludes.cmake
+++ linux/cmake/CinderIncludes.cmake
@@ -8,9 +8,12 @@ if( CINDER_LINUX_EGL_RPI2 )
 		${CINDER_INC_DIR} 
 		${CINDER_INC_DIR}/freetype
 		${CINDER_INC_DIR}/jsoncpp 
+		${CINDER_INC_DIR}/oggvorbis
 		${CINDER_SRC_DIR}/linebreak 
 		${CINDER_SRC_DIR}/libtess2 
 		${CINDER_SRC_DIR}/AntTweakBar
+		${CINDER_SRC_DIR}/oggvorbis/vorbis
+		${CINDER_SRC_DIR}/r8brain
 		${BOOST_INC_DIR}  
 	)
 else()
