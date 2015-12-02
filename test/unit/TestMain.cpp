afad248b2585011dc6156e1dfb0f560bd7b8d13e
David Wicks <david@sosolimited.com>
2015-12-02 11:38:24 -0500

    Rearrange test files by namespace.

--- test/unit/TestMain.cpp
+++ test/unit/TestMain.cpp
@@ -2,7 +2,7 @@
 ///
 /// @file Main test file compiles Catch unit test framework.
 /// Write your own tests in separate source files and build
-/// them and link them with the main project target so they run.
+/// them and link them with the main project target.
 ///
 /// These unit tests are useful for non-visual testing of Cinder.
 ///
