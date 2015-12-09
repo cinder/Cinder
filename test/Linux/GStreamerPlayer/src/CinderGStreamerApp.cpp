533aaafb5bae00b5f49b89dfce99cebbc95607fd
Hai Nguyen <chaoticbob@me.com>
2015-12-08 23:01:58 -0800

    Updated GStreamerPlayer test to use the implemented classes.

--- test/Linux/GStreamerPlayer/src/CinderGStreamerApp.cpp
+++ test/Linux/GStreamerPlayer/src/CinderGStreamerApp.cpp
@@ -1,7 +1,8 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
-#include "MovieBase.h"
+//#include "MovieBase.h"
+#include "cinder/linux/Movie.h"
 
 #include <iomanip>
 
@@ -14,21 +15,23 @@ class CinderGStreamerApp : public App {
   public:
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
-    void keyDown(KeyEvent event) override;
+    void keyDown( KeyEvent event ) override;
 	void update() override;
 	void draw() override;
-    std::vector<string> movies;
-    MovieGlRef movie;
-	ci::Font	mFont;
+
+  private:
+    std::vector<string> 	mMovies;
+    ci::linux::MovieGlRef 	mMovie;
+	ci::Font				mFont;
 };
 
 void CinderGStreamerApp::setup()
 {
 	mFont = ci::Font( "Droid Sans", 30.0f );
 
-    movie = MovieGl::create( "http://pdl.warnerbros.com/wbol/us/dd/med/northbynorthwest/quicktime_page/nbnf_airplane_explosion_qt_500.mov" );
-    movie->play();
-    movie->setLoop( true);
+    mMovie = ci::linux::MovieGl::create( "http://pdl.warnerbros.com/wbol/us/dd/med/northbynorthwest/quicktime_page/nbnf_airplane_explosion_qt_500.mov" );
+    mMovie->play();
+    mMovie->setLoop( true);
 }
 
 void CinderGStreamerApp::mouseDown( MouseEvent event )
@@ -37,42 +40,42 @@ void CinderGStreamerApp::mouseDown( MouseEvent event )
 
 void CinderGStreamerApp::keyDown(KeyEvent event)
 {
-    if( movie ) {
+    if( mMovie ) {
         if( event.getChar() == '1' ) {
-            movie->play();
+            mMovie->play();
         }
         else if( event.getChar() == '2' ) {
-            movie->stop();
+            mMovie->stop();
         }
         else if( event.getChar() == '3' ) {
-            movie->seekToTime( movie->getDuration() / 2.0 );
+            mMovie->seekToTime( mMovie->getDuration() / 2.0 );
         }
         else if( event.getChar() == '4' ) {
-            movie->setRate( 1.0 );
+            mMovie->setRate( 1.0 );
         }
         else if( event.getChar() == '5' ) {
-            movie->setRate( -1.0 );
+            mMovie->setRate( -1.0 );
         }
         else if( event.getChar() == '6' ) {
-            movie->setLoop( true, false );
+            mMovie->setLoop( true, false );
         }
         else if( event.getChar() == '7' ) {
-            movie->load( "http://pdl.warnerbros.com/wbol/us/dd/med/northbynorthwest/quicktime_page/nbnf_airplane_explosion_qt_500.mov" );
-	        movie->play();
+            mMovie = ci::linux::MovieGl::create( "http://pdl.warnerbros.com/wbol/us/dd/med/northbynorthwest/quicktime_page/nbnf_airplane_explosion_qt_500.mov" );
+	        mMovie->play();
         }
         else if( event.getChar() == '8' ) {
-            movie->load( getAssetPath("bbb.mp4") );
-	        movie->play();
+            mMovie = ci::linux::MovieGl::create( getAssetPath("bbb.mp4") );
+	        mMovie->play();
     	    //std::string capsGL = "video/x-raw(memory:GLMemory), format=RGBA";
             //GstCustomPipelineData pipelineData;
     	    //pipelineData.pipeline = "uridecodebin uri=http://movies.apple.com/media/us/quicktime/guide/hd/480p/noisettes_m480p.mov ! glupload ! glcolorconvert ! appsink name=videosink caps=\""+capsGL+"\""; // For now if you use appsink it has to be named 'videosink'. This is still experimental and it will change.
             //movie = MovieGl::create( pipelineData );
-            //movie->play();
+            //mMovie->play();
         }
         else if( event.getChar() == '9' ) {
             //pipelineData.pipeline = "uridecodebin uri=http://pdl.warnerbros.com/wbol/us/dd/med/northbynorthwest/quicktime_page/nbnf_airplane_explosion_qt_500.mov name=decode decode. ! audioconvert ! queue ! autoaudiosink decode. ! videoconvert ! queue ! appsink name=videosink caps=\""+caps+"\""; // For now if you use appsink it has to be named 'videosink'. This is still experimental and it will change.
-            movie->load("http://movies.apple.com/media/us/quicktime/guide/hd/480p/noisettes_m480p.mov");
-            movie->play();
+            mMovie = ci::linux::MovieGl::create( "http://movies.apple.com/media/us/quicktime/guide/hd/480p/noisettes_m480p.mov" );
+            mMovie->play();
         }
     }
 }
@@ -84,8 +87,8 @@ void CinderGStreamerApp::update()
 void CinderGStreamerApp::draw()
 {
     gl::clear( Color( 0, 0, 0 ) );
-    if( movie ){
-        ci::gl::Texture2dRef videoSurface = movie->getTexture();
+    if( mMovie ){
+        ci::gl::Texture2dRef videoSurface = mMovie->getTexture();
         if( videoSurface ) {
             Rectf centeredRect = Rectf( videoSurface->getBounds() ).getCenteredFit( getWindowBounds(), true );
             gl::draw( videoSurface, centeredRect );
