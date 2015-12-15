ddd6791165567c795d1fbff495bf04b0b3c10146
Rich E <rich.eakin@gmail.com>
2015-12-15 14:29:20 -0500

    specify CMAKE_ARCHIVE_OUTPUT_DIRECTORY, although not yet correct for android

--- proj/cmake/libcinder_target.cmake
+++ proj/cmake/libcinder_target.cmake
@@ -1,5 +1,10 @@
 cmake_minimum_required( VERSION 3.0 FATAL_ERROR )
 
+# TODO: deposit lib files in lib/target/arch folder, this is especially necessary on android
+set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/lib/${CINDER_TARGET}/${CMAKE_BUILD_TYPE} )
+
+message( "CMAKE_ARCHIVE_OUTPUT_DIRECTORY: ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}" )
+
 add_library(
     cinder STATIC
     ${CINDER_SRC_FILES}
