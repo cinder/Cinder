b622ed80305f29b086c98a37b4ae9c95158b25ea
Rich E <rich.eakin@gmail.com>
2015-12-15 11:58:23 -0500

    build for Debug if CMAKE_BUILD_TYPE isn’t specified

--- proj/cmake/libcinder_configure_build.cmake
+++ proj/cmake/libcinder_configure_build.cmake
@@ -5,3 +5,11 @@ message( "Building Cinder for ${CINDER_PLATFORM}" )
 
 set( CINDER_SRC_DIR 	"${CINDER_ROOT}/src" )
 set( CINDER_INC_DIR		"${CINDER_ROOT}/include" )
+
+if( NOT CMAKE_BUILD_TYPE )
+	message( "CMAKE_BUILD_TYPE not specified, defaulting to Debug" )
+	set( CMAKE_BUILD_TYPE Debug CACHE STRING
+		"Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel. "
+		FORCE
+	)
+endif()
