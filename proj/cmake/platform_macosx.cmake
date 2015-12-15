084cd9dc221a84605901dcd94281067c1e381871
Rich E <rich.eakin@gmail.com>
2015-12-15 12:46:47 -0500

    consistency renames: platform_windows.cmake -> platform_msw.cmake, going with platform_macosx.cmake on mac

--- proj/cmake/platform_macosx.cmake
+++ proj/cmake/platform_macosx.cmake
@@ -1,7 +1,6 @@
 cmake_minimum_required( VERSION 3.0 FATAL_ERROR )
-set( CMAKE_VERBOSE_MAKEFILE ON )
 
-set( CINDER_PLATFORM "OS X" )
+set( CINDER_PLATFORM "Mac OS X" )
 
 include( ${CINDER_CMAKE_DIR}/libcinder_configure_build.cmake )
 include( ${CINDER_CMAKE_DIR}/libcinder_source_files.cmake )
