e7737e3aefc45231e8b120f8404d75999e8bb7a6
Petros Kataras <petroskataras@gmail.com>
2016-03-08 17:56:59 +0000

    Fix indentation.

--- src/cinder/app/linux/PlatformLinux.cpp
+++ src/cinder/app/linux/PlatformLinux.cpp
@@ -171,16 +171,16 @@ struct DialogHelper {
 						value += static_cast<const char *>( buffer.data() );
 					}
 				}
-                if( ! value.empty() ) {
-                    // Zenity seems to add a new line character at the end of the path, so remove it if present.
-                    const auto newLineCharacterPos = std::strcspn( value.c_str(), "\n" );
+				if( ! value.empty() ) {
+					// Zenity seems to add a new line character at the end of the path, so remove it if present.
+					const auto newLineCharacterPos = std::strcspn( value.c_str(), "\n" );
 
-                    if( newLineCharacterPos !=  value.size() ) {
-                        value[ newLineCharacterPos ] = 0;
-                    }
+					if( newLineCharacterPos !=  value.size() ) {
+						value[ newLineCharacterPos ] = 0;
+					}
 
 					result = value;
-                }
+				}
 			}
 			pclose(pipe);			
 		}
