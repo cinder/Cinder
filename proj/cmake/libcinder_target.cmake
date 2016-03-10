804da6f886970c255d17f94395fded017930f49c
Petros Kataras <petroskataras@gmail.com>
2016-03-10 11:43:36 +0000

    Further work on Linux cmake config and initial export/configure support. WIP.

--- proj/cmake/libcinder_target.cmake
+++ proj/cmake/libcinder_target.cmake
@@ -1,5 +1,5 @@
 cmake_minimum_required( VERSION 3.0 FATAL_ERROR )
-
+cmake_policy( SET CMP0022 NEW )
 # note: CINDER_TARGET_SUBFOLDER is defined by each platform config, to be a folder that lives in cinder/lib/*
 set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/lib/${CINDER_TARGET_SUBFOLDER}/${CMAKE_BUILD_TYPE} )
 
@@ -13,4 +13,11 @@ add_library(
 target_include_directories( cinder BEFORE PUBLIC ${CINDER_INCLUDE_USER} )
 target_include_directories( cinder SYSTEM BEFORE PUBLIC ${CINDER_INCLUDE_SYSTEM} )
 
+target_link_libraries( cinder INTERFACE ${CINDER_LIBS_DEPENDS} )
+
 target_compile_definitions( cinder PUBLIC ${CINDER_DEFINES} )
+
+export( TARGETS cinder FILE ${PROJECT_BINARY_DIR}/cinderTargets.cmake )
+configure_file( ${CMAKE_CURRENT_LIST_DIR}/modules/cinderConfig.buildtree.cmake.in
+				${PROJECT_BINARY_DIR}/cinderConfig.cmake
+)
--- proj/cmake/platform_linux.cmake
+++ proj/cmake/platform_linux.cmake
@@ -1,8 +1,9 @@
 cmake_minimum_required( VERSION 3.0 FATAL_ERROR )
+
 set( CMAKE_VERBOSE_MAKEFILE ON )
 
 set( CINDER_PLATFORM "Posix" )
-
+message( " PATH : " ${CMAKE_MODULE_PATH} )
 # Find architecture name.
 execute_process( COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE CINDER_ARCH )
 
@@ -10,6 +11,7 @@ set( CINDER_TARGET_SUBFOLDER "linux/${CINDER_ARCH}" )
 
 include( ${CINDER_CMAKE_DIR}/libcinder_configure_build.cmake )
 include( ${CINDER_CMAKE_DIR}/libcinder_source_files.cmake )
+
 
 list( APPEND SRC_SET_GLFW 
 	${CINDER_SRC_DIR}/glfw/src/context.c
@@ -33,7 +35,8 @@ list( APPEND SRC_SET_CINDER_APP_LINUX
 	${CINDER_SRC_DIR}/cinder/app/linux/PlatformLinux.cpp
 )
 	
-if( NOT CINDER_GL_ES2_RPI )
+# Relevant source files depending on target GL.
+if( NOT CINDER_GL_ES_2_RPI )
 	if( CINDER_GL_ES )
 		list( APPEND SRC_SET_GLFW 
 			${CINDER_SRC_DIR}/glfw/src/egl_context.c
@@ -73,6 +76,86 @@ list( APPEND CINDER_SRC_FILES
 	${SRC_SET_CINDER_APP_LINUX}
 )
 
+# Relevant libs and include dirs depending on target platform and target GL.
+if( NOT CINDER_GL_ES )
+	find_package( OpenGL REQUIRED )
+	list( APPEND CINDER_LIBS_DEPENDS ${OPENGL_LIBRARIES} )
+	list( APPEND CINDER_INCLUDE_SYSTEM ${OPENGL_INCLUDE_DIR} )
+else()
+	# No X for the RPi2.
+	if( NOT CINDER_GL_ES_2_RPI )
+		find_package( X11 REQUIRED )
+		list( APPEND CINDER_LIBS_DEPENDS ${X11_LIBRARIES} Xcursor Xinerama Xrandr Xi )
+		list( APPEND CINDER_INCLUDE_SYSTEM ${X11_INCLUDE_DIR} )
+	else()
+		list( APPEND CINDER_INCLUDE_SYSTEM 
+			/opt/vc/include
+			/opt/vc/include/interface/vcos/pthreads
+		)
+		list( APPEND CINDER_LIBS_DEPENDS 
+			/opt/vc/lib/libEGL.so
+			/opt/vc/lib/libGLESv2.so
+			/opt/vc/lib/libbcm_host.so
+		)
+	endif()
+endif()
+
+# Where Find*.cmake are located. 
+
+# Common libs for Linux.
+# ZLib
+find_package( ZLIB REQUIRED )
+list( APPEND CINDER_LIBS_DEPENDS ${ZLIB_LIBRARIES} )
+list( APPEND CINDER_INCLUDE_SYSTEM ${ZLIB_INCLUDE_DIR} )
+# Curl
+find_package( CURL REQUIRED )
+list( APPEND CINDER_LIBS_DEPENDS ${CURL_LIBRARIES} )
+list( APPEND CINDER_INCLUDE_SYSTEM ${CURL_INCLUDE_DIR} )
+# FontConfig
+message( " MODULE PATH " ${CMAKE_MODULE_PATH} )
+find_package( FontConfig REQUIRED )
+list( APPEND CINDER_LIBS_DEPENDS ${FONTCONFIG_LIBRARIES} )
+list( APPEND CINDER_INCLUDE_SYSTEM ${FONTGONFIG_INCLUDE_DIRS} )
+# PulseAudio
+find_package( PulseAudio REQUIRED )
+list( APPEND CINDER_LIBS_DEPENDS ${PULSEAUDIO_LIBRARY} )
+list( APPEND CINDER_INCLUDE_SYSTEM ${PULSEAUDIO_INCLUDE_DIR} )
+# mpg123
+find_package( MPG123 REQUIRED )
+list( APPEND CINDER_LIBS_DEPENDS ${MPG123_LIBRARY} )
+list( APPEND CINDER_INCLUDE_DEPENDS ${MPG123_INCLUDE_DIR} )
+# sndfile 
+find_package( SNDFILE REQUIRED )
+list( APPEND CINDER_LIBS_DEPENDS ${SNDFILE_LIBRARY} )
+list( APPEND CINDER_INCLUDE_SYSTEM ${SNDFILE_INCLUDE_DIR} )
+# GStreamer and its dependencies.
+# Glib
+find_package( Glib REQUIRED COMPONENTS gobject )
+list( APPEND CINDER_LIBS_DEPENDS ${GLIB_GOBJECT_LIBRARIES} )
+list( APPEND CINDER_INCLUDE_SYSTEM ${GLIB_INCLUDE_DIRS} )
+# GStreamer
+find_package( GStreamer REQUIRED )
+list( APPEND CINDER_LIBS_DEPENDS 
+	${GSTREAMER_LIBRARIES}
+	${GSTREAMER_BASE_LIBRARIES}
+	${GSTREAMER_APP_LIBRARIES}
+	${GSTREAMER_VIDEO_LIBRARIES}
+)
+list( APPEND CINDER_INCLUDE_SYSTEM
+	${GSTREAMER_INCLUDE_DIRS}
+	${GSTREAMER_BASE_INCLUDE_DIRS}
+	${GSTREAMER_APP_INCLUDE_DIRS}
+	${GSTREAMER_VIDEO_INCLUDE_DIRS}
+)
+# If we have gst-gl available add it.
+if( GSTREAMER_GL_INCLUDE_DIRS AND GSTREAMER_GL_LIBRARIES )
+	list( APPEND CINDER_LIBS_DEPENDS ${GSTREAMER_GL_LIBRARIES} )
+	list( APPEND CINDER_INCLUDE_SYSTEM ${GSTREAMER_GL_INCLUDE_DIRS} )
+endif()
+# Defaults... dl and pthread
+list(  APPEND CINDER_LIBS_DEPENDS dl pthread )
+
+
 source_group( "cinder\\linux"           FILES ${SRC_SET_CINDER_LINUX} )
 source_group( "cinder\\app\\linux"      FILES ${SRC_SET_CINDER_APP_LINUX} )
 
@@ -80,7 +163,24 @@ list( APPEND CINDER_INCLUDE_USER
 	${CINDER_INC_DIR}/glfw
 )
 
-list( APPEND GLFW_FLAGS "-D_GLFW_X11 -D_GLFW_GLX -D_GLFW_USE_OPENGL" )
+# Cinder GL defines depending on target GL.
+if( CINDER_GL_ES AND NOT CINDER_GL_ES_2_RPI ) # es2, es3, es31, es32
+	list( APPEND GLFW_FLAGS "-D_GLFW_X11 -D_GLFW_EGL -D_GLFW_USE_GLESV2" )
+	if( CINDER_GL_ES_2 )
+		list( APPEND CINDER_DEFINES "-DCINDER_GL_ES_2" )
+	elseif( CINDER_GL_ES_3 )
+		list( APPEND CINDER_DEFINES "-DCINDER_GL_ES_3" )
+	elseif( CINDER_GL_ES_3_1 )
+		list( APPEND CINDER_DEFINES "-DCINDER_GL_ES_3_1" )
+	elseif( CINDER_GL_ES_3_2 )
+		list( APPEND CINDER_DEFINES "-DCINDER_GL_ES_3_2" )
+	endif()
+elseif( NOT CINDER_GL_ES ) # Core Profile
+	list( APPEND GLFW_FLAGS "-D_GLFW_X11 -D_GLFW_GLX -D_GLFW_USE_OPENGL" )
+else() # Rpi
+	list( APPEND CINDER_DEFINES "-DCINDER_GL_ES_2" "-DCINDER_LINUX_EGL_ONLY" )
+endif()
+
 list( APPEND CINDER_DEFINES "-D_UNIX ${GLFW_FLAGS}"  )
 
 include( ${CINDER_CMAKE_DIR}/libcinder_target.cmake )
