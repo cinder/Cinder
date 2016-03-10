a81c6e8a8e3807acd737ec071199adafb1cada24
Petros Kataras <petroskataras@gmail.com>
2016-03-10 17:08:19 +0000

    Small cleanup.

--- proj/cmake/platform_linux.cmake
+++ proj/cmake/platform_linux.cmake
@@ -49,7 +49,6 @@ if( NOT CINDER_GL_ES_2_RPI )
 			${CINDER_SRC_DIR}/glfw/src/glx_context.c
 		)
 		list( APPEND SRC_SET_CINDER_LINUX
-			${CINDER_SRC_DIR}/glload/gl_load.c
 			${CINDER_SRC_DIR}/glload/glx_load.c
 		)
 	endif()
