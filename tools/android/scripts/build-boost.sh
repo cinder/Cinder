f2c74693c1d4f4b7fa35ddb543ca4d181360ac1b
Andrew Bell <cinder.afb@gmail.com>
2016-03-11 17:46:56 -0500

    Updating Android Boost scripts and docs

--- tools/android/scripts/build-boost.sh
+++ tools/android/scripts/build-boost.sh
@@ -4,7 +4,8 @@
 #     sh build_boost.sh
 #
 
-cd ../../../boost
+CINDER_LIB="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../../../lib
+cd ../../../../boost
 
 echo "Boost strapping for filesystem,system"
 sh bootstrap.sh --with-libraries=filesystem,system
@@ -19,33 +20,33 @@ BOOST_ARCH=armeabi
 ARCH=armeabi
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=armeabi_v7a
 ARCH=armeabi-v7a
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=x86
 ARCH=x86
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=mips
 ARCH=mips
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 ########################################################################
 ########################################################################
@@ -57,55 +58,55 @@ BOOST_ARCH=armeabi
 ARCH=armeabi
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=armeabi_v7a
 ARCH=armeabi-v7a
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=x86
 ARCH=x86
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=mips
 ARCH=mips
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=arm64_v8a
 ARCH=arm64-v8a
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=x86_64
 ARCH=x86_64
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
 BOOST_ARCH=mips64
 ARCH=mips64
 echo ">>> Building android-${PLATFORM}/${ARCH}: ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}"
 ./b2 -a link=static debug-symbols=off optimization=space toolset=gcc-android_${PLATFORM}_${BOOST_ARCH} --stagedir=stage-${PLATFORM}-${BOOST_ARCH}
-echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}"
-mkdir -p ../lib/android/android-${PLATFORM}/${ARCH}
-cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ../lib/android/android-${PLATFORM}/${ARCH}
+echo ">>> cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}"
+mkdir -p ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
+cp stage-${PLATFORM}-${BOOST_ARCH}/lib/*.a ${CINDER_LIB}/android/android-${PLATFORM}/${ARCH}
 
