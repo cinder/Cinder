2d68f093337621f440ed7bcf0bcc1ebd7d1fe350
Rich E <rich.eakin@gmail.com>
2016-04-13 23:22:39 -0400

    fixed app::msw source paths

--- proj/cmake/platform_msw.cmake
+++ proj/cmake/platform_msw.cmake
@@ -16,11 +16,19 @@ list( APPEND SRC_SET_MSW
 )
 
 list( APPEND SRC_SET_APP_MSW
+	# TODO: should these two files be added to "cinder\\app" group?
 	${CINDER_SRC_DIR}/cinder/app/AppScreenSaver.cpp
-	${CINDER_SRC_DIR}/cinder/app/AppImplMsw.cpp
-	${CINDER_SRC_DIR}/cinder/app/AppImplMswBasic.cpp
-	${CINDER_SRC_DIR}/cinder/app/AppImplMswRendererGl.cpp
 	${CINDER_SRC_DIR}/cinder/app/RendererDx.cpp
+
+	${CINDER_SRC_DIR}/cinder/app/msw/AppImplMsw.cpp
+	${CINDER_SRC_DIR}/cinder/app/msw/AppImplMswBasic.cpp
+	${CINDER_SRC_DIR}/cinder/app/msw/AppImplMswScreenSaver.cpp
+	${CINDER_SRC_DIR}/cinder/app/msw/AppMsw.cpp
+	${CINDER_SRC_DIR}/cinder/app/msw/PlatformMsw.cpp
+	${CINDER_SRC_DIR}/cinder/app/msw/RendererImpl2dGdi.cpp
+	${CINDER_SRC_DIR}/cinder/app/msw/RendererImplDx.cpp
+	${CINDER_SRC_DIR}/cinder/app/msw/RendererImplGlAngle.cpp
+	${CINDER_SRC_DIR}/cinder/app/msw/RendererImplGlMsw.cpp
 )
 
 list( APPEND CINDER_SRC_FILES
