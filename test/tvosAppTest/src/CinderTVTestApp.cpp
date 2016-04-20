7cacddc6da325f0cf966ab4b2d3e043c45dcd761
David Wicks <wicks.db@gmail.com>
2016-04-20 16:41:26 +0200

    Match Cinder style in test app source.

--- test/tvosAppTest/src/CinderTVTestApp.cpp
+++ test/tvosAppTest/src/CinderTVTestApp.cpp
@@ -18,7 +18,7 @@ out vec2 vTexCoord;
 
 void main() {
 	gl_Position = ciModelViewProjection * ciPosition;
-	vTexCoord = ciTexCoord0;
+	vTexCoord =   ciTexCoord0;
 }
 
 )vs";
@@ -32,15 +32,15 @@ out vec4 fColor;
 
 #define PI 3.141592653
 
-float ripple(vec2 center, float cycles, float span, float offset) {
-	float d = distance(gl_FragCoord.xy, center);
-	return cos((d * cycles * 2.0 * PI / span) + offset);
+float ripple( vec2 center, float cycles, float span, float offset ) {
+	float d = distance( gl_FragCoord.xy, center );
+	return cos( (d * cycles * 2.0 * PI / span) + offset );
 }
 
 void main() {
-	vec2 window_size = vec2(1920, 1080);
-	float r = ripple(uNormalizedCursorPos, 8.0, window_size.x / 2.0, -ciElapsedSeconds * 2.0);
-	fColor = vec4(vTexCoord, 1.0, 1.0) * r;
+	vec2 window_size = vec2( 1920, 1080 );
+	float r = ripple( uNormalizedCursorPos * window_size, 8.0, window_size.x / 2.0, -ciElapsedSeconds * 2.0 );
+	fColor = vec4( vTexCoord, 1.0, 1.0 ) * r;
 }
 
 )fs";
@@ -54,37 +54,52 @@ using namespace std;
 class CinderTVTestApp : public App {
   public:
 	void setup() override;
+
+	/// Mouse events are not yet connected to anything on tvOS.
+	/// May create a custom event type for the touchpad input.
 	void mouseDown( MouseEvent event ) override;
 	void mouseMove( MouseEvent event ) override;
-	void touchesBegan( TouchEvent event) override;
+	/// Touch events also are not yet connected to anything on tvOS.
+	void touchesBegan( TouchEvent event ) override;
+
 	void update() override;
 	void draw() override;
+	void attemptWebAccess();
   private:
-	gl::BatchRef		_batch;
-	gl::TextureFontRef	_font;
+	gl::BatchRef		mBatch;
+	gl::TextureFontRef	mFont;
 };
 
 void CinderTVTestApp::setup()
 {
-	auto rect = geom::Rect(Rectf(0, 0, 1920, 1080));
-	_batch = gl::Batch::create(rect, gl::GlslProg::create(VertexShader, FragmentShader));
+	auto nowish = (int)chrono::high_resolution_clock::now().time_since_epoch().count();
+	ci::randSeed( nowish );
 
-	auto result = loadString(loadUrl(Url("https://www.google.com/search?q=volcano&tbm=isch")));
-	console() << "Result from googs: " << result << endl;
+	auto rect = geom::Rect( Rectf( 0, 0, 1920, 1080 ) );
+	mBatch = gl::Batch::create( rect, gl::GlslProg::create( VertexShader, FragmentShader ) );
 
-	auto possible_fonts = Font::getNames();
-	for (auto &font: possible_fonts) {
-		console() << "Font name: " << font << endl;
+	auto possibleFonts = Font::getNames();
+	auto arbitraryFontName = possibleFonts[randInt( (int)possibleFonts.size() )];
+	console() << "Creating a TextureFont using " << arbitraryFontName << endl;
+	mFont = gl::TextureFont::create( Font( arbitraryFontName, 96.0f ) );
+
+	attemptWebAccess();
+}
+
+void CinderTVTestApp::attemptWebAccess()
+{
+	try {
+		auto webContent = loadString( loadUrl( Url( "https://www.google.com/search?q=volcano&tbm=isch" ) ) );
+		console() << "Result from googs starts with: " << webContent.substr(0, 91) << endl;
 	}
-
-	auto ms = (int)chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
-	ci::randSeed(ms);
-	_font = gl::TextureFont::create(Font(possible_fonts[randInt((int)possible_fonts.size())], 96.0f));
+	catch( const std::exception &exc ) {
+		CI_LOG_E( "Error contacting website: " << exc.what() );
+	}
 }
 
 void CinderTVTestApp::touchesBegan(cinder::app::TouchEvent event)
 {
-	CI_LOG_I("Touch event: " << event.getTouches()[0].getPos());
+	CI_LOG_I("Touch began: " << event.getTouches().front().getPos());
 }
 
 void CinderTVTestApp::mouseDown( MouseEvent event )
@@ -93,23 +108,25 @@ void CinderTVTestApp::mouseDown( MouseEvent event )
 
 void CinderTVTestApp::mouseMove( MouseEvent event )
 {
-	auto pos = vec2(event.getPos()) / vec2(getWindowSize());
-	pos.y = 1.0 - pos.y;
-	_batch->getGlslProg()->uniform("uNormalizedCursorPos", pos);
 }
 
 void CinderTVTestApp::update()
 {
+	auto t = -getElapsedSeconds() * 0.2f;
+	auto x = cos( t );
+	auto y = sin( t );
+	auto rippleCenter = vec2( 0.5f ) + vec2( x, y ) * 0.25f;
+	mBatch->getGlslProg()->uniform( "uNormalizedCursorPos", rippleCenter );
 }
 
 void CinderTVTestApp::draw()
 {
 	gl::clear( Color( 0, 0, 0 ) );
-	gl::ScopedBlendPremult blend_scope;
-	_batch->draw();
-	_font->drawString("Whatever", getWindowCenter());
+	gl::ScopedBlendPremult blendScope;
+	mBatch->draw();
+	mFont->drawString( "Whatever", getWindowCenter() );
 }
 
-CINDER_APP( CinderTVTestApp, RendererGl(RendererGl::Options().version(3, 0)), [] (App::Settings *settings) {
+CINDER_APP( CinderTVTestApp, RendererGl( RendererGl::Options().version( 3, 0 ) ), [] ( App::Settings *settings ) {
 	settings->setMultiTouchEnabled(false);
 } )
