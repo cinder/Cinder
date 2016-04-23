3263047837e75fccad8544223edbacd978afa051
vade <vade@vade.info>
2016-04-23 14:45:29 -0400

    Migrate to Protected so that subclasses can properly swizzle these values for alignment should page alignment be required for optimization purposes.

--- include/cinder/Buffer.h
+++ include/cinder/Buffer.h
@@ -72,7 +72,7 @@ class Buffer {
 	//! Writes a Buffer to a DataTarget
 	void	write( const DataTargetRef &dataTarget );
 	
-  private:
+  protected:
 	void*	mData;
 	size_t	mAllocatedSize;
 	size_t	mDataSize;
