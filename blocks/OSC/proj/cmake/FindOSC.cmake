c25abb047e5ab2cba3ddb52e59ca0a43cda56610
Rich E <rich.eakin@gmail.com>
2016-07-18 06:39:39 -0400

    added if guards so block targets don’t get added twice

--- blocks/OSC/proj/cmake/FindOSC.cmake
+++ blocks/OSC/proj/cmake/FindOSC.cmake
@@ -1,13 +1,14 @@
-get_filename_component( OSC_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../src" ABSOLUTE )
-get_filename_component( CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../.." ABSOLUTE )
+if( NOT TARGET OSC )
+	get_filename_component( OSC_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../src" ABSOLUTE )
+	get_filename_component( CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../.." ABSOLUTE )
 
-message( "CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}" )
-message( "CINDER_PATH: ${CINDER_PATH}" )
+	message( "CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}" )
+	message( "CINDER_PATH: ${CINDER_PATH}" )
 
-add_library( OSC ${OSC_SOURCE_PATH}/OSC.cpp )
+	add_library( OSC ${OSC_SOURCE_PATH}/OSC.cpp )
 
-target_compile_options( OSC PUBLIC "-std=c++11" )
+	target_compile_options( OSC PUBLIC "-std=c++11" )
 
-target_include_directories( OSC PUBLIC "${OSC_SOURCE_PATH}" )
-target_include_directories( OSC SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
-
+	target_include_directories( OSC PUBLIC "${OSC_SOURCE_PATH}" )
+	target_include_directories( OSC SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
+endif()
