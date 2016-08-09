c0837050d2a816790d6a7ed1bef9130d6783f3f1
Petros Kataras <petroskataras@gmail.com>
2016-08-09 17:21:41 +0000

    Fix source path for OSC and TUIO blocks.

--- blocks/TUIO/proj/cmake/TUIOConfig.cmake
+++ blocks/TUIO/proj/cmake/TUIOConfig.cmake
@@ -1,7 +1,7 @@
 if( NOT TARGET TUIO )
 	get_filename_component( TUIO_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../src" ABSOLUTE )
 
-	add_library( TUIO ${TUIO_SOURCE_PATH}/cinder/tuio/TUIO.cpp )
+	add_library( TUIO ${TUIO_SOURCE_PATH}/cinder/tuio/Tuio.cpp )
 
 	target_compile_options( TUIO PUBLIC "-std=c++11" )
 	target_include_directories( TUIO PUBLIC "${TUIO_SOURCE_PATH}" )
