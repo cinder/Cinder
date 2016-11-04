0e0b28e6b0709b464b23b9d9fb7a6f8b7ffc33b6
Rich E <rich.eakin@gmail.com>
2016-11-04 01:41:42 -0400

    added ci_log_i that shares common code to build the message

--- proj/cmake/utilities.cmake
+++ proj/cmake/utilities.cmake
@@ -1,17 +1,27 @@
+function( _ci_build_message )
+	# get the path of the CMakeLists file evaluating this macro relative to the project's root source directory
+	file( RELATIVE_PATH _curr_file "${PROJECT_SOURCE_DIR}" "${CMAKE_CURRENT_LIST_FILE}" )
+
+	# Build up all remaining arguments into the _msg var
+	set( _msg )
+	foreach( i RANGE ${ARGC} )
+		set( _msg "${_msg}${ARGV${i}}" )
+	endforeach()
+
+	# deposit the concatenated message into 'msg' var for the parent function to use
+	set( msg "(${_curr_file}) -- ${_msg}" PARENT_SCOPE )
+endfunction()
+
+# Only prints if CINDER_VERBOSE is On
 function( ci_log_v )
 	if( CINDER_VERBOSE )
-		set( msg )
-		foreach( i RANGE ${ARGC} )
-			set( msg "${msg}${ARGV${i}}" )
-		endforeach()
-		message( VERBOSE " |cinder| ${msg}" )
+		_ci_build_message( "${ARGV}" )
+		message( "verbose ${msg}" )
 	endif()
 endfunction()
 
+# Alawys prints
 function( ci_log_i )
-	set( msg )
-	foreach( i RANGE ${ARGC} )
-		set( msg "${msg}${ARGV${i}}" )
-	endforeach()
-	message( INFO " |cinder| ${msg}" )
+	_ci_build_message( "${ARGV}" )
+	message( "info ${msg}" )
 endfunction()
