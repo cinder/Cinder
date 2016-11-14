0949a6fe73880dd93f4b0b74ab9986c2d1d4ea80
Rich E <rich.eakin@gmail.com>
2016-11-14 01:57:45 -0500

    CINDER_RESOURCES macro just used #PATH on linux, as resources live next to executable

--- include/cinder/CinderResources.h
+++ include/cinder/CinderResources.h
@@ -45,8 +45,7 @@
 		#define CINDER_RESOURCE( LOCALPREFIX, PATH, ID, TYPE ) \
 		QUOTE_PATH(LOCALPREFIX##PATH),ID,#TYPE
 	#endif
-#elif (defined(linux) || defined(__linux) || defined(__linux__)) && ! defined(__ANDROID__)
-	#define CINDER_RESOURCE( LOCALPREFIX, PATH, ID, TYPE ) #LOCALPREFIX #PATH
-#else // MAC or iOS RESOURCE
+#elif ! defined(__ANDROID__)
+	// MAC or iOS RESOURCE: lives in app bundle. Linux RESOURCE: lives in folder named 'resources` next to executable
 	#define CINDER_RESOURCE( LOCALPREFIX, PATH, ID, TYPE ) #PATH
 #endif
