1c9cdf86e11fa007f3f6a8fb47e3bd0a2dae83c9
Simon Geilfus <simongeilfus@users.noreply.github.com>
2016-12-22 13:53:55 +0100

    Added content to test/CinderDLL

--- test/CinderDLL/src/CinderDLLApp.cpp
+++ test/CinderDLL/src/CinderDLLApp.cpp
@@ -1,6 +1,8 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/CameraUi.h"
+#include "cinder/Rand.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -12,10 +14,22 @@ class CinderDLLApp : public App {
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+
+	gl::FboRef		mFbo;
+	gl::BatchRef	mObject;
+	CameraPersp		mCamera;
+	CameraUi		mCameraUi;
 };
 
 void CinderDLLApp::setup()
 {
+	mCamera = CameraPersp( getWindowWidth(), getWindowHeight(), 50.0f, 0.01f, 10.0f );
+	mCameraUi = CameraUi( &mCamera, getWindow() );
+	mCamera.lookAt( vec3( 1.0f ), vec3( 0.0f ) );
+	
+	mObject = gl::Batch::create( geom::Teapot(), gl::getStockShader( gl::ShaderDef().color().lambert() ) );
+
+	mFbo = gl::Fbo::create( getWindowWidth(), getWindowHeight(), gl::Fbo::Format().depthTexture() );
 }
 
 void CinderDLLApp::mouseDown( MouseEvent event )
@@ -24,11 +38,20 @@ void CinderDLLApp::mouseDown( MouseEvent event )
 
 void CinderDLLApp::update()
 {
+	gl::ScopedFramebuffer scopedFbo( mFbo );
+	gl::clear( Color::gray( 0.2f ) ); 
+	gl::setMatrices( mCamera );
+
+	gl::ScopedDepth scopedDepth( true );
+	mObject->draw();
 }
 
 void CinderDLLApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear();
+
+	gl::setMatricesWindow( getWindowSize() );
+	gl::draw( mFbo->getColorTexture(), mFbo->getBounds() );
 }
 
-CINDER_APP( CinderDLLApp, RendererGl )
+CINDER_APP( CinderDLLApp, RendererGl( RendererGl::Options().msaa( 8 ) ) )
