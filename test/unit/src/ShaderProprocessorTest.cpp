d4083b64c628732713732378075f095f8696c9b3
Rich E <rich.eakin@gmail.com>
2017-01-17 19:23:02 -0500

    added ShaderPreprocessor unit tests

--- test/unit/src/ShaderProprocessorTest.cpp
+++ test/unit/src/ShaderProprocessorTest.cpp
@@ -2,42 +2,78 @@
 
 #include "cinder/Cinder.h"
 #include "cinder/gl/ShaderPreprocessor.h"
-#include "cinder/gl/GlslProg.h"
 #include "cinder/Utilities.h"
 #include "cinder/app/App.h"
 
 using namespace std;
 using namespace ci;
 
-void preprocessAndCompare( gl::ShaderPreprocessor *preprocessor, const fs::path &sourceFilename )
-{
-	const fs::path shaderAssetFolder = "shader_preprocessor";
-	const fs::path sourcePath = shaderAssetFolder / sourceFilename;
-	const fs::path expectedPath = shaderAssetFolder / ( sourceFilename.stem().string() + "_expected.frag" );
-	const string source = loadString( app::loadAsset( sourcePath ) );
-	const string expected = loadString( app::loadAsset( expectedPath ) );
-
-	// compare by passing the source to ShaderPreprocessor
-	{
-		const string preprocessed = preprocessor->parse( source, sourcePath );
-		REQUIRE( preprocessed == expected );
-	}
-
-	// compare by passing the asset path to ShaderPreprocessor
-	{
-		const string preprocessed = preprocessor->parse( sourcePath );
-		REQUIRE( preprocessed == expected );
-	}
-}
-
 TEST_CASE( "ShaderPreprocessor" )
 {
-	SECTION( "Compare preprocessed output to expected result." )
+	SECTION( "test simple parse filepath with include" )
 	{
-		SECTION( "has version, no defines" )
-		{
-			gl::ShaderPreprocessor preprocessor;
-			preprocessAndCompare( &preprocessor, "has_version_no_defines.frag" );
-		}
+		gl::ShaderPreprocessor preprocessor;
+
+		fs::path sourcePath = app::getAssetPath( "shader_preprocessor/simple.frag" );
+
+		std::set<fs::path> includedFiles;
+		const string result = preprocessor.parse( sourcePath, &includedFiles );
+
+		REQUIRE( result.find( "#include" ) == string::npos );
+		REQUIRE( result.find( "fract( sin( n ) * 43758.5453 )" ) != string::npos );
+
+		fs::path commonSimplePath = app::getAssetPath( "shader_preprocessor/commonSimple.glsl" );
+		REQUIRE( includedFiles.size() == 1 );
+		REQUIRE( includedFiles.count( commonSimplePath ) == 1 );
+	}
+
+	SECTION( "test simple parse filepath with include" )
+	{
+		gl::ShaderPreprocessor preprocessor;
+
+		fs::path sourcePath = app::getAssetPath( "shader_preprocessor/simple.frag" );
+		const string source = loadString( loadFile( sourcePath ) );
+
+		std::set<fs::path> includedFiles;
+		const string result = preprocessor.parse( source, sourcePath, &includedFiles );
+
+		REQUIRE( result.find( "#include" ) == string::npos );
+		REQUIRE( result.find( "fract( sin( n ) * 43758.5453 )" ) != string::npos );
+
+		fs::path commonSimplePath = app::getAssetPath( "shader_preprocessor/commonSimple.glsl" );
+		REQUIRE( includedFiles.size() == 1 );
+		REQUIRE( includedFiles.count( commonSimplePath ) == 1 );
+	}
+
+	SECTION( "test defines" )
+	{
+		gl::ShaderPreprocessor preprocessor;
+		preprocessor.addDefine( "BLARG" );
+		preprocessor.addDefine( "SOMEVAR", "2" );
+
+		const string result = preprocessor.parse( app::getAssetPath( "shader_preprocessor/simple.frag" ) );
+
+		REQUIRE( result.find( "#define BLARG" ) != string::npos );
+		REQUIRE( result.find( "#define SOMEVAR 2" ) != string::npos );
+	}
+
+	SECTION( "test nested includes" )
+	{
+		gl::ShaderPreprocessor preprocessor;
+
+		fs::path sourcePath = app::getAssetPath( "shader_preprocessor/shaderWithNestedIncludes.frag" );
+
+		std::set<fs::path> includedFiles;
+		const string result = preprocessor.parse( sourcePath, &includedFiles );
+
+		REQUIRE( result.find( "#include" ) == string::npos );
+		REQUIRE( result.find( "fract( sin( n ) * 43758.5453 )" ) != string::npos );
+		REQUIRE( result.find( "float noise( in vec2 x )" ) != string::npos );
+
+		fs::path commonPath = app::getAssetPath( "shader_preprocessor/common.glsl" );
+		fs::path hashPath = app::getAssetPath( "shader_preprocessor/hash.glsl" );
+		REQUIRE( includedFiles.size() == 2 );
+		REQUIRE( includedFiles.count( commonPath ) == 1 );
+		REQUIRE( includedFiles.count( hashPath ) == 1 );
 	}
 }
