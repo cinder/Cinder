13068cc47df23da343ac4da1d4cd5d28ca669ce4
Rich E <rich.eakin@gmail.com>
2017-03-13 16:35:38 -0400

    Add intial FileWatcher class

---
+++ include/cinder/FileWatcher.h
@@ -0,0 +1,144 @@
+/*
+ Copyright (c) 2017, The Cinder Project, All rights reserved.
+
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+	the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+#include "cinder/Cinder.h"
+#include "cinder/Exception.h"
+#include "cinder/Filesystem.h"
+#include "cinder/Noncopyable.h"
+#include "cinder/Signals.h"
+
+#include <list>
+#include <vector>
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+namespace cinder {
+
+class Watch;
+typedef std::shared_ptr<class FileWatcher>	FileWatcherRef;
+
+//! Event type returned in callbacks when one more watched files have been modified.
+class WatchEvent {
+public:
+	WatchEvent( const std::vector<ci::fs::path> &filePaths )
+		: mModifiedFiles( filePaths )
+	{}
+
+	//! Returns the vector of absolute file paths which were dropped
+	const std::vector<ci::fs::path>&	getFiles() const								{ return mModifiedFiles; }
+	//! Returns the number of files dropped during the event
+	size_t								getNumFiles() const								{ return mModifiedFiles.size(); }
+	//! Returns the absolute filepath for file number \a index.
+	const								ci::fs::path& getFile( size_t index = 0 ) const	{ return mModifiedFiles.at( index ); }
+
+private:
+
+	std::vector<ci::fs::path>	mModifiedFiles;
+};
+
+//! FileMonitor provides a system for monitoring the filesystem for changes at runtime using callbacks.
+//!
+//! Performs file watching asynchronously, however all callbacks will be emitted on the main thread. It is advisable to capture
+//! the resulting signals::Connection with with some sort of scope controlling to ensure that your callbacks are disconnected
+//! when your object is destroyed. \see signals::ScopedConnection, signals::ConnectionList.
+//!
+//! \note any argument that takes an `fs::path` considers that operation to be global, that is any and all watches in place that
+//! include that file with be affected (examples are unwatch() and disable()). If you want to disable a single instance of a watch
+//! on a specific file, you can use the returned Connection's disable() or disconnect() methods.
+class CI_API FileWatcher : private ci::Noncopyable {
+public:
+	~FileWatcher();
+
+	//! Optional parameters provided to watch()
+	struct Options {
+		//! If true (default), the callback is fired directly after the watch is added, before the call to watch() returns.
+		Options& callOnWatch( bool b )	{ mCallOnWatch = b; return *this; }
+
+	private:
+		bool	mCallOnWatch = true;
+
+		friend class FileWatcher;
+	};
+
+	//! Returns the global instance of FileWatcher
+	static const FileWatcherRef&	instance();
+	//! Creates and returns a new FileWatcher within a shared_ptr.
+	static FileWatcherRef			create(); 
+
+	//! Enables or disables file watching.
+	void	setWatchingEnabled( bool enable );
+	//! Returns whether file watching is enabled or disabled.
+	bool	isWatchingEnabled() const	{ return mWatchingEnabled; }
+
+	//! Adds a single file at \a filePath to the watch list. Does not immediately call \a callback, but calls it whenever the file has been updated.
+	ci::signals::Connection watch( const ci::fs::path &filePath, const std::function<void ( const WatchEvent& )> &callback );
+	//! Adds a single file at \a filePath to the watch list, with optional \a options.
+	ci::signals::Connection watch( const ci::fs::path &filePath, const Options &options, const std::function<void ( const WatchEvent& )> &callback );
+	//! Adds the files in \a filePaths to the watch list. Does not immediately call \a callback, but calls it whenever one of the files has been updated.
+	ci::signals::Connection watch( const std::vector<ci::fs::path> &filePaths, const std::function<void ( const WatchEvent& )> &callback );
+	//! Adds the files in \a filePaths to the watch list, with optional \a options.
+	ci::signals::Connection watch( const std::vector<ci::fs::path> &filePaths, const Options &options, const std::function<void ( const WatchEvent& )> &callback );
+
+	//! Removes any watches for \a filePath
+	void unwatch( const ci::fs::path &filePath );
+	//! Removes any watches for \a filePaths
+	void unwatch( const std::vector<ci::fs::path> &filePaths );
+	//! Marks a file already in the watch list as active.
+	void enable( const ci::fs::path &filePath );
+	//! Marks a file as inactive, but doesn't remove it from the watch list.
+	void disable( const ci::fs::path &filePath );
+
+	//! Returns the number of Watch instances being watched.
+	const size_t	getNumWatches() const	{ return mWatchList.size(); }
+	//! Returns the total number of watched files, taking into account the number of files being watched by a WatchMany
+	const size_t	getNumWatchedFiles() const;
+
+private:
+	FileWatcher();
+
+	void	startWatching();
+	void	stopWatching();
+	void	threadEntry();
+	void	update();
+
+	std::list<std::unique_ptr<Watch>>	mWatchList;
+	std::recursive_mutex				mMutex;
+	std::unique_ptr<std::thread>		mThread;
+	std::atomic<bool>					mWatchingEnabled = { true };
+	std::atomic<bool>					mThreadShouldQuit;
+	std::atomic<double>					mThreadUpdateInterval = { 0.02 };
+	ci::signals::Connection				mUpdateConn;
+};
+
+//! Exception type thrown from errors within FileWatcher
+class CI_API FileWatcherException : public ci::Exception {
+public:
+	FileWatcherException( const std::string &description )
+		: Exception( description )
+	{}
+};
+
+} // namespace cinder
---
+++ src/cinder/FileWatcher.cpp
@@ -0,0 +1,398 @@
+/*
+ Copyright (c) 2017, The Cinder Project, All rights reserved.
+
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+	the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "cinder/FileWatcher.h"
+
+#include "cinder/app/App.h"
+#include "cinder/Log.h"
+
+//#define LOG_UPDATE( stream )	CI_LOG_I( stream )
+#define LOG_UPDATE( stream )	( (void)( 0 ) )
+
+using namespace ci;
+using namespace std;
+
+namespace cinder {
+
+//! Base class for Watch types, which are returned from FileWatcher::load() and watch()
+class Watch : public std::enable_shared_from_this<Watch>, private Noncopyable {
+public:
+	Watch( const std::vector<fs::path> &filePaths, bool needsCallback );
+
+	signals::Connection	connect( const function<void ( const WatchEvent& )> &callback )	{ return mSignalChanged.connect( callback ); }
+
+	//! Checks if the asset file is up-to-date. Also may discard the Watch if there are no more connected slots.
+	void checkCurrent();
+	//! Remove any watches for \a filePath. If it is the last file associated with this Watch, discard
+	void unwatch( const fs::path &filePath );
+	//! Emit the signal callback. 
+	void emitCallback();
+	//! Enables or disables a Watch
+	void setEnabled( bool enable, const fs::path &filePath );
+
+	//! Marks the Watch as needing its callback to be emitted on the main thread.
+	void setNeedsCallback( bool b )	{ mNeedsCallback = b; }
+	//! Returns whether the Watch needs its callback emitted on the main thread.
+	bool needsCallback() const		{ return mNeedsCallback; }
+	//! Marks the Watch as discarded, will be destroyed the next update loop
+	void markDiscarded()			{ mDiscarded = true; }
+	//! Returns whether the Watch is discarded and should be destroyed.
+	bool isDiscarded() const		{ return mDiscarded; }
+
+	struct WatchItem {
+		fs::path			mFilePath;
+		fs::file_time_type	mTimeStamp;
+		bool				mEnabled;
+	};
+
+	const std::vector<WatchItem>&	getItems() const	{ return mWatchItems; }
+
+private:
+	bool mDiscarded = false;
+	bool mEnabled = true;
+	bool mNeedsCallback = false;
+
+	std::vector<WatchItem>				mWatchItems;
+	std::vector<fs::path>				mModifiedFilePaths;
+
+	signals::Signal<void ( const WatchEvent& )>	mSignalChanged;
+};
+
+namespace {
+
+fs::path findFullFilePath( const fs::path &filePath )
+{
+	if( filePath.empty() )
+		throw FileWatcherException( "empty path" );
+
+	if( filePath.is_absolute() && fs::exists( filePath ) )
+		return filePath;
+
+	auto resolvedAssetPath = app::getAssetPath( filePath );
+	if( ! fs::exists( resolvedAssetPath ) )
+		throw FileWatcherException( "could not resolve file path: " + filePath.string() );
+
+	return resolvedAssetPath;
+}
+
+// Used from the debugger.
+void debugPrintWatches( const std::list<std::unique_ptr<Watch>>&watchList )
+{
+	int i = 0;
+	string str;
+	for( const auto &watch : watchList ) {
+		string needsCallback = watch->needsCallback() ? "true" : "false";
+		string discarded = watch->isDiscarded() ? "true" : "false";
+
+		const auto &items = watch->getItems();
+		string filePathStr = items.front().mFilePath.string();
+		if( items.size() > 1 )
+			filePathStr += " ...(" + to_string( items.size() ) + " files)";
+
+		str += "[" + to_string( i ) + "] needs callback: " + needsCallback + ", discarded: " + discarded + ", file: " + filePathStr + "\n";
+
+		i++;
+	}
+
+	app::console() << str << std::endl;
+}
+
+} // anonymous namespace
+
+// ----------------------------------------------------------------------------------------------------
+// Watch
+// ----------------------------------------------------------------------------------------------------
+
+Watch::Watch( const vector<fs::path> &filePaths, bool needsCallback )
+{
+	mWatchItems.reserve( filePaths.size() );
+	for( const auto &fp : filePaths ) {
+		auto fullPath = findFullFilePath( fp );
+		mWatchItems.push_back( { fullPath, fs::last_write_time( fullPath ), true } );
+	}
+
+	if( needsCallback ) {
+		// mark all files as modified, using the full path we just resolved.
+		for( const auto &item : mWatchItems )
+			mModifiedFilePaths.push_back( item.mFilePath );
+
+		setNeedsCallback( true );
+	}
+
+}
+
+void Watch::checkCurrent()
+{
+	// Discard when there are no more connected slots
+	if( mSignalChanged.getNumSlots() == 0 ) {
+		markDiscarded();
+		return;
+	}
+
+	mModifiedFilePaths.clear();
+	for( auto &item : mWatchItems ) {
+		if( item.mEnabled && fs::exists( item.mFilePath ) ) {
+			auto timeLastWrite = fs::last_write_time( item.mFilePath );
+			if( item.mTimeStamp < timeLastWrite ) {
+				item.mTimeStamp = timeLastWrite;
+				mModifiedFilePaths.push_back( item.mFilePath );
+				setNeedsCallback( true );
+			}
+		}
+	}
+}
+
+void Watch::unwatch( const fs::path &filePath ) 
+{
+	mWatchItems.erase( remove_if( mWatchItems.begin(), mWatchItems.end(),
+		[&filePath]( const WatchItem &item ) {
+			return item.mFilePath == filePath;
+		} ),
+		mWatchItems.end() );
+	
+	if( mWatchItems.empty() )
+		markDiscarded();
+}
+
+void Watch::setEnabled( bool enable, const fs::path &filePath )
+{
+	for( auto &item : mWatchItems ) {
+		if( item.mFilePath == filePath ) {
+			item.mEnabled = enable;
+
+			// update the timestamp so that any modifications while
+			// the watch was disabled don't trigger a callback
+			item.mTimeStamp = fs::last_write_time( item.mFilePath );
+		}
+	}
+}
+
+void Watch::emitCallback() 
+{
+	WatchEvent event( mModifiedFilePaths );
+
+	mSignalChanged.emit( event );
+	setNeedsCallback( false );
+} 
+
+// ----------------------------------------------------------------------------------------------------
+// FileWatcher
+// ----------------------------------------------------------------------------------------------------
+
+// static
+const FileWatcherRef& FileWatcher::instance()
+{
+	static FileWatcherRef sInstance;
+	if( ! sInstance )
+		sInstance = create();
+
+	return sInstance;
+}
+
+// static
+FileWatcherRef FileWatcher::create()
+{
+	return FileWatcherRef( new FileWatcher );
+}
+
+FileWatcher::FileWatcher()
+{
+	startWatching();
+}
+
+FileWatcher::~FileWatcher()
+{
+	stopWatching();
+}
+
+void FileWatcher::setWatchingEnabled( bool enable )
+{
+	if( mWatchingEnabled == enable )
+		return;
+
+	mWatchingEnabled = enable;
+	if( enable )
+		instance()->startWatching();
+	else
+		instance()->stopWatching();
+}
+
+signals::Connection FileWatcher::watch( const fs::path &filePath, const function<void ( const WatchEvent& )> &callback )
+{ 
+	vector<fs::path> filePaths = { filePath };
+	return watch( filePaths, Options(), callback );
+}
+
+signals::Connection FileWatcher::watch( const fs::path &filePath, const Options &options, const function<void( const WatchEvent& )> &callback )
+{
+	vector<fs::path> filePaths = { filePath };
+	return watch( filePaths, options, callback );
+}
+
+signals::Connection FileWatcher::watch( const vector<fs::path> &filePaths, const function<void ( const WatchEvent& )> &callback )
+{ 
+	return watch( filePaths, Options(), callback );
+}
+
+signals::Connection FileWatcher::watch( const vector<fs::path> &filePaths, const Options &options, const function<void ( const WatchEvent& )> &callback )
+{
+	auto watch = new Watch( filePaths, options.mCallOnWatch );
+	auto conn = watch->connect( callback );
+
+	lock_guard<recursive_mutex> lock( mMutex );
+
+	mWatchList.emplace_back( watch );
+
+	if( options.mCallOnWatch )
+		watch->emitCallback();
+
+	return watch->connect( callback );
+}
+
+void FileWatcher::unwatch( const fs::path &filePath )
+{
+	auto fullPath = findFullFilePath( filePath );
+
+	for( auto &watch : mWatchList ) {
+		watch->unwatch( fullPath );
+	}
+}
+
+void FileWatcher::unwatch( const vector<fs::path> &filePaths )
+{
+	for( const auto &filePath : filePaths ) {
+		unwatch( filePath );
+	}
+}
+
+void FileWatcher::enable( const fs::path &filePath )
+{
+	auto fullPath = findFullFilePath( filePath );
+
+	for( auto &watch : mWatchList ) {
+		watch->setEnabled( true, fullPath );
+	}
+}
+
+void FileWatcher::disable( const fs::path &filePath )
+{
+	auto fullPath = findFullFilePath( filePath );
+
+	for( auto &watch : mWatchList ) {
+		watch->setEnabled( false, fullPath );
+	}
+}
+
+void FileWatcher::startWatching()
+{
+	if( app::App::get() && ! mUpdateConn.isConnected() )
+		mUpdateConn = app::App::get()->getSignalUpdate().connect( bind( &FileWatcher::update, this ) );
+
+	mThreadShouldQuit = false;
+	mThread = make_unique<thread>( std::bind( &FileWatcher::threadEntry, this ) );
+}
+
+void FileWatcher::stopWatching()
+{
+	mUpdateConn.disconnect();
+
+	mThreadShouldQuit = true;
+	if( mThread && mThread->joinable() ) {
+		mThread->join();
+		mThread = nullptr;
+	}
+}
+
+void FileWatcher::threadEntry()
+{
+	while( ! mThreadShouldQuit ) {
+		LOG_UPDATE( "elapsed seconds: " << app::getElapsedSeconds() );
+		{
+			lock_guard<recursive_mutex> lock( mMutex );
+
+			LOG_UPDATE( "\t - updating watches, elapsed seconds: " << app::getElapsedSeconds() );
+
+			try {
+				for( auto it = mWatchList.begin(); it != mWatchList.end(); /* */ ) {
+					const auto &watch = *it;
+
+					// erase discarded
+					if( watch->isDiscarded() ) {
+						it = mWatchList.erase( it );
+						continue;
+					}
+					// check if Watch's target has been modified and needs a callback, if not already marked.
+					if( ! watch->needsCallback() ) {
+						watch->checkCurrent();
+
+						// If the Watch needs a callback, move it to the front of the list
+						if( watch->needsCallback() && it != mWatchList.begin() ) {							
+							mWatchList.splice( mWatchList.begin(), mWatchList, it );
+						}
+					}
+
+					++it;
+				}
+			}
+			catch( fs::filesystem_error & ) {
+				// some file probably got locked by the system. Do nothing this update frame, we'll check again next
+			}
+		}
+
+		this_thread::sleep_for( chrono::duration<double>( mThreadUpdateInterval ) );
+	}
+}
+
+void FileWatcher::update()
+{
+	LOG_UPDATE( "elapsed seconds: " << app::getElapsedSeconds() );
+
+	// try-lock so we don't block the main thread, if we fail to acquire the mutex then we skip this update
+	unique_lock<recursive_mutex> lock( mMutex, std::try_to_lock );
+	if( ! lock.owns_lock() )
+		return;
+
+	LOG_UPDATE( "\t - checking watches, elapsed seconds: " << app::getElapsedSeconds() );
+
+	// Watches are sorted so that all that need a callback are in the beginning.
+	// So break when we hit the first one that doesn't need a callback
+	for( const auto &watch : mWatchList ) {
+
+		if( ! watch->needsCallback() )
+			break;
+
+		watch->emitCallback();
+	}
+}
+
+const size_t FileWatcher::getNumWatchedFiles() const
+{
+	size_t result = 0;
+	for( const auto &w : mWatchList ) {
+		result += w->getItems().size();
+	}
+
+	return result;
+}
+
+} // namespace cinder
