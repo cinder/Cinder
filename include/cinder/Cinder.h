e080e582c4d1f0c5f3866adf7485a547958b830a
Andrew Bell <cinder.afb@gmail.com>
2017-03-31 08:53:55 -0700

    Updating Cinder.h to reflect 0.9.2dev

--- include/cinder/Cinder.h
+++ include/cinder/Cinder.h
@@ -36,8 +36,8 @@
 //  CINDER_VERSION % 100 is the patch level
 //  CINDER_VERSION / 100 % 1000 is the minor version
 //  CINDER_VERSION / 100000 is the major version
-#define CINDER_VERSION		901
-#define CINDER_VERSION_STR	"0.9.1"
+#define CINDER_VERSION		902
+#define CINDER_VERSION_STR	"0.9.2dev"
 
 #if ! defined( ASIO_STANDALONE )
 #define ASIO_STANDALONE 1
