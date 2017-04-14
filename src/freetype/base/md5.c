097ffc797aa8496f45a7d31273c78ec8e1cf2f62
IntellectualKitty <IntellectualKitty@gmx.com>
2017-04-14 16:08:10 -0600

    Upgraded freetype to 2.7.1

--- src/freetype/base/md5.c
+++ src/freetype/base/md5.c
@@ -72,23 +72,23 @@
  *
  * Unfortunately, this optimization may be a C strict aliasing rules violation
  * if the caller's data buffer has effective type that cannot be aliased by
- * FT_MD5_u32plus.  In practice, this problem may occur if these MD5 routines are
+ * MD5_u32plus.  In practice, this problem may occur if these MD5 routines are
  * inlined into a calling function, or with future and dangerously advanced
  * link-time optimizations.  For the time being, keeping these MD5 routines in
  * their own translation unit avoids the problem.
  */
 #if defined(__i386__) || defined(__x86_64__) || defined(__vax__)
 #define SET(n) \
-	(*(FT_MD5_u32plus *)&ptr[(n) * 4])
+	(*(MD5_u32plus *)&ptr[(n) * 4])
 #define GET(n) \
 	SET(n)
 #else
 #define SET(n) \
 	(ctx->block[(n)] = \
-	(FT_MD5_u32plus)ptr[(n) * 4] | \
-	((FT_MD5_u32plus)ptr[(n) * 4 + 1] << 8) | \
-	((FT_MD5_u32plus)ptr[(n) * 4 + 2] << 16) | \
-	((FT_MD5_u32plus)ptr[(n) * 4 + 3] << 24))
+	(MD5_u32plus)ptr[(n) * 4] | \
+	((MD5_u32plus)ptr[(n) * 4 + 1] << 8) | \
+	((MD5_u32plus)ptr[(n) * 4 + 2] << 16) | \
+	((MD5_u32plus)ptr[(n) * 4 + 3] << 24))
 #define GET(n) \
 	(ctx->block[(n)])
 #endif
@@ -97,11 +97,11 @@
  * This processes one or more 64-byte data blocks, but does NOT update the bit
  * counters.  There are no alignment requirements.
  */
-static const void *body(FT_MD5_CTX *ctx, const void *data, unsigned long size)
+static const void *body(MD5_CTX *ctx, const void *data, unsigned long size)
 {
 	const unsigned char *ptr;
-	FT_MD5_u32plus a, b, c, d;
-	FT_MD5_u32plus saved_a, saved_b, saved_c, saved_d;
+	MD5_u32plus a, b, c, d;
+	MD5_u32plus saved_a, saved_b, saved_c, saved_d;
 
 	ptr = (const unsigned char *)data;
 
@@ -204,7 +204,7 @@ static const void *body(FT_MD5_CTX *ctx, const void *data, unsigned long size)
 	return ptr;
 }
 
-void FT_MD5_Init(FT_MD5_CTX *ctx)
+void MD5_Init(MD5_CTX *ctx)
 {
 	ctx->a = 0x67452301;
 	ctx->b = 0xefcdab89;
@@ -215,9 +215,9 @@ void FT_MD5_Init(FT_MD5_CTX *ctx)
 	ctx->hi = 0;
 }
 
-void FT_MD5_Update(FT_MD5_CTX *ctx, const void *data, unsigned long size)
+void MD5_Update(MD5_CTX *ctx, const void *data, unsigned long size)
 {
-	FT_MD5_u32plus saved_lo;
+	MD5_u32plus saved_lo;
 	unsigned long used, available;
 
 	saved_lo = ctx->lo;
@@ -255,7 +255,7 @@ void FT_MD5_Update(FT_MD5_CTX *ctx, const void *data, unsigned long size)
 	(dst)[2] = (unsigned char)((src) >> 16); \
 	(dst)[3] = (unsigned char)((src) >> 24);
 
-void FT_MD5_Final(unsigned char *result, FT_MD5_CTX *ctx)
+void MD5_Final(unsigned char *result, MD5_CTX *ctx)
 {
 	unsigned long used, available;
 
