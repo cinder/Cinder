8432966def64466e729f5e75a9cacdb902bad100
IntellectualKitty <IntellectualKitty@gmx.com>
2017-04-22 02:47:50 -0600

    Added osx-build.sh for use without xcpretty

---
+++ proj/xcode/osx-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CINDER_XCODEPROJ="${CURRENT_DIR}/cinder.xcodeproj"
+
+# OS X
+set -o pipefail && xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder -configuration Release $@
+set -o pipefail && xcrun xcodebuild -project ${CINDER_XCODEPROJ} -target cinder -configuration Debug $@
+
