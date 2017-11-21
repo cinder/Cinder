f000c34f37194404f6f6e11164f10af307154c7b
Andrew Bell <cinder.afb@gmail.com>
2017-11-21 19:23:20 +0000

    Emitting displayChanged signal on scaling change on MSW

--- src/cinder/app/msw/PlatformMsw.cpp
+++ src/cinder/app/msw/PlatformMsw.cpp
@@ -396,7 +396,7 @@ void app::PlatformMsw::refreshDisplays()
 			DisplayMsw *oldDisplay = reinterpret_cast<DisplayMsw*>( displayIt->get() );
 			if( oldDisplay->mMonitor == newDisplay->mMonitor ) {
 				// found this display; see if anything changed
-				if( ( oldDisplay->mArea != newDisplay->mArea ) || ( oldDisplay->mBitsPerPixel != newDisplay->mBitsPerPixel ) )
+				if( ( oldDisplay->mArea != newDisplay->mArea ) || ( oldDisplay->mBitsPerPixel != newDisplay->mBitsPerPixel ) || ( oldDisplay->mContentScale != newDisplay->mContentScale ) )
 					changedDisplays.push_back( *displayIt );
 				*oldDisplay = *newDisplay;
 				oldDisplay->mVisitedFlag = true;
