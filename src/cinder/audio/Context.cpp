991ef2126fbae6b7daf063904ebf4746657b4c65
Richard Eakin <rich.eakin@gmail.com>
2018-10-10 01:26:12 -0400

    register for master context cleanup from Context base constructor
    fixes missing cleanup signal when using Context::setAsMaster()

--- src/cinder/audio/Context.cpp
+++ src/cinder/audio/Context.cpp
@@ -97,9 +97,8 @@ Context* Context::master()
 #elif defined( CINDER_LINUX )
 		sMasterContext.reset( new linux::ContextPulseAudio() );
 #endif
-		if( ! sIsRegisteredForCleanup )
-			registerClearStatics();
 	}
+
 	return sMasterContext.get();
 }
 
@@ -122,10 +121,8 @@ DeviceManager* Context::deviceManager()
 #elif defined( CINDER_LINUX )
 		sDeviceManager.reset( new linux::DeviceManagerPulseAudio() );
 #endif
-
-		if( ! sIsRegisteredForCleanup )
-			registerClearStatics();
 	}
+
 	return sDeviceManager.get();
 }
 
@@ -139,6 +136,8 @@ void Context::setMaster( Context *masterContext, DeviceManager *deviceManager )
 Context::Context()
 	: mEnabled( false ), mAutoPullRequired( false ), mAutoPullCacheDirty( false ), mNumProcessedFrames( 0 ), mTimeDuringLastProcessLoop( -1.0 )
 {
+	if( ! sIsRegisteredForCleanup )
+		registerClearStatics();
 }
 
 Context::~Context()
