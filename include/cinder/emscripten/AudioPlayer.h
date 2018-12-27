4d9ee65c6b815c04dcd067f661d4e6cc96c975d8
Joseph Chow <joe@xoio.co>
2018-12-27 06:32:44 -0800

    Move + rename simpleaudio. Also more cleanup
    - rename simpleaudio to AudioPlayer and move into cinder/emscripten instead of cinder/audio/emscripten
    - add implementation file for AudioPlayer
    - remove node_modules folder
    - add node_modules to gitignore

---
+++ include/cinder/emscripten/AudioPlayer.h
@@ -0,0 +1,38 @@
+
+#include <emscripten.h>
+#include <emscripten/bind.h>
+#include <string>
+#include "cinder/emscripten/CinderEmscripten.h"
+
+
+namespace cinder { namespace em {
+
+  /**
+   * This is a very basic audio playback class utilizing the html <audio> tag. 
+   * It's purpose is solely playing back audio and has no built-in mechanism to 
+   * allow you to extract the audio information. 
+   */ 
+  class AudioPlayer
+  {
+    //! Represents the DOM <audio> tag
+    emscripten::val audioNode;
+  public:
+
+    AudioPlayer(
+      fs::path src,
+      bool useControls=false,
+      bool loop=false
+    );
+
+    //! Starts playback
+    void play();
+
+    //! Pauses playback
+    void pause();
+
+    //! Adds an additional source to the player. If the browser doesn't support
+    //! a particular file format, the browser will cycle through any additional sources
+    //! until a suitable one is found, or simply not play. 
+    AudioPlayer& addSource(fs::path src);
+  }
+}}
---
+++ src/cinder/emscripten/AudioPlayer.cpp
@@ -0,0 +1,53 @@
+#include "cinder/emscripten/AudioPlayer.h"
+
+using namespace ci;
+using namespace emscripten;
+
+namespace cinder { namespace em {
+
+    AudioPlayer::AudioPlayer( fs::path src, bool useControls, bool loop ):audioNode( val::undefined() )
+    {
+
+        audioNode = val::global( "document" ).createElement( "audio" );
+
+        // take the filename as the id of the element.
+        auto id = p.stem();
+        audioNode.set( "id", val( id ) );
+
+        // set cross-origin properties
+        audioNode.set( "crossorigin", val( "anonymous" ) );
+
+        // set whether or not it will loop
+        audioNode.set( "loop",val( loop ) );
+
+        if( useControls )
+        {
+            audioNode.set( "controls", useControls );
+        }
+
+        if( src != "" )
+        {
+            audioNode.set( "src",src.string() );
+        }
+
+        val::global( "document" )[ "body" ].call<void>( "appendChild", audioNode );
+    }
+
+    void AudioPlayer::play()
+    {
+        audioNode.call<void>( "play" );
+    }
+
+    void AudioPlayer::pause()
+    {
+        audioNode.call<void>( "pause" );
+    }
+
+    AudioPlayer& AudioPlayer::appendSource( fs::path src )
+    {
+        val source = val::global( "document" ).createElement( "source" );
+        source.set( "src",val( src.string() ) );
+        audioNode.call<void>( "append", source );
+        return *this;
+    }
+}}
