b4da3dedead1204c9c46363b0cd868d8b0545bd2
Joseph Chow <joe@xoio.co>
2019-01-11 07:01:32 -0800

    minor style cleanup

--- samples/_emscripten/dragdrop/src/dragdrop.cpp
+++ samples/_emscripten/dragdrop/src/dragdrop.cpp
@@ -1,16 +1,17 @@
 #include "dragdrop.h"
 #include "cinder/Log.h"
+
 using namespace ci;
 using namespace emscripten;
 using namespace std;
 
-DragDrop::DragDrop( std::string elId, std::function<void(emscripten::val)> onProcess):
-mEl(emscripten::val::undefined()),
-mFile(emscripten::val::undefined())
+DragDrop::DragDrop( std::string elId, std::function<void(emscripten::val)> onProcess ):
+mEl( emscripten::val::undefined() ),
+mFile( emscripten::val::undefined() )
 {
 
     // query element
-    mEl = em::helpers::querySelector(elId);    
+    mEl = em::helpers::querySelector( elId );    
 
     this->onProcess = onProcess;
 
@@ -21,11 +22,10 @@ mFile(emscripten::val::undefined())
         return;
     }
 
-
     setup();
 }
 
-void DragDrop::setOnDrop(std::function<void( emscripten::val )> onDrop)
+void DragDrop::setOnDrop( std::function<void( emscripten::val )> onDrop )
 {
     this->onDrop = onDrop;
     setup();
@@ -33,26 +33,31 @@ void DragDrop::setOnDrop(std::function<void( emscripten::val )> onDrop)
 
 // ========================= //
 
-void DragDrop::setup(){
+void DragDrop::setup()
+{
     // setup default handler for when things are dropped onto canvas. 
     // This assumes that only one item is being dropped at a time, if you need to handle more
     // than one file, add your own drop handler.  
-    std::function<void(emscripten::val)> func = onProcess;               
+    std::function<void(emscripten::val)> func = onProcess;     
+
     onDrop = [func]( emscripten::val e )-> void {
         e.call<void>( "preventDefault" );
 
         // if there are items being dropped
-        if(e["dataTransfer"]["items"].as<bool>()){
+        if( e["dataTransfer"]["items"].as<bool>() )
+        {
 
             // loop through and process whatever the last file was that was dropped. 
             auto len = e["dataTransfer"]["items"]["length"].as<int>();
+
             for(int i = 0; i < len; ++i){
                 emscripten::val itm = e["dataTransfer"]["items"][i];
 
                 // if the itm dropped is classified as file, set that to be the current item.
-                if(itm["kind"].as<string>() == "file"){
+                if( itm["kind"].as<string>() == "file" )
+                {
                   
-                   func(itm.call<val>("getAsFile"));
+                   func( itm.call<val>( "getAsFile" ) );
                 }
             }
         }
