e4a098b9247d22637f064ebd8af7eb2e79370f66
Pierre Schefler <schefler.pierre@gmail.com>
2020-01-12 21:51:09 +0100

    Fixed opengl samples for linux

--- samples/_opengl/ParticlesBasic/src/ParticlesBasicApp.cpp
+++ samples/_opengl/ParticlesBasic/src/ParticlesBasicApp.cpp
@@ -39,7 +39,7 @@ public:
 	bool	isOffscreen( const vec2 &v );
 
 public:
-	static const int	NUM_PARTICLES = 10000;
+	static const int	NUM_PARTICLES = 100;
 
 	float				mConservationOfVelocity;
 	float				mSpeed;
