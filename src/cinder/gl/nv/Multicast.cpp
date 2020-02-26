9158f966c83408496a57d4cfac7ea9d33699151f
RV <eric.renaud.houde@gmail.com>
2020-02-26 13:55:26 -0500

    Further cleanup and documentation.

--- src/cinder/gl/nv/Multicast.cpp
+++ src/cinder/gl/nv/Multicast.cpp
@@ -1,140 +1,128 @@
 #include "cinder/gl/nv/Multicast.h"
-
 #include "cinder/gl/Texture.h"
 #include "cinder/gl/BufferObj.h"
 #include "cinder/gl/scoped.h"
+#include "cinder/Noncopyable.h"
 
 namespace cinder { namespace gl { namespace nv { namespace multicast {
 
-GLuint Device::getIndex() const
-{
-	return mIndex;
-}
-
-GLbitfield Device::getMask() const
-{
-	return mMask;
-}
-
-Context& Context::instance()
-{
-	static Context sContext;
-	return sContext;
-}
-
-bool Context::isEnabled() const
-{
-	return mLinkedDevices.size() > 1;
-}
-
-const std::vector<Device>& Context::getDevices() const
-{
-	return mLinkedDevices;
-}
-
-GLbitfield Context::getMaskAll() const
-{
-	return mMaskAll;
-}
-
-Context::Context() : mMaskAll{ 0 }
-{
-	int numGPUs;
-	glGetIntegerv( GL_MULTICAST_GPUS_NV, &numGPUs );
-	for( int i = 0; i < numGPUs; ++i ) {
-		mLinkedDevices.emplace_back( Device{ (GLuint)i } );
-		mMaskAll |= mLinkedDevices.back().getMask();
+class Context : public Noncopyable {
+public:
+	static Context& instance() {
+		static Context sContext;
+		return sContext;
 	}
+	const std::vector<Device>& getDevices() const { return mLinkedDevices; }
+	GLbitfield					getMaskAll() const { return mMaskAll; }
+private:
+	Context() : mMaskAll{ 0 } {
+		int numGPUs;
+		glGetIntegerv( GL_MULTICAST_GPUS_NV, &numGPUs );
+		for( int i = 0; i < numGPUs; ++i ) {
+			mLinkedDevices.emplace_back( Device{ (GLuint)i } );
+			mMaskAll |= mLinkedDevices.back().getMask();
+		}
+	}
+	std::vector<Device> mLinkedDevices;
+	GLbitfield			mMaskAll;
+};
+} // multicast namespace
+
+const std::vector<multicast::Device>& multicast::getDevices()
+{
+	return multicast::Context::instance().getDevices();
+}
+GLbitfield multicast::getMask( const std::vector<Device>& devices )
+{
+	GLbitfield mask = 0;
+	for( const Device& device : devices ) {
+		mask |= device.getMask();
+	}
+	return mask;
 }
 
-bool isEnabled()
+void multicast::enableUploadMask( GLbitfield mask )
 {
-	return Context::instance().isEnabled();
+	glUploadGpuMaskNVX( mask );
 }
 
-const std::vector<Device>& getDevices()
-{
-	return Context::instance().getDevices();
-} 
-
-void enableUploadMask( const Device& device )
+void multicast::enableUploadMask( const Device& device )
 {
 	glUploadGpuMaskNVX( device.getMask() );
 }
 
-void enableUploadMask( GLbitfield mask )
+void multicast::disableUploadMask()
 {
-	glUploadGpuMaskNVX( mask );
+	glUploadGpuMaskNVX( multicast::Context::instance().getMaskAll() );
 }
 
-void enableUploadMask( const std::vector<Device>& devices )
+void multicast::enableRenderMask( GLbitfield mask )
 {
-	GLbitfield mask = 0;
-	for( auto& device : devices ) {
-		mask |= device.getMask();
-	}
-	glUploadGpuMaskNVX( mask );
+	glRenderGpuMaskNV( mask );
 }
 
-void disableUploadMask()
-{
-	glUploadGpuMaskNVX( Context::instance().getMaskAll() );
-}
-
-void enableRenderMask( const Device& device )
+void multicast::enableRenderMask( const Device& device )
 {
 	glRenderGpuMaskNV( device.getMask() );
 }
 
-void enableRenderMask( GLbitfield mask )
+void multicast::disableRenderMask()
 {
-	glRenderGpuMaskNV( mask );
+	glRenderGpuMaskNV( multicast::Context::instance().getMaskAll() );
 }
 
-void enableRenderMask( const std::vector<Device>& devices )
-{
-	GLbitfield mask = 0;
-	for( auto& device : devices ) {
-		mask |= device.getMask();
-	}
-	glRenderGpuMaskNV( mask );
-}
-
-void disableRenderMask()
-{
-	glRenderGpuMaskNV( Context::instance().getMaskAll() );
-}
-
-void signalWaitSync( const Device& source, const Device& target )
+void multicast::signalWaitSync( const multicast::Device& source, const multicast::Device& target )
 {
 	glMulticastWaitSyncNV( source.getIndex(), target.getMask() );
 }
 
-void signalWaitSync( GLbitfield indexSource, GLbitfield targetMask )
+void multicast::signalWaitSync( GLbitfield indexSource, GLbitfield targetMask )
 {
 	glMulticastWaitSyncNV( indexSource, targetMask );
 }
 
-void updateMasked( const std::shared_ptr<BufferObj>& buffer, GLsizeiptr size, const void* data, GLbitfield gpuMask )
+void multicast::bufferSubData( const std::shared_ptr<BufferObj>& buffer, GLsizeiptr size, const void* data, GLbitfield targetMask )
 {
-	glMulticastBufferSubDataNV( gpuMask, buffer->getId(), 0, size, data );
+	glMulticastBufferSubDataNV( targetMask, buffer->getId(), 0, size, data );
 }
 
-void copyTexture( const std::shared_ptr<TextureBase>& source, const std::shared_ptr<TextureBase>& destination, GLuint sourceDeviceIndex, GLbitfield destinationMask )
+void multicast::bufferSubData( const std::shared_ptr<BufferObj>& buffer, GLsizeiptr size, const void* data, const Device& targetDevice )
+{
+	glMulticastBufferSubDataNV( targetDevice.getMask(), buffer->getId(), 0, size, data );
+}
+
+void multicast::copyImageSubData( const std::shared_ptr<TextureBase>& source, const std::shared_ptr<TextureBase>& destination, GLuint sourceDeviceIndex, GLbitfield destinationMask )
 {
 	glMulticastCopyImageSubDataNV( sourceDeviceIndex, destinationMask, source->getId(), GL_TEXTURE_2D, 0, 0, 0, 0, destination->getId(), GL_TEXTURE_2D, 0, 0, 0, 0, source->getWidth(), source->getHeight(), 1 );
 }
 
-void scissor( const Device& device, const ivec2& position, const ivec2& size )
+void multicast::copyImageSubData( const std::shared_ptr<TextureBase>& source, const std::shared_ptr<TextureBase>& destination, const Device& sourceDevice, const Device& targetDevice )
+{
+	glMulticastCopyImageSubDataNV( sourceDevice.getIndex(), targetDevice.getMask(), source->getId(), GL_TEXTURE_2D, 0, 0, 0, 0, destination->getId(), GL_TEXTURE_2D, 0, 0, 0, 0, source->getWidth(), source->getHeight(), 1 );
+}
+
+void multicast::scissor( GLuint deviceIndex, const glm::ivec2& position, const glm::ivec2& size )
+{
+	const GLint scissors[4] = { position.x, position.y, size.x, size.y };
+	glMulticastScissorArrayvNVX( deviceIndex, 0, 1, scissors );
+}
+
+void multicast::scissor( const Device& device, const ivec2& position, const ivec2& size )
 {
 	const GLint scissors[4] = { position.x, position.y, size.x, size.y };
 	glMulticastScissorArrayvNVX( device.getIndex(), 0, 1, scissors );
 }
 
-void viewport( const Device& device, const ivec2& position, const ivec2& size )
+void multicast::viewport( GLuint deviceIndex, const glm::ivec2& position, const glm::ivec2& size )
+{
+	const GLfloat viewports[4] = { (float)position.x, (float)position.y, (float)size.x, (float)size.y };
+	glMulticastViewportArrayvNVX( deviceIndex, 0, 1, viewports );
+}
+
+void multicast::viewport( const Device& device, const ivec2& position, const ivec2& size )
 {
 	const GLfloat viewports[4] = { (float)position.x, (float)position.y, (float)size.x, (float)size.y };
 	glMulticastViewportArrayvNVX( device.getIndex(), 0, 1, viewports );
 }
 
-} } } }
+} } } // ci::gl::nv namespaces
