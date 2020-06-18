1a4153f68ca15d1645487234cd54cea595671288
Brad Howes <bradhowes@mac.com>
2020-06-18 18:22:09 +0200

    Correct indentation

--- src/cinder/audio/Param.cpp
+++ src/cinder/audio/Param.cpp
@@ -352,7 +352,7 @@ void Param::removeEventsAt( double time )
 		if( event.getTimeBegin() >= time ) {
 			if( contextDisabled ) {
 				eventIt = mEvents.erase( eventIt );
-                continue;
+                                continue;
 			}
 			else {
 				event.cancel();
