77fd511d92c2b98ef60c4a767c1755874ebd7efe
ufidstudios-ch <61551542+ufidstudios-ch@users.noreply.github.com>
2020-12-27 02:52:17 +0100

    Gradle structure changes

---
+++ src/cinder/android/app/src/main/java/ch/ufidstudios/androidcppskeleton/MainActivity.kt
@@ -0,0 +1,28 @@
+package ch.ufidstudios.androidcppskeleton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
