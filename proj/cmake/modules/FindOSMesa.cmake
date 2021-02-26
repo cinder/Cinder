17ee33d7b5e1775ae8baac42d943e7563fd1a144
Andrew Bell <cinder.afb@gmail.com>
2021-02-26 17:15:28 -0500

    Updating FindOSMesa.cmake to search ${OSMESA_ROOT}/lib/x86_64-linux-gnu

--- proj/cmake/modules/FindOSMesa.cmake
+++ proj/cmake/modules/FindOSMesa.cmake
@@ -5,8 +5,8 @@
 # OSMESA_LIBRARIES		- Link this to use OSMesa
 
 find_path( OSMESA_INCLUDE_DIR NAMES GL/osmesa.h PATHS  "${OSMESA_ROOT}/include" "$ENV{OSMESA_ROOT}/include" "/usr/local/include" "/usr/include" NO_DEFAULT_PATH )
-find_library( OSMESA_LIBRARIES NAMES OSMesa PATHS  "${OSMESA_ROOT}/lib" "$ENV{OSMESA_ROOT}/lib" "/usr/local/lib" "/usr/lib/x86_64-linux-gnu" PATH_SUFFIXES gallium NO_DEFAULT_PATH )
-find_library( OSMESA_GL_LIBRARIES Names GL PATHS "${OSMESA_ROOT}/lib" "$ENV{OSMESA_ROOT}/lib" "/usr/local/lib" "/usr/lib/x86_64-linux-gnu" PATH_SUFFIXES gallium NO_DEFAULT_PATH )
+find_library( OSMESA_LIBRARIES NAMES OSMesa PATHS  "${OSMESA_ROOT}/lib" "$ENV{OSMESA_ROOT}/lib" "${OSMESA_ROOT}/lib/x86_64-linux-gnu" "/usr/local/lib" "/usr/lib/x86_64-linux-gnu" PATH_SUFFIXES gallium NO_DEFAULT_PATH )
+find_library( OSMESA_GL_LIBRARIES Names GL PATHS "${OSMESA_ROOT}/lib" "$ENV{OSMESA_ROOT}/lib" "${OSMESA_ROOT}/lib/x86_64-linux-gnu" "/usr/local/lib" "/usr/lib/x86_64-linux-gnu" PATH_SUFFIXES gallium NO_DEFAULT_PATH )
 
 include( FindPackageHandleStandardArgs )
 find_package_handle_standard_args( OSMesa DEFAULT_MSG OSMESA_LIBRARIES OSMESA_INCLUDE_DIR OSMESA_GL_LIBRARIES )
